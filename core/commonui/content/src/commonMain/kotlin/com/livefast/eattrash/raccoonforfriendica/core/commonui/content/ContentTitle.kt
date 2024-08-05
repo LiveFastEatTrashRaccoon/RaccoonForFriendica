520f78c7aebda21ead01db91f16ac52d5e5907d6
Akesi Seli <andongue.nena@gmail.com>
2024-08-05 16:19:46 +0200

    add ContentTitle

---
+++ core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/ContentTitle.kt
@@ -0,0 +1,37 @@
+package com.livefast.eattrash.raccoonforfriendica.core.commonui.content
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.livefast.eattrash.raccoonforfriendica.core.htmlparse.parseHtml
+
+@Composable
+fun ContentTitle(
+    content: String = "",
+    modifier: Modifier = Modifier,
+    onOpenUrl: ((String) -> Unit)? = null,
+) {
+    val fullColor = MaterialTheme.colorScheme.onBackground
+    Box(modifier = modifier) {
+        val annotatedContent =
+            content.parseHtml(
+                linkColor = MaterialTheme.colorScheme.primary,
+            )
+        ClickableText(
+            style = MaterialTheme.typography.titleSmall.copy(color = fullColor),
+            text = annotatedContent,
+            onClick = { offset ->
+                val url =
+                    annotatedContent
+                        .getStringAnnotations(start = offset, end = offset)
+                        .firstOrNull()
+                        ?.item
+                if (!url.isNullOrBlank()) {
+                    onOpenUrl?.invoke(url)
+                }
+            },
+        )
+    }
+}
