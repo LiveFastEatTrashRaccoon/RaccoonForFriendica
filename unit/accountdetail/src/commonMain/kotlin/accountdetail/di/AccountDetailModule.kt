ea2356448d8e4d9726d7da4f90dafa51ceb1ff19
Akesi Seli <andongue.nena@gmail.com>
2024-08-06 00:49:17 +0200

    setup DI

---
+++ unit/accountdetail/src/commonMain/kotlin/accountdetail/di/AccountDetailModule.kt
@@ -0,0 +1,15 @@
+package accountdetail.di
+
+import accountdetail.AccountDetailMviModel
+import accountdetail.AccountDetailViewModel
+import org.koin.dsl.module
+
+val unitAccountDetailModule =
+    module {
+        factory<AccountDetailMviModel> { params ->
+            AccountDetailViewModel(
+                id = params[0],
+                accountRepository = get(),
+            )
+        }
+    }
