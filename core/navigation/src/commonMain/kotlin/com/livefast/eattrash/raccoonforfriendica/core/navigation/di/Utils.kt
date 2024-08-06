16ee6e9a7748ae4b852c48364011346008a2709c
Akesi Seli <andongue.nena@gmail.com>
2024-08-06 17:18:54 +0200

    create DetailOpener and its implementation

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
+++ core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
@@ -1,5 +1,8 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation.di
 
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
 
 expect fun getNavigationCoordinator(): NavigationCoordinator
+
+expect fun getDetailOpener(): DetailOpener
--- core/navigation/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
+++ core/navigation/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
@@ -1,11 +1,15 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation.di
 
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 actual fun getNavigationCoordinator(): NavigationCoordinator = CoreNavigationDiHelper.navigationCoordinator
 
+actual fun getDetailOpener(): DetailOpener = CoreNavigationDiHelper.detailOpener
+
 internal object CoreNavigationDiHelper : KoinComponent {
     val navigationCoordinator: NavigationCoordinator by inject()
+    val detailOpener: DetailOpener by inject()
 }
