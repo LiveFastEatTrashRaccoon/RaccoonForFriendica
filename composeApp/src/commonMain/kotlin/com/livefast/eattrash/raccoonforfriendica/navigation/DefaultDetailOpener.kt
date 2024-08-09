bf5dac0becb8d81dc1fb3de7c4540288614dd2ba
Akesi Seli <andongue.nena@gmail.com>
2024-08-10 00:27:11 +0200

    add opening methods to DetailOpener

--- composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/navigation/DefaultDetailOpener.kt
+++ composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/navigation/DefaultDetailOpener.kt
@@ -3,10 +3,12 @@ package com.livefast.eattrash.raccoonforfriendica.navigation
 import com.livefast.eattrash.feature.userdetail.UserDetailScreen
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserListType
 import com.livefast.eattrash.raccoonforfriendica.feature.entrydetail.EntryDetailScreen
 import com.livefast.eattrash.raccoonforfriendica.feature.hashtag.HashtagScreen
 import com.livefast.eattrash.raccoonforfriendica.feature.login.LoginScreen
 import com.livefast.eattrash.raccoonforfriendica.feature.settings.SettingsScreen
+import com.livefast.eattrash.raccoonforfriendica.feature.userlist.UserListScreen
 
 class DefaultDetailOpener(
     private val navigationCoordinator: NavigationCoordinator,
@@ -33,6 +35,16 @@ class DefaultDetailOpener(
 
     override fun openHashtag(tag: String) {
         val screen = HashtagScreen(tag)
+        navigationCoordinator.push(screen)
+    }
+
+    override fun openFollowers(userId: String) {
+        val screen = UserListScreen(userId, UserListType.Follower)
+        navigationCoordinator.push(screen)
+    }
+
+    override fun openFollowing(userId: String) {
+        val screen = UserListScreen(userId, UserListType.Following)
         navigationCoordinator.push(screen)
     }
 }
