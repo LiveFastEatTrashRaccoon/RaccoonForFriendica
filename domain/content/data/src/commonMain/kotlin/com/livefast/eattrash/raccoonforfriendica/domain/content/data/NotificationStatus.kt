b8af5ca7e05c9ce112616836b47da18dd4b61cf6
Akesi Seli <andongue.nena@gmail.com>
2024-08-09 09:35:33 +0200

    move relationship and notification status to model

--- domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/NotificationStatus.kt
+++ domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/NotificationStatus.kt
@@ -1,5 +1,11 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.data
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.NotificationsActive
+import androidx.compose.material.icons.filled.NotificationsNone
+import androidx.compose.ui.graphics.vector.ImageVector
+
 sealed interface NotificationStatus {
     data object Enabled : NotificationStatus
 
@@ -13,4 +19,11 @@ fun RelationshipModel.toNotificationStatus(): NotificationStatus =
         mutingNotifications -> NotificationStatus.Disabled
         notifying -> NotificationStatus.Enabled
         else -> NotificationStatus.Undetermined
+    }
+
+fun NotificationStatus.toIcon(): ImageVector =
+    when (this) {
+        NotificationStatus.Disabled -> Icons.Default.NotificationsNone
+        NotificationStatus.Enabled -> Icons.Default.NotificationsActive
+        NotificationStatus.Undetermined -> Icons.Default.Notifications
     }
