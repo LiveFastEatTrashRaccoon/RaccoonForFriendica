e5579db0ac3868a12e1bdcab717c81d4ef9661c2
Akesi Seli <andongue.nena@gmail.com>
2024-08-09 15:30:50 +0200

    add pagination manager for explore section

--- domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/di/ContentPaginationModule.kt
+++ domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/di/ContentPaginationModule.kt
@@ -1,7 +1,9 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.di
 
+import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.DefaultExplorePaginationManager
 import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.DefaultNotificationsPaginationManager
 import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.DefaultTimelinePaginationManager
+import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.ExplorePaginationManager
 import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.NotificationsPaginationManager
 import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.TimelinePaginationManager
 import org.koin.dsl.module
@@ -16,6 +18,12 @@ val domainContentPaginationModule =
         factory<NotificationsPaginationManager> {
             DefaultNotificationsPaginationManager(
                 notificationRepository = get(),
+                accountRepository = get(),
+            )
+        }
+        factory<ExplorePaginationManager> {
+            DefaultExplorePaginationManager(
+                trendsRepository = get(),
                 accountRepository = get(),
             )
         }
