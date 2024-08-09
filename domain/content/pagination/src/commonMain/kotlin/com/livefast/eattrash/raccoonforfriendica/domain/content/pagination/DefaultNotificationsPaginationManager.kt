1ce77ccb0984705d2899dda974837f982e9fd6b0
Akesi Seli <andongue.nena@gmail.com>
2024-08-09 09:37:22 +0200

    retrieve relationship status in notifications

--- domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/DefaultNotificationsPaginationManager.kt
+++ domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/DefaultNotificationsPaginationManager.kt
@@ -1,10 +1,14 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.pagination
 
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.NotificationModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toNotificationStatus
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toStatus
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.AccountRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.NotificationRepository
 
 internal class DefaultNotificationsPaginationManager(
     private val notificationRepository: NotificationRepository,
+    private val accountRepository: AccountRepository,
 ) : NotificationsPaginationManager {
     private var specification: NotificationsPaginationSpecification? = null
     private var pageCursor: String? = null
@@ -28,6 +32,22 @@ internal class DefaultNotificationsPaginationManager(
                         types = specification.types,
                     )
             }.deduplicate()
+                .map {
+                    // get relationship status with account
+                    val accountId = it.account?.id
+                    if (accountId == null) {
+                        it
+                    } else {
+                        val relationship = accountRepository.getRelationship(accountId)
+                        it.copy(
+                            account =
+                                it.account?.copy(
+                                    relationshipStatus = relationship?.toStatus(),
+                                    notificationStatus = relationship?.toNotificationStatus(),
+                                ),
+                        )
+                    }
+                }
 
         if (results.isNotEmpty()) {
             pageCursor = results.last().id
