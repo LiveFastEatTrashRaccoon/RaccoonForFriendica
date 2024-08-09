9a6b6c9ce2acbeec1b1b0ede257516e821b0f9d0
Akesi Seli <andongue.nena@gmail.com>
2024-08-09 17:26:45 +0200

    implement timeline selection

--- feature/timeline/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/timeline/TimelineScreen.kt
+++ feature/timeline/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/timeline/TimelineScreen.kt
@@ -1,5 +1,6 @@
 package com.livefast.eattrash.raccoonforfriendica.feature.timeline
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
@@ -16,15 +18,19 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -37,7 +43,10 @@ import com.livefast.eattrash.raccoonforfriendica.core.l10n.messages.LocalStrings
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getDetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getNavigationCoordinator
 import com.livefast.eattrash.raccoonforfriendica.core.utils.di.getUrlManager
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.readableName
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toReadableName
+import com.livefast.eattrash.raccoonforfriendica.feature.timeline.composable.TimelineTypeBottomSheet
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class TimelineScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
@@ -52,15 +61,39 @@ class TimelineScreen : Screen {
         val uriHandler = LocalUriHandler.current
         val urlManager = remember { getUrlManager(uriHandler) }
         val detailOpener = remember { getDetailOpener() }
+        var timelineTypeSelectorOpen by remember { mutableStateOf(false) }
+        val lazyListState = rememberLazyListState()
+
+        LaunchedEffect(model) {
+            model.effects
+                .onEach { event ->
+                    when (event) {
+                        TimelineMviModel.Effect.BackToTop ->
+                            runCatching {
+                                lazyListState.scrollToItem(0)
+                                topAppBarState.heightOffset = 0f
+                                topAppBarState.contentOffset = 0f
+                            }
+                    }
+                }.launchIn(this)
+        }
 
         Scaffold(
             topBar = {
                 TopAppBar(
                     scrollBehavior = scrollBehavior,
                     title = {
-                        Column {
+                        Column(
+                            modifier =
+                                Modifier
+                                    .clickable {
+                                        if (uiState.availableTimelineTypes.isNotEmpty()) {
+                                            timelineTypeSelectorOpen = true
+                                        }
+                                    }.fillMaxWidth(),
+                        ) {
                             Text(
-                                text = uiState.timelineType.readableName(),
+                                text = uiState.timelineType.toReadableName(),
                                 style = MaterialTheme.typography.titleMedium,
                             )
                             Text(
@@ -97,7 +130,9 @@ class TimelineScreen : Screen {
                         ).nestedScroll(scrollBehavior.nestedScrollConnection)
                         .pullRefresh(pullRefreshState),
             ) {
-                LazyColumn {
+                LazyColumn(
+                    state = lazyListState,
+                ) {
                     items(
                         items = uiState.entries,
                         key = { it.id },
@@ -146,6 +181,23 @@ class TimelineScreen : Screen {
                     contentColor = MaterialTheme.colorScheme.onBackground,
                 )
             }
+        }
+
+        if (timelineTypeSelectorOpen) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    timelineTypeSelectorOpen = false
+                },
+                content = {
+                    TimelineTypeBottomSheet(
+                        types = uiState.availableTimelineTypes,
+                        onSelected = { type ->
+                            timelineTypeSelectorOpen = false
+                            model.reduce(TimelineMviModel.Intent.ChangeType(type))
+                        },
+                    )
+                },
+            )
         }
     }
 }
