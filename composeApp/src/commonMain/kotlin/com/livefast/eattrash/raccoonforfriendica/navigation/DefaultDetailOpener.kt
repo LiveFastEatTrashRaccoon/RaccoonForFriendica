6ad2676bb36041c8ae95e75627a4fd31a9afb86f
Akesi Seli <andongue.nena@gmail.com>
2024-08-09 21:37:00 +0200

    add ability to open users and hashtags to DetailOpener

--- composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/navigation/DefaultDetailOpener.kt
+++ composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/navigation/DefaultDetailOpener.kt
@@ -1,17 +1,18 @@
 package com.livefast.eattrash.raccoonforfriendica.navigation
 
-import com.livefast.eattrash.feature.accountdetail.AccountDetailScreen
+import com.livefast.eattrash.feature.userdetail.UserDetailScreen
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
 import com.livefast.eattrash.raccoonforfriendica.feature.entrydetail.EntryDetailScreen
+import com.livefast.eattrash.raccoonforfriendica.feature.hashtag.HashtagScreen
 import com.livefast.eattrash.raccoonforfriendica.feature.login.LoginScreen
 import com.livefast.eattrash.raccoonforfriendica.feature.settings.SettingsScreen
 
 class DefaultDetailOpener(
     private val navigationCoordinator: NavigationCoordinator,
 ) : DetailOpener {
-    override fun openAccountDetail(id: String) {
-        val screen = AccountDetailScreen(id)
+    override fun openUserDetail(id: String) {
+        val screen = UserDetailScreen(id)
         navigationCoordinator.push(screen)
     }
 
@@ -27,6 +28,11 @@ class DefaultDetailOpener(
 
     override fun openLogin() {
         val screen = LoginScreen()
+        navigationCoordinator.push(screen)
+    }
+
+    override fun openHashtag(tag: String) {
+        val screen = HashtagScreen(tag)
         navigationCoordinator.push(screen)
     }
 }
