c2ee33b14dc74bdf05195ba9dc6786dbf6d098b6
Akesi Seli <andongue.nena@gmail.com>
2024-08-11 13:12:22 +0200

    add drawer coordinator

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/NavigationModule.kt
+++ core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/NavigationModule.kt
@@ -1,6 +1,8 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation.di
 
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DefaultDrawerCoordinator
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DefaultNavigationCoordinator
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DrawerCoordinator
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
 import org.koin.dsl.module
 
@@ -8,5 +10,8 @@ val coreNavigationModule =
     module {
         single<NavigationCoordinator> {
             DefaultNavigationCoordinator()
+        }
+        single<DrawerCoordinator> {
+            DefaultDrawerCoordinator()
         }
     }
--- core/navigation/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
+++ core/navigation/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
@@ -1,6 +1,7 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation.di
 
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DrawerCoordinator
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -9,7 +10,10 @@ actual fun getNavigationCoordinator(): NavigationCoordinator = CoreNavigationDiH
 
 actual fun getDetailOpener(): DetailOpener = CoreNavigationDiHelper.detailOpener
 
+actual fun getDrawerCoordinator() = CoreNavigationDiHelper.drawerCoordinator
+
 internal object CoreNavigationDiHelper : KoinComponent {
     val navigationCoordinator: NavigationCoordinator by inject()
     val detailOpener: DetailOpener by inject()
+    val drawerCoordinator: DrawerCoordinator by inject()
 }
