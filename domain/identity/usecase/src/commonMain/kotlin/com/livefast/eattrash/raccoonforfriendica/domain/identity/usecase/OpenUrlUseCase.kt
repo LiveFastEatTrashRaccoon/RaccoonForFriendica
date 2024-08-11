96b0da87ebd28f708ca10cd8698e60140440cbf6
Akesi Seli <andongue.nena@gmail.com>
2024-08-11 07:38:44 +0200

    add @Stable annotations to object used in lambdas

--- domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/OpenUrlUseCase.kt
+++ domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/OpenUrlUseCase.kt
@@ -1,5 +1,8 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.identity.usecase
 
+import androidx.compose.runtime.Stable
+
+@Stable
 interface OpenUrlUseCase {
     operator fun invoke(url: String)
 }
