fb5147ec4b4a3486f8f123b64e2704380ec89c12
Akesi Seli <andongue.nena@gmail.com>
2024-08-14 21:54:19 +0200

    implement user detail in forum mode

---
+++ feature/userdetail/src/commonMain/kotlin/com/livefast/eattrash/feature/userdetail/forum/ForumListMviModel.kt
@@ -0,0 +1,41 @@
+package com.livefast.eattrash.feature.userdetail.forum
+
+import androidx.compose.runtime.Stable
+import cafe.adriel.voyager.core.model.ScreenModel
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
+
+@Stable
+interface ForumListMviModel :
+    ScreenModel,
+    MviModel<ForumListMviModel.Intent, ForumListMviModel.State, ForumListMviModel.Effect> {
+    sealed interface Intent {
+        data object Refresh : Intent
+
+        data object LoadNextPage : Intent
+
+        data class ToggleReblog(
+            val entry: TimelineEntryModel,
+        ) : Intent
+
+        data class ToggleFavorite(
+            val entry: TimelineEntryModel,
+        ) : Intent
+
+        data class ToggleBookmark(
+            val entry: TimelineEntryModel,
+        ) : Intent
+    }
+
+    data class State(
+        val refreshing: Boolean = false,
+        val loading: Boolean = false,
+        val initial: Boolean = true,
+        val canFetchMore: Boolean = true,
+        val user: UserModel? = null,
+        val entries: List<TimelineEntryModel> = emptyList(),
+    )
+
+    sealed interface Effect
+}
