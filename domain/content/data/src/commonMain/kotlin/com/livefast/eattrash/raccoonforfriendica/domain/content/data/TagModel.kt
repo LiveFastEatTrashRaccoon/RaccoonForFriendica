ad9b391cf5a91730d6eb36b50c8a07548d8ad4fc
Akesi Seli <andongue.nena@gmail.com>
2024-08-16 10:18:56 +0200

    add property to tag model

--- domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/TagModel.kt
+++ domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/TagModel.kt
@@ -1,8 +1,12 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.data
 
+import kotlin.jvm.Transient
+
 data class TagModel(
     val name: String,
     val url: String? = null,
     val following: Boolean? = null,
+    @Transient
+    val followingPending: Boolean = false,
     val history: List<HashtagHistoryItem> = emptyList(),
 )
