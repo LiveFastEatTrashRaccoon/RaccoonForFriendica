7024d65c08a6d0c04989be7382e1051a5ef0a1ab
Akesi Seli <andongue.nena@gmail.com>
2024-08-17 17:45:16 +0200

    add support for options to timeline items

--- core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/TimelineItem.kt
+++ core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/TimelineItem.kt
@@ -4,41 +4,34 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.IconSize
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.CustomDropDown
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.MediaType
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
-
-val TimelineEntryModel.safeKey: String
-    get() {
-        fun StringBuilder.appendKeys(e: TimelineEntryModel) {
-            append(e.id)
-            append("-")
-            append(e.favorite)
-            append("-")
-            append(e.favoriteLoading)
-            append("-")
-            append(e.reblogged)
-            append("-")
-            append(e.reblogLoading)
-            append("-")
-            append(e.bookmarked)
-            append("-")
-            append(e.bookmarkLoading)
-        }
-        return buildString {
-            appendKeys(this@safeKey)
-            reblog?.run {
-                append("--")
-                appendKeys(this@run)
-            }
-        }
-    }
 
 @Composable
 fun TimelineItem(
@@ -48,6 +41,7 @@ fun TimelineItem(
     extendedSocialInfoEnabled: Boolean = false,
     reshareAndReplyVisible: Boolean = true,
     blurNsfw: Boolean = true,
+    options: List<Option> = emptyList(),
     onOpenUrl: ((String) -> Unit)? = null,
     onClick: ((TimelineEntryModel) -> Unit)? = null,
     onOpenUser: ((UserModel) -> Unit)? = null,
@@ -57,9 +51,12 @@ fun TimelineItem(
     onBookmark: ((TimelineEntryModel) -> Unit)? = null,
     onOpenUsersFavorite: ((TimelineEntryModel) -> Unit)? = null,
     onOpenUsersReblog: ((TimelineEntryModel) -> Unit)? = null,
+    onOptionSelected: ((OptionId) -> Unit)? = null,
 ) {
     val isReblog = entry.reblog != null
     val entryToDisplay = entry.reblog ?: entry
+    var optionsOffset by remember { mutableStateOf(Offset.Zero) }
+    var optionsMenuOpen by remember { mutableStateOf(false) }
 
     Box(
         modifier =
@@ -71,33 +68,83 @@ fun TimelineItem(
         Column(
             verticalArrangement = Arrangement.spacedBy(Spacing.xs),
         ) {
-            if (reshareAndReplyVisible) {
-                if (isReblog) {
-                    entry.creator?.let {
-                        ReblogInfo(
-                            modifier = Modifier.fillMaxWidth(),
-                            user = it,
-                            onOpenUser = onOpenUser,
-                        )
+            Row {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(Spacing.xs),
+                ) {
+                    if (reshareAndReplyVisible) {
+                        if (isReblog) {
+                            entry.creator?.let {
+                                ReblogInfo(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    user = it,
+                                    onOpenUser = onOpenUser,
+                                )
+                            }
+                        } else {
+                            entry.inReplyTo?.creator?.let {
+                                InReplyToInfo(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    user = it,
+                                    onOpenUser = onOpenUser,
+                                )
+                            }
+                        }
                     }
-                } else {
-                    entry.inReplyTo?.creator?.let {
-                        InReplyToInfo(
-                            modifier = Modifier.fillMaxWidth(),
-                            user = it,
-                            onOpenUser = onOpenUser,
+
+                    ContentHeader(
+                        modifier = Modifier.fillMaxWidth(),
+                        user = entryToDisplay.creator,
+                        date = entryToDisplay.edited ?: entryToDisplay.created,
+                        isEdited = entryToDisplay.edited != null,
+                        onOpenUser = onOpenUser,
+                    )
+                }
+                if (options.isNotEmpty()) {
+                    Box {
+                        Icon(
+                            modifier =
+                                Modifier
+                                    .size(IconSize.m)
+                                    .padding(Spacing.xs)
+                                    .onGloballyPositioned {
+                                        optionsOffset = it.positionInParent()
+                                    }.clickable {
+                                        optionsMenuOpen = true
+                                    },
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onBackground,
                         )
+                        CustomDropDown(
+                            expanded = optionsMenuOpen,
+                            onDismiss = {
+                                optionsMenuOpen = false
+                            },
+                            offset =
+                                with(LocalDensity.current) {
+                                    DpOffset(
+                                        x = optionsOffset.x.toDp(),
+                                        y = optionsOffset.y.toDp(),
+                                    )
+                                },
+                        ) {
+                            options.forEach { option ->
+                                DropdownMenuItem(
+                                    text = {
+                                        Text(option.label)
+                                    },
+                                    onClick = {
+                                        optionsMenuOpen = false
+                                        onOptionSelected?.invoke(option.id)
+                                    },
+                                )
+                            }
+                        }
                     }
                 }
             }
-
-            ContentHeader(
-                modifier = Modifier.fillMaxWidth(),
-                user = entryToDisplay.creator,
-                date = entryToDisplay.edited ?: entryToDisplay.created,
-                isEdited = entryToDisplay.edited != null,
-                onOpenUser = onOpenUser,
-            )
 
             entryToDisplay.title?.let { title ->
                 ContentTitle(
