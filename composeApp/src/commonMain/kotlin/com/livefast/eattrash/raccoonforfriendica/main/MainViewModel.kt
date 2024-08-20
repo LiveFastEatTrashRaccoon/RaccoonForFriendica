4488554b7850f0fd11b06003669759d698a958b6
Akesi Seli <andongue.nena@gmail.com>
2024-08-20 19:16:51 +0200

    update main view model to listen to inbox unread items

--- composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/main/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/main/MainViewModel.kt
@@ -3,22 +3,33 @@ package com.livefast.eattrash.raccoonforfriendica.main
 import cafe.adriel.voyager.core.model.screenModelScope
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.DefaultMviModel
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.BottomNavigationSection
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.InboxManager
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.IdentityRepository
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class MainViewModel :
-    DefaultMviModel<MainMviModel.Intent, MainMviModel.UiState, MainMviModel.Effect>(
-        initialState =
-            MainMviModel.UiState(
-                bottomNavigationSections =
-                    listOf(
-                        BottomNavigationSection.Home,
-                        BottomNavigationSection.Explore,
-                        BottomNavigationSection.Inbox,
-                        BottomNavigationSection.Profile,
-                    ),
-            ),
+class MainViewModel(
+    private val inboxManager: InboxManager,
+    private val identityRepository: IdentityRepository,
+) : DefaultMviModel<MainMviModel.Intent, MainMviModel.UiState, MainMviModel.Effect>(
+        initialState = MainMviModel.UiState(),
     ),
     MainMviModel {
+    init {
+        screenModelScope.launch {
+            inboxManager.unreadCount
+                .onEach { inboxUnread ->
+                    updateState {
+                        it.copy(bottomNavigationSections = getSections(inboxUnread))
+                    }
+                }.launchIn(this)
+            identityRepository.isLogged
+                .onEach {
+                    inboxManager.refreshUnreadCount()
+                }.launchIn(this)
+        }
+    }
 
     override fun reduce(intent: MainMviModel.Intent) {
         when (intent) {
@@ -29,4 +40,14 @@ class MainViewModel :
             }
         }
     }
+
+    private fun getSections(inboxUnread: Int) =
+        listOf(
+            BottomNavigationSection.Home,
+            BottomNavigationSection.Explore,
+            BottomNavigationSection.Inbox(
+                unreadItems = inboxUnread,
+            ),
+            BottomNavigationSection.Profile,
+        )
 }
