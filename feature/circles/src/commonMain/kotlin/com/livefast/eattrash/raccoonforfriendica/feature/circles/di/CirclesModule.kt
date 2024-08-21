1f138cf6dd96fe61ecf0921fa635ff7957a1d357
Akesi Seli <andongue.nena@gmail.com>
2024-08-21 23:57:07 +0200

    introduce a contact cache (for following users)

--- feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/di/CirclesModule.kt
+++ feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/di/CirclesModule.kt
@@ -2,12 +2,20 @@ package com.livefast.eattrash.raccoonforfriendica.feature.circles.di
 
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.detail.CircleDetailMviModel
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.detail.CircleDetailViewModel
+import com.livefast.eattrash.raccoonforfriendica.feature.circles.domain.ContactCache
+import com.livefast.eattrash.raccoonforfriendica.feature.circles.domain.DefaultContactCache
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.list.CirclesMviModel
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.list.CirclesViewModel
 import org.koin.dsl.module
 
 val featureCirclesModule =
     module {
+        single<ContactCache> {
+            DefaultContactCache(
+                identityRepository = get(),
+                userPaginationManager = get(),
+            )
+        }
         factory<CirclesMviModel> {
             CirclesViewModel(
                 circlesRepository = get(),
