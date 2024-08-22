1d1fda7902798df46b6987505dbe563f786468b5
Akesi Seli <andongue.nena@gmail.com>
2024-08-22 12:30:23 +0200

    further remove some ripple effects from cards

--- core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/ReblogInfo.kt
+++ core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/ReblogInfo.kt
@@ -1,6 +1,7 @@
 package com.livefast.eattrash.raccoonforfriendica.core.commonui.content
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,7 +40,10 @@ internal fun ReblogInfo(
 
     Row(
         modifier =
-            modifier.clickable {
+            modifier.clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+            ) {
                 if (user != null) {
                     onOpenUser?.invoke(user)
                 }
@@ -71,7 +76,10 @@ internal fun ReblogInfo(
         } else {
             PlaceholderImage(
                 modifier =
-                    Modifier.clickable {
+                    Modifier.clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                    ) {
                         if (user != null) {
                             onOpenUser?.invoke(user)
                         }
