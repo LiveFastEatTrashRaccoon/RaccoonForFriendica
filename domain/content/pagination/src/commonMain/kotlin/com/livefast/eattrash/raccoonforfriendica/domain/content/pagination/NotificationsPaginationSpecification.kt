2aca67ea3ceacd4c27568573ff7ddc2582658304
akesi seli <andongue.nena@gmail.com>
2024-08-23 19:07:45 +0200

    include polls in notifications

--- domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/NotificationsPaginationSpecification.kt
+++ domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/NotificationsPaginationSpecification.kt
@@ -2,16 +2,19 @@ package com.livefast.eattrash.raccoonforfriendica.domain.content.pagination
 
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.NotificationType
 
+private val DEFAULT_TYPES =
+    listOf(
+        NotificationType.Follow,
+        NotificationType.FollowRequest,
+        NotificationType.Mention,
+        NotificationType.Entry,
+        NotificationType.Update,
+        NotificationType.Poll,
+)
+
 sealed interface NotificationsPaginationSpecification {
     data class Default(
-        val types: List<NotificationType> =
-            listOf(
-                NotificationType.Follow,
-                NotificationType.FollowRequest,
-                NotificationType.Mention,
-                NotificationType.Entry,
-                NotificationType.Update,
-            ),
+        val types: List<NotificationType> = DEFAULT_TYPES,
         val includeNsfw: Boolean = true,
     ) : NotificationsPaginationSpecification
 }
