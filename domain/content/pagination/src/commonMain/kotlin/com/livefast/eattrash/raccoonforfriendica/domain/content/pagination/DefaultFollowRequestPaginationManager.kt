e9f8ba0602880a6710dc0af51b3961c295fc1804
akesi seli <andongue.nena@gmail.com>
2024-08-24 22:19:09 +0200

    add pagination manager for follow requests

---
+++ domain/content/pagination/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/pagination/DefaultFollowRequestPaginationManager.kt
@@ -0,0 +1,38 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.content.pagination
+
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.UserRepository
+
+internal class DefaultFollowRequestPaginationManager(
+    private val userRepository: UserRepository,
+) : FollowRequestPaginationManager {
+    private var pageCursor: String? = null
+    override var canFetchMore: Boolean = true
+    private val history = mutableListOf<UserModel>()
+
+    override suspend fun reset() {
+        pageCursor = null
+        history.clear()
+        canFetchMore = true
+    }
+
+    override suspend fun loadNextPage(): List<UserModel> {
+        val (tags, cursor) = userRepository.getFollowRequests(pageCursor)
+        val results = tags.deduplicate().updatePaginationData(cursor)
+        history.addAll(results)
+
+        // return a copy
+        return history.map { it }
+    }
+
+    private fun List<UserModel>.updatePaginationData(cursor: String?): List<UserModel> =
+        apply {
+            pageCursor = cursor
+            canFetchMore = isNotEmpty()
+        }
+
+    private fun List<UserModel>.deduplicate(): List<UserModel> =
+        filter { e1 ->
+            history.none { e2 -> e1.id == e2.id }
+        }
+}
