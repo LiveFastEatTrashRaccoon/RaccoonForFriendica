3ae06b69f564f0faa11a7d3b1f02d0dc51d151b2
akesi seli <andongue.nena@gmail.com>
2024-08-26 09:29:57 +0200

    simplify logged user observation in drawer content

--- feature/drawer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/drawer/DrawerViewModel.kt
+++ feature/drawer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/drawer/DrawerViewModel.kt
@@ -2,17 +2,15 @@ package com.livefast.eattrash.raccoonforfriendica.feature.drawer
 
 import cafe.adriel.voyager.core.model.screenModelScope
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.DefaultMviModel
-import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.UserRepository
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AccountRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.IdentityRepository
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class DrawerViewModel(
     private val apiConfigurationRepository: ApiConfigurationRepository,
-    private val accountRepository: AccountRepository,
-    private val userRepository: UserRepository,
+    private val identityRepository: IdentityRepository,
 ) : DefaultMviModel<DrawerMviModel.Intent, DrawerMviModel.State, DrawerMviModel.Effect>(
         initialState = DrawerMviModel.State(),
     ),
@@ -27,15 +25,9 @@ class DrawerViewModel(
                         )
                     }
                 }.launchIn(this)
-            accountRepository
-                .getActiveAsFlow()
-                .onEach { activeAccount ->
-                    if (activeAccount == null) {
-                        updateState { it.copy(user = null) }
-                    } else {
-                        val currentUser = userRepository.getByHandle(activeAccount.handle)
-                        updateState { it.copy(user = currentUser) }
-                    }
+            identityRepository.currentUser
+                .onEach { currentUser ->
+                    updateState { it.copy(user = currentUser) }
                 }.launchIn(this)
         }
     }
