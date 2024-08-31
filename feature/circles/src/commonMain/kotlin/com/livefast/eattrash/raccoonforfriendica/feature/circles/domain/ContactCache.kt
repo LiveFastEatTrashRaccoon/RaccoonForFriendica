ff65924bf37faa2aa3311bba91ec5c2cb6f13f06
akesi seli <andongue.nena@gmail.com>
2024-08-31 09:36:42 +0200

    remove contact cache
    which was too expensive to build!

--- feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/domain/ContactCache.kt
+++
@@ -1,9 +0,0 @@
-package com.livefast.eattrash.raccoonforfriendica.feature.circles.domain
-
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
-
-interface ContactCache {
-    fun refresh()
-
-    fun getContacts(excludeIds: List<String>): List<UserModel>
-}
