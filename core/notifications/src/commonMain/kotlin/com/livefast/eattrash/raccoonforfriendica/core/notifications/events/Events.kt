e56e0de8024b11b53a4e2fa20e2269a7458538af
akesi seli <andongue.nena@gmail.com>
2024-09-02 22:14:36 +0200

    define new event for entry and tag updates

--- core/notifications/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/notifications/events/Events.kt
+++ core/notifications/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/notifications/events/Events.kt
@@ -1,7 +1,17 @@
 package com.livefast.eattrash.raccoonforfriendica.core.notifications.events
 
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TagModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
 
 data class UserUpdatedEvent(
     val user: UserModel,
+) : NotificationCenterEvent
+
+data class TimelineEntryUpdatedEvent(
+    val entry: TimelineEntryModel,
+) : NotificationCenterEvent
+
+data class TagUpdatedEvent(
+    val tag: TagModel,
 ) : NotificationCenterEvent
