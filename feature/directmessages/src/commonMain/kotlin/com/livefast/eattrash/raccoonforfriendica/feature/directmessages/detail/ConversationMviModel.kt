58f8a594b187b6f375899952f449756ccdbd5313
akesi seli <andongue.nena@gmail.com>
2024-09-04 22:04:39 +0200

    implement conversation detail screen

---
+++ feature/directmessages/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/directmessages/detail/ConversationMviModel.kt
@@ -0,0 +1,41 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.directmessages.detail
+
+import androidx.compose.ui.text.input.TextFieldValue
+import cafe.adriel.voyager.core.model.ScreenModel
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.DirectMessageModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
+
+interface ConversationMviModel :
+    ScreenModel,
+    MviModel<ConversationMviModel.Intent, ConversationMviModel.State, ConversationMviModel.Effect> {
+    sealed interface Intent {
+        data object Refresh : Intent
+
+        data object LoadNextPage : Intent
+
+        data class SetNewMessageValue(
+            val value: TextFieldValue,
+        ) : Intent
+
+        data object Submit : Intent
+    }
+
+    data class State(
+        val otherUser: UserModel? = null,
+        val currentUser: UserModel? = null,
+        val refreshing: Boolean = false,
+        val loading: Boolean = false,
+        val sendInProgress: Boolean = false,
+        val initial: Boolean = true,
+        val canFetchMore: Boolean = true,
+        val items: List<DirectMessageModel> = emptyList(),
+        val newMessageValue: TextFieldValue = TextFieldValue(),
+    )
+
+    sealed interface Effect {
+        data object BackToTop : Effect
+
+        data object Failure : Effect
+    }
+}
--- feature/directmessages/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/directmessages/di/DirectMessagesModule.kt
+++ feature/directmessages/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/directmessages/di/DirectMessagesModule.kt
@@ -1,5 +1,7 @@
 package com.livefast.eattrash.raccoonforfriendica.feature.directmessages.di
 
+import com.livefast.eattrash.raccoonforfriendica.feature.directmessages.detail.ConversationMviModel
+import com.livefast.eattrash.raccoonforfriendica.feature.directmessages.detail.ConversationViewModel
 import com.livefast.eattrash.raccoonforfriendica.feature.directmessages.list.DirectMessageListMviModel
 import com.livefast.eattrash.raccoonforfriendica.feature.directmessages.list.DirectMessageListViewModel
 import org.koin.dsl.module
@@ -11,6 +13,16 @@ val featureDirectMessagesModule =
                 paginationManager = get(),
                 identityRepository = get(),
                 userPaginationManager = get(),
+            )
+        }
+        factory<ConversationMviModel> { params ->
+            ConversationViewModel(
+                otherUserId = params[0],
+                parentUri = params[1],
+                paginationManager = get(),
+                userRepository = get(),
+                identityRepository = get(),
+                messageRepository = get(),
             )
         }
     }
