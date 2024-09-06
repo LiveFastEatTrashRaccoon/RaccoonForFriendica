3b5c491c92ffef7a2c93abe70195ca7ff276957d
akesi seli <andongue.nena@gmail.com>
2024-09-06 08:43:18 +0200

    fix loading image placeholder

--- feature/gallery/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/gallery/components/AlbumImageItem.kt
+++ feature/gallery/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/gallery/components/AlbumImageItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -123,12 +124,16 @@ internal fun AlbumImageItem(
         }
 
         if (attachment.loading) {
-            CircularProgressIndicator(
-                modifier =
-                    Modifier
-                        .size(IconSize.l)
-                        .align(Alignment.Center),
-            )
+            Box(
+                modifier = Modifier.fillMaxWidth().aspectRatio(3.5f),
+            ) {
+                CircularProgressIndicator(
+                    modifier =
+                        Modifier
+                            .size(IconSize.l)
+                            .align(Alignment.Center),
+                )
+            }
         }
 
         Row(
