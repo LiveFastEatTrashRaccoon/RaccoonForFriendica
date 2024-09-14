a0d1684c96554bac601fdfe429ce60ccae456289
akesi seli <andongue.nena@gmail.com>
2024-09-14 11:53:02 +0200

    add missing date utils

--- core/utils/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/datetime/DateFunctions.kt
+++ core/utils/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/datetime/DateFunctions.kt
@@ -23,25 +23,80 @@ import kotlin.time.toDuration
 
 actual fun epochMillis(): Long = (NSDate().timeIntervalSince1970 * 1000).toLong()
 
-actual fun Long.toIso8601Timestamp(): String? {
-    val dateFormatter = NSDateFormatter()
-    dateFormatter.locale = NSLocale.autoupdatingCurrentLocale
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"
-    dateFormatter.calendar = NSCalendar(calendarIdentifier = NSCalendarIdentifierGregorian)
+private fun getDateFormatter(format: String) =
+    NSDateFormatter().apply {
+        locale = NSLocale.autoupdatingCurrentLocale
+        dateFormat = format
+        calendar = NSCalendar(calendarIdentifier = NSCalendarIdentifierGregorian)
+    }
+
+private val defaultFormatter = getDateFormatter("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+private val backupFormatter = getDateFormatter("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+private fun String.tryParse(): NSDate? {
+    var res = runCatching { defaultFormatter.dateFromString(this) }.getOrNull()
+    if (res == null) {
+        res = runCatching { backupFormatter.dateFromString(this) }.getOrNull()
+    }
+    return res
+}
+
+private fun getDateFromIso8601Timestamp(string: String): NSDate? = NSISO8601DateFormatter().dateFromString(string)
+
+actual fun Long.toIso8601Timestamp(withLocalTimezone: Boolean): String? {
     val date = NSDate(timeIntervalSinceReferenceDate = (this.toDouble() / 1000))
-    return dateFormatter.stringFromDate(date)
+    return defaultFormatter
+        .apply {
+            if (withLocalTimezone) {
+                timeZone = NSTimeZone.localTimeZone
+            }
+        }.stringFromDate(date)
+}
+
+actual fun String.toEpochMillis(): Long {
+    val date = tryParse() ?: return 0
+    return (date.timeIntervalSince1970 * 1000).toLong()
+}
+
+actual fun Long.concatDateWithTime(
+    hours: Int,
+    minutes: Int,
+    seconds: Int,
+): Long {
+    val date = NSDate(timeIntervalSinceReferenceDate = (this.toDouble() / 1000))
+    val calendar = NSCalendar(calendarIdentifier = NSCalendarIdentifierGregorian)
+    val dateComponents =
+        calendar.components(
+            unitFlags =
+                NSCalendarUnitSecond
+                    .or(NSCalendarUnitMinute)
+                    .or(NSCalendarUnitHour)
+                    .or(NSCalendarUnitDay)
+                    .or(NSCalendarUnitMonth)
+                    .or(NSCalendarUnitYear),
+            fromDate = date,
+        )
+    dateComponents.hour = hours.toLong()
+    dateComponents.minute = minutes.toLong()
+    dateComponents.second = seconds.toLong()
+
+    val result = calendar.dateFromComponents(dateComponents) ?: return 0
+    return (result.timeIntervalSince1970 * 1000).toLong()
 }
 
 actual fun getFormattedDate(
     iso8601Timestamp: String,
     format: String,
+    withLocalTimezone: Boolean,
 ): String {
     val date = getDateFromIso8601Timestamp(iso8601Timestamp) ?: return ""
-
-    val dateFormatter = NSDateFormatter()
-    dateFormatter.timeZone = NSTimeZone.localTimeZone
-    dateFormatter.locale = NSLocale.autoupdatingCurrentLocale
-    dateFormatter.dateFormat = format
+    val dateFormatter =
+        getDateFormatter(format).run {
+            if (withLocalTimezone) {
+                timeZone = NSTimeZone.localTimeZone
+            }
+            this
+        }
     return dateFormatter.stringFromDate(date)
 }
 
@@ -49,12 +104,11 @@ actual fun parseDate(
     value: String,
     format: String,
 ): String {
-    val dateFormatter = NSDateFormatter()
-    dateFormatter.timeZone = NSTimeZone.localTimeZone
-    dateFormatter.locale = NSLocale.autoupdatingCurrentLocale
-    dateFormatter.dateFormat = format
-
-    val date = dateFormatter.dateFromString(value) ?: return ""
+    val dateFormatter = getDateFormatter(format)
+    val date =
+        runCatching {
+            dateFormatter.dateFromString(value)
+        }.getOrNull() ?: return ""
     return NSISO8601DateFormatter().stringFromDate(date)
 }
 
@@ -137,4 +191,32 @@ actual fun getDurationFromDateToNow(iso8601Timestamp: String): Duration? {
     return interval.toDuration(DurationUnit.SECONDS)
 }
 
-private fun getDateFromIso8601Timestamp(string: String): NSDate? = NSISO8601DateFormatter().dateFromString(string)
+actual fun isToday(iso8601Timestamp: String): Boolean {
+    val date = iso8601Timestamp.tryParse() ?: return false
+    val calendar = NSCalendar(calendarIdentifier = NSCalendarIdentifierGregorian)
+    val dateComponents =
+        calendar.components(
+            unitFlags =
+                NSCalendarUnitDay
+                    .or(NSCalendarUnitMonth)
+                    .or(NSCalendarUnitYear),
+            fromDate = date,
+        )
+    val year = dateComponents.year
+    val month = dateComponents.month
+    val day = dateComponents.day
+
+    val componentsNow =
+        calendar.components(
+            unitFlags =
+                NSCalendarUnitDay
+                    .or(NSCalendarUnitMonth)
+                    .or(NSCalendarUnitYear),
+            fromDate = NSDate(),
+        )
+    val yearNow = componentsNow.year
+    val monthNow = componentsNow.month
+    val dayNow = componentsNow.day
+
+    return year == yearNow && month == monthNow && day == dayNow
+}
