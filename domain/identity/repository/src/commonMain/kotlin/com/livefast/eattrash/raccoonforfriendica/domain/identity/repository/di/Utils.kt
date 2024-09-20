8eb8c0d1c403d92817243cef9e7ec680f16edd21
akesi seli <andongue.nena@gmail.com>
2024-09-20 16:13:37 +0200

    introduce entry action repository

--- domain/identity/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/repository/di/Utils.kt
+++ domain/identity/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/repository/di/Utils.kt
@@ -2,6 +2,7 @@ package com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.di
 
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AuthManager
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.EntryActionRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.SettingsRepository
 
 expect fun getApiConfigurationRepository(): ApiConfigurationRepository
@@ -9,3 +10,5 @@ expect fun getApiConfigurationRepository(): ApiConfigurationRepository
 expect fun getSettingsRepository(): SettingsRepository
 
 expect fun getAuthManager(): AuthManager
+
+expect fun getEntryActionRepository(): EntryActionRepository
