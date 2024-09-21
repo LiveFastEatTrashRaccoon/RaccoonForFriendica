923be867f4134a6d3ee71aad5495cf28a875df83
akesi seli <andongue.nena@gmail.com>
2024-09-22 00:02:26 +0200

    update features every time the user/node changes

--- domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/DefaultActiveAccountMonitor.kt
+++ domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/DefaultActiveAccountMonitor.kt
@@ -1,5 +1,6 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.identity.usecase
 
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.SupportedFeatureRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.data.AccountModel
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.data.SettingsModel
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AccountCredentialsCache
@@ -23,6 +24,7 @@ internal class DefaultActiveAccountMonitor(
     private val identityRepository: IdentityRepository,
     private val accountCredentialsCache: AccountCredentialsCache,
     private val settingsRepository: SettingsRepository,
+    private val supportedFeatureRepository: SupportedFeatureRepository,
     coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ActiveAccountMonitor {
     private val scope = CoroutineScope(SupervisorJob() + coroutineDispatcher)
@@ -53,8 +55,10 @@ internal class DefaultActiveAccountMonitor(
         if (account == null) {
             apiConfigurationRepository.setAuth(null)
             identityRepository.refreshCurrentUser(null)
+            supportedFeatureRepository.refresh()
             return
         }
+
         val node =
             account.handle.substringAfter("@").takeIf { it.isNotEmpty() }
                 ?: apiConfigurationRepository.defaultNode
@@ -63,6 +67,7 @@ internal class DefaultActiveAccountMonitor(
         apiConfigurationRepository.setAuth(credentials)
 
         identityRepository.refreshCurrentUser(account.remoteId)
+        supportedFeatureRepository.refresh()
 
         val defaultSettings = settingsRepository.get(account.id) ?: SettingsModel()
         settingsRepository.changeCurrent(defaultSettings)
