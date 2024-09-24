6bde0214857eac768799e3d3f9603ca5a46032d4
akesi seli <andongue.nena@gmail.com>
2024-09-24 18:35:56 +0200

    define EmojiRepository

--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/InstanceService.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/InstanceService.kt
@@ -1,5 +1,6 @@
 package com.livefast.eattrash.raccoonforfriendica.core.api.service
 
+import com.livefast.eattrash.raccoonforfriendica.core.api.dto.CustomEmoji
 import com.livefast.eattrash.raccoonforfriendica.core.api.dto.Instance
 import com.livefast.eattrash.raccoonforfriendica.core.api.dto.InstanceRule
 import de.jensklingenberg.ktorfit.http.GET
@@ -10,4 +11,7 @@ interface InstanceService {
 
     @GET("v1/instance/rules")
     suspend fun getRules(): List<InstanceRule>
+
+    @GET("v1/custom_emojis")
+    suspend fun getCustomEmojis(): List<CustomEmoji>
 }
