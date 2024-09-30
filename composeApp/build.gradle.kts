fd8e426f9697c74e89988dfc19ea0320cea8ab40
akesi seli <andongue.nena@gmail.com>
2024-09-30 20:43:34 +0200

    version 0.1.0-beta03

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -129,8 +129,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 31
-        versionName = "0.1.0-beta02"
+        versionCode = 32
+        versionName = "0.1.0-beta03"
     }
     base.archivesName = "RaccoonForFriendica"
     packaging {
