87268d21f45f910d88457587aa1b464357208938
akesi seli <andongue.nena@gmail.com>
2024-09-30 19:17:19 +0200

    use default notification types in unread check

--- domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultInboxManager.kt
+++ domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultInboxManager.kt
@@ -1,6 +1,5 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.repository
 
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.NotificationType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.flow.update
@@ -11,15 +10,7 @@ internal class DefaultInboxManager(
     override val unreadCount = MutableStateFlow(0)
 
     override suspend fun refreshUnreadCount() {
-        val types =
-            listOf(
-                NotificationType.Follow,
-                NotificationType.FollowRequest,
-                NotificationType.Mention,
-                NotificationType.Entry,
-                NotificationType.Update,
-            )
-        val notifications = notificationRepository.getAll(types)
+        val notifications = notificationRepository.getAll()
         unreadCount.update {
             notifications?.count { !it.read } ?: 0
         }
