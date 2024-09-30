027ab9d02d18901df45cd11972c8b810a5085f47
akesi seli <andongue.nena@gmail.com>
2024-09-30 09:06:40 +0200

    remove helper methods from EmojiRepository

--- domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultEmojiRepository.kt
+++ domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultEmojiRepository.kt
@@ -2,10 +2,7 @@ package com.livefast.eattrash.raccoonforfriendica.domain.content.repository
 
 import com.livefast.eattrash.raccoonforfriendica.core.api.provider.ServiceProvider
 import com.livefast.eattrash.raccoonforfriendica.core.utils.cache.LruCache
-import com.livefast.eattrash.raccoonforfriendica.core.utils.nodeName
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.EmojiModel
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.utils.toModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -17,9 +14,12 @@ internal class DefaultEmojiRepository(
 ) : EmojiRepository {
     private val cache = LruCache<String, List<EmojiModel>>(20)
 
-    override suspend fun getAll(node: String?): List<EmojiModel>? {
+    override suspend fun getAll(
+        node: String?,
+        refresh: Boolean,
+    ): List<EmojiModel>? {
         val key = node.orEmpty()
-        return if (cache.containsKey(key)) {
+        return if (cache.containsKey(key) && !refresh) {
             cache.get(key)
         } else {
             retrieve(node).orEmpty().also {
@@ -27,34 +27,6 @@ internal class DefaultEmojiRepository(
             }
         }
     }
-
-    override suspend fun UserModel.withEmojisIfMissing(): UserModel {
-        if (emojis.isNotEmpty()) {
-            return this
-        }
-        val texts =
-            arrayOf(
-                displayName.orEmpty(),
-                bio.orEmpty(),
-            )
-        if (texts.none { it.contains(EMOJI_REGEX) }) {
-            return this
-        }
-
-        val node = handle.nodeName
-        val emojis = getAll(node)?.filterContainedIn(*texts).orEmpty()
-        return copy(emojis = emojis)
-    }
-
-    override suspend fun TimelineEntryModel.withEmojisIfMissing(): TimelineEntryModel =
-        copy(
-            creator = creator?.withEmojisIfMissing(),
-            inReplyTo =
-                inReplyTo?.copy(
-                    creator = inReplyTo?.creator?.withEmojisIfMissing(),
-                ),
-            reblog = reblog?.copy(reblog = null)?.withEmojisIfMissing(),
-        )
 
     private suspend fun retrieve(node: String?): List<EmojiModel>? =
         withContext(Dispatchers.IO) {
@@ -69,14 +41,4 @@ internal class DefaultEmojiRepository(
                 res.map { it.toModel() }
             }.getOrElse { null }
         }
-
-    companion object {
-        private val EMOJI_REGEX = Regex(":\\w+:")
-    }
 }
-
-private fun List<EmojiModel>.filterContainedIn(vararg texts: String): List<EmojiModel> =
-    filter { emoji ->
-        val occurrence = ":${emoji.code}:"
-        texts.any { it.contains(occurrence) }
-    }
