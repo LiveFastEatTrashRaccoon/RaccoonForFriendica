75e5bd64288a17d8ce2e5a1ff0b58214ac4e0a63
akesi seli <andongue.nena@gmail.com>
2024-10-06 11:39:02 +0200

    remove unnecessary request headers from StatusService

--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/StatusService.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/StatusService.kt
@@ -41,43 +41,36 @@ interface StatusService {
     ): Status
 
     @POST("v1/statuses/{id}/unreblog")
-    @Headers("Content-Type: application/json")
     suspend fun unreblog(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/pin")
-    @Headers("Content-Type: application/json")
     suspend fun pin(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/unpin")
-    @Headers("Content-Type: application/json")
     suspend fun unpin(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/bookmark")
-    @Headers("Content-Type: application/json")
     suspend fun bookmark(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/unbookmark")
-    @Headers("Content-Type: application/json")
     suspend fun unbookmark(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/favourite")
-    @Headers("Content-Type: application/json")
     suspend fun favorite(
         @Path("id") id: String,
     ): Status
 
     @POST("v1/statuses/{id}/unfavourite")
-    @Headers("Content-Type: application/json")
     suspend fun unfavorite(
         @Path("id") id: String,
     ): Status
