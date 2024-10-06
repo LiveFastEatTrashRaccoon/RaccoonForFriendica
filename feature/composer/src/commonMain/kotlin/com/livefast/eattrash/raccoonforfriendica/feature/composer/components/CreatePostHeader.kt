bafda2d270a26dd4b9c657ee0929372ff48aed7f
akesi seli <andongue.nena@gmail.com>
2024-10-06 17:33:01 +0200

    align composer post header with timeline header

--- feature/composer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/composer/components/CreatePostHeader.kt
+++ feature/composer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/composer/components/CreatePostHeader.kt
@@ -35,7 +35,7 @@ import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.ancillary
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.CustomDropDown
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.CustomImage
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.PlaceholderImage
-import com.livefast.eattrash.raccoonforfriendica.core.utils.ellipsize
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.TextWithCustomEmojis
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.Visibility
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toIcon
@@ -62,7 +62,7 @@ internal fun CreatePostHeader(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(Spacing.s),
     ) {
-        val creatorName = (author?.displayName ?: author?.username ?: "").ellipsize(30)
+        val creatorName = author?.displayName ?: author?.username ?: ""
         val creatorAvatar = author?.avatar.orEmpty()
         val iconSize = IconSize.l
         if (creatorAvatar.isNotEmpty()) {
@@ -86,8 +86,9 @@ internal fun CreatePostHeader(
         Column(
             modifier = Modifier.weight(1f),
         ) {
-            Text(
+            TextWithCustomEmojis(
                 text = creatorName,
+                emojis = author?.emojis.orEmpty(),
                 style = MaterialTheme.typography.bodyMedium,
                 color = fullColor,
             )
