38b956095e6749da0c01adf0bd96850e61d2abf2
akesi seli <andongue.nena@gmail.com>
2024-10-11 14:10:35 +0200

    chore: add test for active account monitor (#323)
    * update deps
    
    * refactor: improve conditional readability
    
    * add test for DefaultActiveAccountMonitor

---
+++ domain/identity/usecase/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/DefaultActiveAccountMonitorTest.kt
@@ -0,0 +1,104 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.identity.usecase
+
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toTimelineType
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.InboxManager
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.SupportedFeatureRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.data.AccountModel
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.data.SettingsModel
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AccountCredentialsCache
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AccountRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiCredentials
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.IdentityRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.SettingsRepository
+import dev.mokkery.MockMode
+import dev.mokkery.answering.returns
+import dev.mokkery.every
+import dev.mokkery.everySuspend
+import dev.mokkery.matcher.any
+import dev.mokkery.mock
+import dev.mokkery.verify
+import dev.mokkery.verifySuspend
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+
+class DefaultActiveAccountMonitorTest {
+    private val accountRepository = mock<AccountRepository>(mode = MockMode.autoUnit)
+    private val apiConfigurationRepository =
+        mock<ApiConfigurationRepository>(mode = MockMode.autoUnit)
+    private val identityRepository = mock<IdentityRepository>(mode = MockMode.autoUnit)
+    private val accountCredentialsCache = mock<AccountCredentialsCache>(mode = MockMode.autoUnit)
+    private val settingsRepository = mock<SettingsRepository>(mode = MockMode.autoUnit)
+    private val supportedFeatureRepository =
+        mock<SupportedFeatureRepository>(mode = MockMode.autoUnit)
+    private val inboxManager = mock<InboxManager>(mode = MockMode.autoUnit)
+    private val contentPreloadManager = mock<ContentPreloadManager>(mode = MockMode.autoUnit)
+    private val sut =
+        DefaultActiveAccountMonitor(
+            accountRepository = accountRepository,
+            apiConfigurationRepository = apiConfigurationRepository,
+            identityRepository = identityRepository,
+            accountCredentialsCache = accountCredentialsCache,
+            settingsRepository = settingsRepository,
+            supportedFeatureRepository = supportedFeatureRepository,
+            inboxManager = inboxManager,
+            contentPreloadManager = contentPreloadManager,
+        )
+
+    @Test
+    fun `given no active account when started then interactions are as expected`() =
+        runTest {
+            val account: AccountModel? = null
+            val accountChannel = Channel<AccountModel?>()
+            every { accountRepository.getActiveAsFlow() } returns accountChannel.receiveAsFlow()
+            everySuspend { accountRepository.getActive() } returns account
+            every { apiConfigurationRepository.node } returns MutableStateFlow("test-instance")
+
+            sut.start()
+            accountChannel.send(account)
+
+            verify {
+                apiConfigurationRepository.setAuth(null)
+            }
+            verifySuspend {
+                identityRepository.refreshCurrentUser(null)
+                supportedFeatureRepository.refresh()
+                contentPreloadManager.preload()
+            }
+        }
+
+    @Test
+    fun `given active account when started then interactions are as expected`() =
+        runTest {
+            val accountId = 1L
+            val userId = "fake-user-id"
+            val account =
+                AccountModel(id = accountId, handle = "user@example.com", remoteId = userId)
+            val accountChannel = Channel<AccountModel?>()
+            every { accountRepository.getActiveAsFlow() } returns accountChannel.receiveAsFlow()
+            everySuspend { accountRepository.getActive() } returns account
+            every { apiConfigurationRepository.node } returns MutableStateFlow("test-instance")
+            val credentials = ApiCredentials.OAuth2("fake-access-token", "")
+            every { accountCredentialsCache.get(any()) } returns credentials
+            val settings = SettingsModel()
+            everySuspend { settingsRepository.get(any()) } returns settings
+
+            sut.start()
+            accountChannel.send(account)
+
+            verify {
+                apiConfigurationRepository.changeNode("example.com")
+                apiConfigurationRepository.setAuth(credentials)
+            }
+            verifySuspend {
+                contentPreloadManager.preload(userId, settings.defaultTimelineType.toTimelineType())
+                identityRepository.refreshCurrentUser(userId)
+                supportedFeatureRepository.refresh()
+                settingsRepository.changeCurrent(settings)
+                inboxManager.refreshUnreadCount()
+            }
+        }
+}
