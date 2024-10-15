563f8dc2f479ac7436553094e9459ae4c3a51adb
akesi seli <andongue.nena@gmail.com>
2024-10-15 22:01:57 +0200

    fix service definition

--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/MarkerService.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/MarkerService.kt
@@ -4,11 +4,14 @@ import com.livefast.eattrash.raccoonforfriendica.core.api.dto.Markers
 import de.jensklingenberg.ktorfit.http.Body
 import de.jensklingenberg.ktorfit.http.GET
 import de.jensklingenberg.ktorfit.http.POST
+import de.jensklingenberg.ktorfit.http.Query
 import io.ktor.client.request.forms.FormDataContent
 
 interface MarkerService {
     @GET("v1/markers")
-    suspend fun get(): Markers
+    suspend fun get(
+        @Query("timeline[]") timelines: List<String>,
+    ): Markers
 
     @POST("v1/markers")
     suspend fun update(
