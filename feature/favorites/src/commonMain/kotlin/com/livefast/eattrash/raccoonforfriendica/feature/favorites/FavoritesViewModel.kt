2f38a283076e8746d4884fc0602d0d3fab1cbe06
akesi seli <andongue.nena@gmail.com>
2024-10-17 23:02:40 +0200

    add copy action to favorites

--- feature/favorites/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/favorites/FavoritesViewModel.kt
+++ feature/favorites/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/favorites/FavoritesViewModel.kt
@@ -92,6 +92,7 @@ class FavoritesViewModel(
             is FavoritesMviModel.Intent.TogglePin -> togglePin(intent.entry)
             is FavoritesMviModel.Intent.SubmitPollVote -> submitPoll(intent.entry, intent.choices)
             is FavoritesMviModel.Intent.ToggleSpoilerActive -> toggleSpoiler(intent.entry)
+            is FavoritesMviModel.Intent.CopyToClipboard -> copyToClipboard(intent.entry)
         }
     }
 
@@ -383,6 +384,23 @@ class FavoritesViewModel(
     private fun toggleSpoiler(entry: TimelineEntryModel) {
         screenModelScope.launch {
             updateEntryInState(entry.id) { entry.copy(isSpoilerActive = !entry.isSpoilerActive) }
+        }
+    }
+
+    private fun copyToClipboard(entry: TimelineEntryModel) {
+        screenModelScope.launch {
+            val source = timelineEntryRepository.getSource(entry.id)
+            if (source != null) {
+                val text =
+                    buildString {
+                        if (!entry.title.isNullOrBlank()) {
+                            append(entry.title)
+                            append("\n")
+                        }
+                        append(source.content)
+                    }
+                emitEffect(FavoritesMviModel.Effect.TriggerCopy(text))
+            }
         }
     }
 }
