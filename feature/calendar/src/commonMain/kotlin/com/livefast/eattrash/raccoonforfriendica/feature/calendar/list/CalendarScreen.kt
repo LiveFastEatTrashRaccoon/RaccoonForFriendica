3408e9045523e8e3ed3594c8f2e7936f64404343
akesi seli <andongue.nena@gmail.com>
2024-10-19 23:14:24 +0200

    add calendar event list

---
+++ feature/calendar/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/calendar/list/CalendarScreen.kt
@@ -0,0 +1,262 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.calendar.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.getScreenModel
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.toWindowInsets
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.ListLoadingIndicator
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.OptionId
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.toOption
+import com.livefast.eattrash.raccoonforfriendica.core.l10n.messages.LocalStrings
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getDetailOpener
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getNavigationCoordinator
+import com.livefast.eattrash.raccoonforfriendica.core.utils.datetime.toEpochMillis
+import com.livefast.eattrash.raccoonforfriendica.core.utils.di.getCalendarHelper
+import com.livefast.eattrash.raccoonforfriendica.core.utils.isNearTheEnd
+import com.livefast.eattrash.raccoonforfriendica.feature.calendar.composables.CalendarHeader
+import com.livefast.eattrash.raccoonforfriendica.feature.calendar.composables.CalendarRow
+import com.livefast.eattrash.raccoonforfriendica.feature.calendar.composables.CalendarRowPlaceholder
+import kotlinx.coroutines.launch
+
+class CalendarScreen : Screen {
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+    @Composable
+    override fun Content() {
+        val model = getScreenModel<CalendarMviModel>()
+        val uiState by model.uiState.collectAsState()
+        val topAppBarState = rememberTopAppBarState()
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+        val uriHandler = LocalUriHandler.current
+        val detailOpener = getDetailOpener()
+        val navigationCoordinator = remember { getNavigationCoordinator() }
+        val lazyListState = rememberLazyListState()
+        val scope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
+        val calendarHelper = getCalendarHelper()
+
+        fun goBackToTop() {
+            runCatching {
+                scope.launch {
+                    lazyListState.scrollToItem(0)
+                    topAppBarState.heightOffset = 0f
+                    topAppBarState.contentOffset = 0f
+                }
+            }
+        }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier.clickable { scope.launch { goBackToTop() } },
+                    windowInsets = topAppBarState.toWindowInsets(),
+                    scrollBehavior = scrollBehavior,
+                    title = {
+                        Text(
+                            text = LocalStrings.current.calendarTitle,
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    },
+                    navigationIcon = {
+                        if (navigationCoordinator.canPop.value) {
+                            IconButton(
+                                onClick = {
+                                    navigationCoordinator.pop()
+                                },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                    contentDescription = null,
+                                )
+                            }
+                        }
+                    },
+                )
+            },
+            snackbarHost = {
+                SnackbarHost(
+                    hostState = snackbarHostState,
+                ) { data ->
+                    Snackbar(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        snackbarData = data,
+                    )
+                }
+            },
+        ) { padding ->
+            val pullRefreshState =
+                rememberPullRefreshState(
+                    refreshing = uiState.refreshing,
+                    onRefresh = {
+                        model.reduce(CalendarMviModel.Intent.Refresh)
+                    },
+                )
+            Box(
+                modifier =
+                    Modifier
+                        .padding(padding)
+                        .fillMaxWidth()
+                        .nestedScroll(scrollBehavior.nestedScrollConnection)
+                        .pullRefresh(pullRefreshState),
+            ) {
+                LazyColumn(
+                    state = lazyListState,
+                ) {
+                    if (uiState.initial) {
+                        val placeholderCount = 20
+                        items(placeholderCount) { idx ->
+                            CalendarRowPlaceholder(modifier = Modifier.fillMaxWidth())
+                            if (idx < placeholderCount - 1) {
+                                HorizontalDivider(
+                                    modifier = Modifier.padding(vertical = Spacing.s),
+                                )
+                            }
+                        }
+                    }
+                    itemsIndexed(
+                        items = uiState.items,
+                        key = { _, item ->
+                            when (item) {
+                                is CalendarItem.EventItem -> item.event.id
+                                is CalendarItem.Header -> "${item.year}-${item.month}"
+                            }
+                        },
+                    ) { idx, item ->
+                        when (item) {
+                            is CalendarItem.Header ->
+                                CalendarHeader(
+                                    modifier =
+                                        Modifier.fillMaxWidth().padding(
+                                            top = if (idx == 0) 0.dp else Spacing.m,
+                                            bottom = Spacing.s,
+                                        ),
+                                    header = item,
+                                )
+
+                            is CalendarItem.EventItem -> {
+                                CalendarRow(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    event = item.event,
+                                    onOpenUrl = { url ->
+                                        uriHandler.openUri(url)
+                                    },
+                                    onClick = {
+                                        detailOpener.openEvent(event = item.event)
+                                    },
+                                    options =
+                                        buildList {
+                                            if (calendarHelper.supportsExport) {
+                                                this += SaveToCalendar.toOption(LocalStrings.current.actionSaveToCalendar)
+                                            }
+                                        },
+                                    onOptionSelected = { optionId ->
+                                        when (optionId) {
+                                            SaveToCalendar ->
+                                                with(item.event) {
+                                                    calendarHelper.export(
+                                                        title = title,
+                                                        startDate = startTime.toEpochMillis(),
+                                                        endDate = endTime?.toEpochMillis(),
+                                                        location = place,
+                                                    )
+                                                }
+
+                                            else -> Unit
+                                        }
+                                    },
+                                )
+                                if (idx < uiState.items.lastIndex && uiState.items[idx + 1] !is CalendarItem.Header) {
+                                    HorizontalDivider(
+                                        modifier = Modifier.padding(vertical = Spacing.s),
+                                    )
+                                }
+                            }
+                        }
+
+                        val canFetchMore =
+                            !uiState.initial && !uiState.loading && uiState.canFetchMore
+                        val isNearTheEnd = idx.isNearTheEnd(uiState.items)
+                        if (isNearTheEnd && canFetchMore) {
+                            model.reduce(CalendarMviModel.Intent.LoadNextPage)
+                        }
+                    }
+
+                    item {
+                        if (uiState.loading && !uiState.refreshing) {
+                            Box(
+                                modifier = Modifier.fillMaxWidth(),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                ListLoadingIndicator()
+                            }
+                        }
+                    }
+
+                    if (!uiState.initial && !uiState.refreshing && !uiState.loading && uiState.items.isEmpty()) {
+                        item {
+                            Text(
+                                modifier = Modifier.fillMaxWidth().padding(top = Spacing.m),
+                                text = LocalStrings.current.messageEmptyList,
+                                textAlign = TextAlign.Center,
+                                style = MaterialTheme.typography.bodyLarge,
+                            )
+                        }
+                    }
+
+                    item {
+                        Spacer(modifier = Modifier.height(Spacing.xxxl))
+                    }
+                }
+
+                PullRefreshIndicator(
+                    refreshing = uiState.refreshing,
+                    state = pullRefreshState,
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    backgroundColor = MaterialTheme.colorScheme.surfaceContainerHighest,
+                    contentColor = MaterialTheme.colorScheme.primary,
+                )
+            }
+        }
+    }
+}
+
+private object SaveToCalendar : OptionId.Custom
---
+++ feature/calendar/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/calendar/list/CalendarViewModel.kt
@@ -0,0 +1,94 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.calendar.list
+
+import cafe.adriel.voyager.core.model.screenModelScope
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.DefaultMviModel
+import com.livefast.eattrash.raccoonforfriendica.core.utils.datetime.extractDatePart
+import com.livefast.eattrash.raccoonforfriendica.core.utils.datetime.toEpochMillis
+import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.EventPaginationManager
+import com.livefast.eattrash.raccoonforfriendica.domain.content.pagination.EventsPaginationSpecification
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.IdentityRepository
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class CalendarViewModel(
+    private val identityRepository: IdentityRepository,
+    private val paginationManager: EventPaginationManager,
+) : DefaultMviModel<CalendarMviModel.Intent, CalendarMviModel.State, CalendarMviModel.Effect>(
+        initialState = CalendarMviModel.State(),
+    ),
+    CalendarMviModel {
+    init {
+        screenModelScope.launch {
+            identityRepository.currentUser
+                .onEach {
+                    refresh(initial = true)
+                }.launchIn(this)
+        }
+    }
+
+    override fun reduce(intent: CalendarMviModel.Intent) {
+        when (intent) {
+            CalendarMviModel.Intent.LoadNextPage ->
+                screenModelScope.launch {
+                    loadNextPage()
+                }
+
+            CalendarMviModel.Intent.Refresh ->
+                screenModelScope.launch {
+                    refresh()
+                }
+        }
+    }
+
+    private suspend fun refresh(initial: Boolean = false) {
+        updateState {
+            it.copy(initial = initial, refreshing = !initial)
+        }
+        paginationManager.reset(EventsPaginationSpecification.All)
+        loadNextPage()
+    }
+
+    private suspend fun loadNextPage() {
+        if (uiState.value.loading) {
+            return
+        }
+        val wasRefreshing = uiState.value.refreshing
+        updateState { it.copy(loading = true) }
+        val events = paginationManager.loadNextPage()
+        val eventsGrouped =
+            events.groupBy {
+                val timestamp = it.startTime.toEpochMillis()
+                val (year, month) = timestamp.extractDatePart()
+                CalendarItem.Header(year = year, month = month)
+            }
+        val keys =
+            eventsGrouped.keys.sortedByDescending {
+                val monthPart = "${it.month}".padStart(2, '0')
+                "${it.year}-$monthPart"
+            }
+        val items =
+            buildList {
+                for (key in keys) {
+                    this += key
+                    val monthEvents =
+                        eventsGrouped[key]?.sortedByDescending { it.startTime }.orEmpty()
+                    for (event in monthEvents) {
+                        this += CalendarItem.EventItem(event)
+                    }
+                }
+            }
+        updateState {
+            it.copy(
+                items = items,
+                canFetchMore = paginationManager.canFetchMore,
+                loading = false,
+                initial = false,
+                refreshing = false,
+            )
+        }
+        if (wasRefreshing) {
+            emitEffect(CalendarMviModel.Effect.BackToTop)
+        }
+    }
+}
