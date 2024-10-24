715ce3257e43664a14c5ce41bfe46c39ae33f27d
akesi seli <andongue.nena@gmail.com>
2024-10-24 21:46:20 +0200

    remove section to/from int mapping

--- core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/UserSection.kt
+++ core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/UserSection.kt
@@ -13,22 +13,6 @@ sealed interface UserSection {
     data object Media : UserSection
 }
 
-fun UserSection.toInt(): Int =
-    when (this) {
-        UserSection.Posts -> 0
-        UserSection.All -> 1
-        UserSection.Pinned -> 2
-        UserSection.Media -> 3
-    }
-
-fun Int.toAccountSection(): UserSection =
-    when (this) {
-        1 -> UserSection.All
-        2 -> UserSection.Pinned
-        3 -> UserSection.Media
-        else -> UserSection.Posts
-    }
-
 @Composable
 fun UserSection.toReadableName(): String =
     when (this) {
