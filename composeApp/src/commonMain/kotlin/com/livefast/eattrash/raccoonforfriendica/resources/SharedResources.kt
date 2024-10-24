e969388150a8cd964055467e6c94a826e9ae357e
akesi seli <andongue.nena@gmail.com>
2024-10-24 20:43:34 +0200

    feat: add atkinson hyperlegible font (#392)
    * add resources
    
    * add map to font family
    
    * add option in settings
    
    * add bug report icon in about dialog
    
    * fixes in pl l10n
    
    * fixes in pt l10n

--- composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/resources/SharedResources.kt
+++ composeApp/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/resources/SharedResources.kt
@@ -7,6 +7,9 @@ import androidx.compose.ui.text.font.FontWeight
 import com.livefast.eattrash.raccoonforfriendica.core.resources.CoreResources
 import org.jetbrains.compose.resources.Font
 import raccoonforfriendica.composeapp.generated.resources.Res
+import raccoonforfriendica.composeapp.generated.resources.atkinsonhyperlegible_bold
+import raccoonforfriendica.composeapp.generated.resources.atkinsonhyperlegible_italic
+import raccoonforfriendica.composeapp.generated.resources.atkinsonhyperlegible_regular
 import raccoonforfriendica.composeapp.generated.resources.exo_bold
 import raccoonforfriendica.composeapp.generated.resources.exo_italic
 import raccoonforfriendica.composeapp.generated.resources.exo_light
@@ -17,14 +20,13 @@ import raccoonforfriendica.composeapp.generated.resources.notosans_medium
 import raccoonforfriendica.composeapp.generated.resources.notosans_regular
 
 internal class SharedResources : CoreResources {
-    override val notoSans: FontFamily
+    override val atkinsonHyperlegible: FontFamily
         @Composable
         get() =
             FontFamily(
-                Font(Res.font.notosans_regular, FontWeight.Normal, FontStyle.Normal),
-                Font(Res.font.notosans_bold, FontWeight.Bold, FontStyle.Normal),
-                Font(Res.font.notosans_medium, FontWeight.Medium, FontStyle.Normal),
-                Font(Res.font.notosans_italic, FontWeight.Normal, FontStyle.Italic),
+                Font(Res.font.atkinsonhyperlegible_regular, FontWeight.Normal, FontStyle.Normal),
+                Font(Res.font.atkinsonhyperlegible_bold, FontWeight.Bold, FontStyle.Normal),
+                Font(Res.font.atkinsonhyperlegible_italic, FontWeight.Normal, FontStyle.Italic),
             )
 
     override val exo2: FontFamily
@@ -35,5 +37,14 @@ internal class SharedResources : CoreResources {
                 Font(Res.font.exo_bold, FontWeight.Bold, FontStyle.Normal),
                 Font(Res.font.exo_light, FontWeight.Light, FontStyle.Normal),
                 Font(Res.font.exo_italic, FontWeight.Normal, FontStyle.Italic),
+            )
+    override val notoSans: FontFamily
+        @Composable
+        get() =
+            FontFamily(
+                Font(Res.font.notosans_regular, FontWeight.Normal, FontStyle.Normal),
+                Font(Res.font.notosans_bold, FontWeight.Bold, FontStyle.Normal),
+                Font(Res.font.notosans_medium, FontWeight.Medium, FontStyle.Normal),
+                Font(Res.font.notosans_italic, FontWeight.Normal, FontStyle.Italic),
             )
 }
--- core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/data/UiFontFamily.kt
+++ core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/data/UiFontFamily.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import com.livefast.eattrash.raccoonforfriendica.core.l10n.messages.LocalStrings
 
 sealed interface UiFontFamily {
+    data object AtkinsonHyperlegible : UiFontFamily
+
     data object Exo2 : UiFontFamily
 
     data object NotoSans : UiFontFamily
@@ -15,6 +17,7 @@ fun Int.toUiFontFamily() =
     when (this) {
         1 -> UiFontFamily.Exo2
         2 -> UiFontFamily.NotoSans
+        3 -> UiFontFamily.AtkinsonHyperlegible
         else -> UiFontFamily.Default
     }
 
@@ -22,12 +25,14 @@ fun UiFontFamily.toInt() =
     when (this) {
         UiFontFamily.Exo2 -> 1
         UiFontFamily.NotoSans -> 2
+        UiFontFamily.AtkinsonHyperlegible -> 3
         else -> 0
     }
 
 @Composable
 fun UiFontFamily.toReadableName() =
     when (this) {
+        UiFontFamily.AtkinsonHyperlegible -> "Atkinson Hyperlegible"
         UiFontFamily.Exo2 -> "Exo2"
         UiFontFamily.NotoSans -> "Noto Sans"
         UiFontFamily.Default -> LocalStrings.current.systemDefault
