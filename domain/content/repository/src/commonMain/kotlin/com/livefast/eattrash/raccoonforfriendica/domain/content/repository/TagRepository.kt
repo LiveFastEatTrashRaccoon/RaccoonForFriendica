0d6449e6ef6727ddad76c3f34d3f1c5c1c736825
akesi seli <andongue.nena@gmail.com>
2024-10-29 09:00:32 +0100

    update TagRepository

--- domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/TagRepository.kt
+++ domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/TagRepository.kt
@@ -1,9 +1,10 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.repository
 
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TagModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.utils.ListWithPageCursor
 
 interface TagRepository {
-    suspend fun getFollowed(pageCursor: String? = null): Pair<List<TagModel>, String?>?
+    suspend fun getFollowed(pageCursor: String? = null): ListWithPageCursor<TagModel>?
 
     suspend fun getBy(name: String): TagModel?
 
