60a58b9d64d94107c31d446612cd247df7644537
akesi seli <andongue.nena@gmail.com>
2024-11-01 12:46:47 +0100

    update circle screen contract

--- feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/list/CirclesMviModel.kt
+++ feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/list/CirclesMviModel.kt
@@ -5,6 +5,7 @@ import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
 import com.livefast.eattrash.raccoonforfriendica.core.utils.validation.ValidationError
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.CircleModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.CircleReplyPolicy
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.CircleType
 
 data class CircleEditorData(
     val id: String? = null,
@@ -13,6 +14,16 @@ data class CircleEditorData(
     val replyPolicy: CircleReplyPolicy = CircleReplyPolicy.List,
     val exclusive: Boolean = false,
 )
+
+sealed interface CircleListItem {
+    data class Header(
+        val type: CircleType,
+    ) : CircleListItem
+
+    data class Circle(
+        val circle: CircleModel,
+    ) : CircleListItem
+}
 
 interface CirclesMviModel :
     ScreenModel,
@@ -41,7 +52,7 @@ interface CirclesMviModel :
         val initial: Boolean = true,
         val refreshing: Boolean = false,
         val loading: Boolean = false,
-        val items: List<CircleModel> = emptyList(),
+        val items: List<CircleListItem> = emptyList(),
         val editorData: CircleEditorData? = null,
     )
 
