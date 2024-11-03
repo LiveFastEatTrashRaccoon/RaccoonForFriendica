a957c25bc22684abac673a6b04a236537cee4bd5
akesi seli <andongue.nena@gmail.com>
2024-11-03 08:57:51 +0100

    add support for host/tags/:name hashtags

--- domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/HashtagProcessor.kt
+++ domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/HashtagProcessor.kt
@@ -1,23 +1,29 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor
 
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.UriHandlerConstants.DETAIL_FRAGMENT
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.UriHandlerConstants.INSTANCE_FRAGMENT
 
 internal interface HashtagProcessor : UrlProcessor
 
 internal class DefaultHashtagProcessor(
-    private val apiConfigurationRepository: ApiConfigurationRepository,
     private val detailOpener: DetailOpener,
 ) : HashtagProcessor {
     override suspend fun process(uri: String): Boolean {
-        val currentNode = apiConfigurationRepository.node.value
-        val tagPrefix = "https://$currentNode/search?tag="
-        if (!uri.startsWith(tagPrefix)) {
-            return false
-        }
+        val tag =
+            listOfNotNull(
+                REGEX_1.find(uri)?.groups?.let { it["tag"]?.value },
+                REGEX_2.find(uri)?.groups?.let { it["tag"]?.value },
+            ).firstOrNull() ?: return false
 
-        val tag = uri.replace(tagPrefix, "")
         detailOpener.openHashtag(tag)
         return true
+    }
+
+    companion object {
+        private val REGEX_1 =
+            Regex("https://(?<instance>$INSTANCE_FRAGMENT)/search\\?tag=(?<tag>$DETAIL_FRAGMENT)")
+        private val REGEX_2 =
+            Regex("https://(?<instance>$INSTANCE_FRAGMENT)/tags/(?<tag>$DETAIL_FRAGMENT)")
     }
 }
