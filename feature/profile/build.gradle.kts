dcda98d262bbb838fd198a158eb812fc5a41c12b
akesi seli <andongue.nena@gmail.com>
2024-11-04 13:09:49 +0100

    completely redesign anonymous screen

--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -47,6 +47,7 @@ kotlin {
                 implementation(projects.core.l10n)
                 implementation(projects.core.navigation)
                 implementation(projects.core.notifications)
+                implementation(projects.core.resources)
                 implementation(projects.core.utils)
 
                 implementation(projects.domain.content.data)
--- feature/profile/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/profile/anonymous/AnonymousMviModel.kt
+++ feature/profile/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/profile/anonymous/AnonymousMviModel.kt
@@ -2,21 +2,20 @@ package com.livefast.eattrash.raccoonforfriendica.feature.profile.anonymous
 
 import cafe.adriel.voyager.core.model.ScreenModel
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
+import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.LoginType
 
 interface AnonymousMviModel :
     ScreenModel,
     MviModel<AnonymousMviModel.Intent, AnonymousMviModel.State, AnonymousMviModel.Effect> {
     sealed interface Intent {
-        data object StartOauth2Flow : Intent
+        data class StartOauth2Flow(
+            val type: LoginType,
+        ) : Intent
 
         data object StartLegacyFlow : Intent
     }
 
     object State
 
-    sealed interface Effect {
-        data class Failure(
-            val message: String?,
-        ) : Effect
-    }
+    sealed interface Effect
 }
