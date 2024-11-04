1bf7d6748dc88d5155bdd519884fa77d4b0495b6
akesi seli <andongue.nena@gmail.com>
2024-11-04 17:34:20 +0100

    fix: avoid resolving URLs for user detail if match is not exact (#443)

--- domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCase.kt
+++ domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCase.kt
@@ -8,11 +8,15 @@ internal class DefaultFetchUserUseCase(
     private val userRepository: UserRepository,
 ) : FetchUserUseCase {
     override suspend fun invoke(handle: String): UserModel? =
+        // wait at most USER_SEARCH_TIMEOUT_MILLIS failing if the request takes longer
         withTimeoutOrNull(USER_SEARCH_TIMEOUT_MILLIS) {
-            userRepository.getByHandle(handle)
+            userRepository.getByHandle(handle).takeIf {
+                // eventual consistency check: the match should be exact
+                it?.handle == handle
+            }
         }
 
     companion object {
-        private const val USER_SEARCH_TIMEOUT_MILLIS = 1000L
+        private const val USER_SEARCH_TIMEOUT_MILLIS = 1500L
     }
 }
