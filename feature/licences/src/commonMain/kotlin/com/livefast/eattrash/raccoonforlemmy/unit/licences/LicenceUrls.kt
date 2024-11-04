ca55d87c83aeff261c670596791399b0c894816b
akesi seli <andongue.nena@gmail.com>
2024-11-04 15:41:56 +0100

    add image licenses

--- feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/licences/LicenceUrls.kt
+++ feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforlemmy/unit/licences/LicenceUrls.kt
@@ -5,11 +5,13 @@ internal object LicenceUrls {
     const val COIL = "https://github.com/coil-kt/coil/blob/main/LICENSE.txt"
     const val COMPOSE_COLORPICKER =
         "https://github.com/skydoves/colorpicker-compose/blob/main/LICENSE"
+    const val FRIENDICA = "https://github.com/friendica/friendica/blob/2024.09-rc/LICENSE"
     const val KOIN = "https://github.com/InsertKoinIO/koin/blob/main/LICENSE"
     const val KSOUP = "https://github.com/fleeksoft/ksoup/blob/release/LICENSE.md"
     const val KTOR = "https://github.com/ktorio/ktor/blob/main/LICENSE"
     const val KTORFIT = "https://github.com/Foso/Ktorfit/blob/master/LICENSE"
     const val LYRICIST = "https://github.com/adrielcafe/lyricist/blob/main/LICENSE.md"
+    const val MASTODON = "https://github.com/mastodon/mastodon/blob/main/LICENSE"
     const val MATERIAL_ICONS = "https://github.com/google/material-design-icons/blob/master/LICENSE"
     const val MATERIAL_KOLOR = "https://github.com/jordond/MaterialKolor/blob/main/LICENSE"
     const val OFL = "https://openfontlicense.org/"
