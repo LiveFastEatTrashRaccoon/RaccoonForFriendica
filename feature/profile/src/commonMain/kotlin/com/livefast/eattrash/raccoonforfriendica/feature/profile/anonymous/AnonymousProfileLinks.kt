bc03f8e0765351634b7b1823022d6ced83e730d0
akesi seli <andongue.nena@gmail.com>
2024-11-08 09:30:20 +0100

    remove anonymous profile screen

--- feature/profile/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/profile/anonymous/AnonymousProfileLinks.kt
+++
@@ -1,8 +0,0 @@
-package com.livefast.eattrash.raccoonforfriendica.feature.profile.anonymous
-
-internal object AnonymousProfileLinks {
-    const val ABOUT_FRIENDICA = "https://friendi.ca/"
-    const val FRIENDICA_INSTANCE_HELP = "https://dir.friendica.social/servers"
-    const val ABOUT_MASTODON = "https://joinmastodon.org/"
-    const val MASTODON_INSTANCE_HELP = "https://joinmastodon.org/servers"
-}
