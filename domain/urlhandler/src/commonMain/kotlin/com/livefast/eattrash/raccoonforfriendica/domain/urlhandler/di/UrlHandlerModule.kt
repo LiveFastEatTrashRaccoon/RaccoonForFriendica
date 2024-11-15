785a079c4f1d0966fefd5cc7f891623f20a4b827
akesi seli <andongue.nena@gmail.com>
2024-11-15 23:17:13 +0100

    update DI

--- domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/di/UrlHandlerModule.kt
+++ domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/di/UrlHandlerModule.kt
@@ -2,32 +2,26 @@ package com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.di
 
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.CustomUriHandler
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.DefaultCustomUriHandler
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultExternalUserProcessor
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultEntryProcessor
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultFetchEntryUseCase
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultFetchUserUseCase
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultFriendicaUserProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultGuppeProcessor
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultHashtagProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultLemmyProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultMastodonUserProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultPeertubeProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.DefaultPixelfedProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.ExternalUserProcessor
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.EntryProcessor
+import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.FetchEntryUseCase
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.FetchUserUseCase
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.FriendicaUserProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.GuppeProcessor
 import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.HashtagProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.LemmyProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.MastodonUserProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.PeertubeProcessor
-import com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor.PixelfedProcessor
 import org.koin.dsl.module
 
 val domainUrlHandlerModule =
     module {
         single<FetchUserUseCase> {
             DefaultFetchUserUseCase(
-                userRepository = get(),
-                apiConfigurationRepository = get(),
+                searchRepository = get(),
+            )
+        }
+        single<FetchEntryUseCase> {
+            DefaultFetchEntryUseCase(
+                searchRepository = get(),
             )
         }
         single<HashtagProcessor> {
@@ -35,46 +29,10 @@ val domainUrlHandlerModule =
                 detailOpener = get(),
             )
         }
-        single<FriendicaUserProcessor> {
-            DefaultFriendicaUserProcessor(
+        single<EntryProcessor> {
+            DefaultEntryProcessor(
                 detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<ExternalUserProcessor> {
-            DefaultExternalUserProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<MastodonUserProcessor> {
-            DefaultMastodonUserProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<PixelfedProcessor> {
-            DefaultPixelfedProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<LemmyProcessor> {
-            DefaultLemmyProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<GuppeProcessor> {
-            DefaultGuppeProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
-            )
-        }
-        single<PeertubeProcessor> {
-            DefaultPeertubeProcessor(
-                detailOpener = get(),
-                fetchUser = get(),
+                fetchEntry = get(),
             )
         }
         single<CustomUriHandler> { params ->
@@ -82,15 +40,10 @@ val domainUrlHandlerModule =
                 defaultHandler = params[0],
                 customTabsHelper = get(),
                 settingsRepository = get(),
-                hashtagProcessor = get(),
-                friendicaUserProcessor = get(),
-                externalUserProcessor = get(),
-                mastodonUserProcessor = get(),
-                lemmyProcessor = get(),
-                guppeProcessor = get(),
-                peertubeProcessor = get(),
-                pixelfedProcessor = get(),
                 detailOpener = get(),
+                hashtagProcessor = get(),
+                userProcessor = get(),
+                entryProcessor = get(),
             )
         }
     }
