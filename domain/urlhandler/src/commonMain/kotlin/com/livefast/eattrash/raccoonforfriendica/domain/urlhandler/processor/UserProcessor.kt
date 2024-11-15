89916d56971d7629b57b63c520781fb6167836b2
akesi seli <andongue.nena@gmail.com>
2024-11-15 23:15:25 +0100

    refactor UserProcessor

---
+++ domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/UserProcessor.kt
@@ -0,0 +1,16 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor
+
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
+
+internal interface UserProcessor : UrlProcessor
+
+internal class DefaultUserProcessor(
+    private val detailOpener: DetailOpener,
+    private val fetchUser: FetchUserUseCase,
+) : UserProcessor {
+    override suspend fun process(uri: String): Boolean {
+        val remoteUser = fetchUser(uri) ?: return false
+        detailOpener.openUserDetail(remoteUser)
+        return true
+    }
+}
