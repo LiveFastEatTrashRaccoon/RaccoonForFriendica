c22845153c890bb1cd6e82d35fee340f79b14df8
akesi seli <andongue.nena@gmail.com>
2024-11-15 23:13:33 +0100

    refactor FetchUserUseCase

--- domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCase.kt
+++ domain/urlhandler/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCase.kt
@@ -1,34 +1,34 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor
 
-import com.livefast.eattrash.raccoonforfriendica.core.utils.detailName
-import com.livefast.eattrash.raccoonforfriendica.core.utils.nodeName
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.ExploreItemModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.SearchResultType
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
-import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.UserRepository
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.SearchRepository
 import kotlinx.coroutines.withTimeoutOrNull
 
 internal class DefaultFetchUserUseCase(
-    private val userRepository: UserRepository,
-    private val apiConfigurationRepository: ApiConfigurationRepository,
+    private val searchRepository: SearchRepository,
 ) : FetchUserUseCase {
-    override suspend fun invoke(handle: String): UserModel? =
-        // wait at most USER_SEARCH_TIMEOUT_MILLIS failing if the request takes longer
-        withTimeoutOrNull(USER_SEARCH_TIMEOUT_MILLIS) {
-            userRepository.getByHandle(handle).takeIf {
-                // eventual consistency check: the match should be exact
-                val currentNode = apiConfigurationRepository.node.value
-                val userNode = handle.nodeName
-                if (currentNode == userNode) {
-                    // local user
-                    it?.handle == handle.detailName
-                } else {
-                    // federated user
-                    it?.handle == handle
+    override suspend fun invoke(url: String): UserModel? =
+        // wait at most SEARCH_TIMEOUT_MILLIS failing if the request takes longer
+        withTimeoutOrNull(SEARCH_TIMEOUT_MILLIS) {
+            searchRepository
+                .search(
+                    query = url,
+                    resolve = true,
+                    type = SearchResultType.Users,
+                ).orEmpty()
+                .mapNotNull { res ->
+                    when (res) {
+                        is ExploreItemModel.User -> res.user
+                        else -> null
+                    }
+                }.firstOrNull {
+                    it.url == url
                 }
-            }
         }
 
     companion object {
-        private const val USER_SEARCH_TIMEOUT_MILLIS = 1500L
+        private const val SEARCH_TIMEOUT_MILLIS = 1500L
     }
 }
--- domain/urlhandler/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCaseTest.kt
+++ domain/urlhandler/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/urlhandler/processor/DefaultFetchUserUseCaseTest.kt
@@ -1,17 +1,16 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.urlhandler.processor
 
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.ExploreItemModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.SearchResultType
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
-import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.UserRepository
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.SearchRepository
 import dev.mokkery.answering.calls
 import dev.mokkery.answering.returns
-import dev.mokkery.every
 import dev.mokkery.everySuspend
 import dev.mokkery.matcher.any
 import dev.mokkery.mock
 import dev.mokkery.verifySuspend
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertNotNull
@@ -19,96 +18,99 @@ import kotlin.test.assertNull
 import kotlin.time.Duration.Companion.seconds
 
 class DefaultFetchUserUseCaseTest {
-    private val userRepository = mock<UserRepository>()
-    private val apiConfigurationRepository =
-        mock<ApiConfigurationRepository> {
-            every { node } returns MutableStateFlow(LOCAL_HOST)
-        }
+    private val searchRepository = mock<SearchRepository>()
     private val sut =
         DefaultFetchUserUseCase(
-            userRepository = userRepository,
-            apiConfigurationRepository = apiConfigurationRepository,
+            searchRepository = searchRepository,
         )
 
     @Test
     fun `given user found when invoke then result is as expected`() =
         runTest {
-            val handle = "$USERNAME@$HOST"
-            val user = UserModel(id = "0", username = USERNAME, handle = handle)
-            everySuspend { userRepository.getByHandle(any()) } returns user
+            val user = UserModel(id = "0", username = USERNAME, url = URL)
+            everySuspend {
+                searchRepository.search(
+                    query = any(),
+                    type = any(),
+                    pageCursor = any(),
+                    resolve = any(),
+                )
+            } returns
+                listOf(ExploreItemModel.User(user))
 
-            val res = sut.invoke(handle)
+            val res = sut.invoke(URL)
 
             assertNotNull(res)
             verifySuspend {
-                userRepository.getByHandle(handle)
+                searchRepository.search(
+                    query = URL,
+                    type = SearchResultType.Users,
+                    pageCursor = null,
+                    resolve = true,
+                )
             }
         }
 
     @Test
     fun `given user not found when invoke then result is as expected`() =
         runTest {
-            everySuspend { userRepository.getByHandle(any()) } returns null
-            val handle = "$USERNAME@$HOST"
+            everySuspend {
+                searchRepository.search(
+                    query = any(),
+                    type = any(),
+                    pageCursor = any(),
+                    resolve = any(),
+                )
+            } returns emptyList()
 
-            val res = sut.invoke(handle)
-
-            assertNull(res)
-            verifySuspend {
-                userRepository.getByHandle(handle)
-            }
-        }
-
-    @Test
-    fun `given user handle not matching when invoke then result is as expected`() =
-        runTest {
-            val handle = "$USERNAME@$HOST"
-            val user = UserModel(id = "0", username = USERNAME, handle = "")
-            everySuspend { userRepository.getByHandle(any()) } returns user
-
-            val res = sut.invoke(handle)
+            val res = sut.invoke(URL)
 
             assertNull(res)
             verifySuspend {
-                userRepository.getByHandle(handle)
+                searchRepository.search(
+                    query = URL,
+                    type = SearchResultType.Users,
+                    pageCursor = null,
+                    resolve = true,
+                )
             }
         }
 
-    @Test
-    fun `given local user when invoke then result is as expected`() =
-        runTest {
-            val handle = "$USERNAME@$LOCAL_HOST"
-            val user = UserModel(id = "0", username = USERNAME, handle = USERNAME)
-            everySuspend { userRepository.getByHandle(any()) } returns user
-
-            val res = sut.invoke(handle)
-
-            assertNotNull(res)
-            verifySuspend {
-                userRepository.getByHandle(handle)
-            }
-        }
 
     @Test
     fun `given request timeout when invoke then result is as expected`() =
         runTest {
-            val handle = "$USERNAME@$HOST"
-            everySuspend { userRepository.getByHandle(any()) } calls {
+            everySuspend {
+                searchRepository.search(
+                    query = any(),
+                    type = any(),
+                    pageCursor = any(),
+                    resolve = any(),
+                )
+            } calls {
                 delay(10.seconds)
-                UserModel(id = "0", username = USERNAME, handle = handle)
+                listOf(
+                    ExploreItemModel.User(
+                        UserModel(id = "0", username = USERNAME),
+                    ),
+                )
             }
 
-            val res = sut.invoke(handle)
+            val res = sut.invoke(URL)
 
             assertNull(res)
             verifySuspend {
-                userRepository.getByHandle(handle)
+                searchRepository.search(
+                    query = URL,
+                    type = SearchResultType.Users,
+                    pageCursor = null,
+                    resolve = true,
+                )
             }
         }
 
     companion object {
-        private const val HOST = "example.com"
-        private const val LOCAL_HOST = "my.instance"
+        private const val URL = "https://example.com/profile/username"
         private const val USERNAME = "username"
     }
 }
