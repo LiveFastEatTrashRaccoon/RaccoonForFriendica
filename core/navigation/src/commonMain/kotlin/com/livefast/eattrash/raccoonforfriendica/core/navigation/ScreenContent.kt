5281c2f1ea08810ad7c9adb97f092c73d0f2ab85
akesi seli <andongue.nena@gmail.com>
2024-11-15 21:50:01 +0100

    remove ScreenContent utility

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/ScreenContent.kt
+++
@@ -1,9 +0,0 @@
-package com.livefast.eattrash.raccoonforfriendica.core.navigation
-
-import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.core.screen.Screen
-
-@Composable
-fun ScreenContent(screen: Screen) {
-    screen.Content()
-}
