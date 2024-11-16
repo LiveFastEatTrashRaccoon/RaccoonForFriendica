5d5f54c8acda65f42a0f0add09f353478eb707d6
akesi seli <andongue.nena@gmail.com>
2024-11-17 00:20:20 +0100

    add export to user list screen

--- feature/userlist/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/userlist/UserListScreen.kt
+++ feature/userlist/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/userlist/UserListScreen.kt
@@ -1,6 +1,7 @@
 package com.livefast.eattrash.raccoonforfriendica.feature.userlist
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,11 +11,16 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -29,20 +35,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.DpOffset
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.koin.getScreenModel
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.toWindowInsets
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.CustomDropDown
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.ProgressHud
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.CustomConfirmDialog
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.OptionId
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.UserItem
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.UserItemPlaceholder
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.toOption
 import com.livefast.eattrash.raccoonforfriendica.core.l10n.messages.LocalStrings
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getDetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getNavigationCoordinator
+import com.livefast.eattrash.raccoonforfriendica.core.utils.fs.getFileSystemManager
 import com.livefast.eattrash.raccoonforfriendica.core.utils.isNearTheEnd
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.RelationshipStatusNextAction
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserListType
@@ -57,6 +73,7 @@ class UserListScreen(
     private val userId: String? = null,
     private val entryId: String? = null,
     private val infoCount: Int? = null,
+    private val enableExport: Boolean = false,
 ) : Screen {
     override val key: ScreenKey
         get() =
@@ -101,12 +118,17 @@ class UserListScreen(
         val uiState by model.uiState.collectAsState()
         val topAppBarState = rememberTopAppBarState()
         val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+        val snackbarHostState = remember { SnackbarHostState() }
         val navigationCoordinator = remember { getNavigationCoordinator() }
         val detailOpener = remember { getDetailOpener() }
+        val fileSystemManager = remember { getFileSystemManager() }
         val lazyListState = rememberLazyListState()
         val scope = rememberCoroutineScope()
+        val successMessage = LocalStrings.current.messageSuccess
+        val errorMessage = LocalStrings.current.messageGenericError
         var confirmUnfollowDialogUserId by remember { mutableStateOf<String?>(null) }
         var confirmDeleteFollowRequestDialogUserId by remember { mutableStateOf<String?>(null) }
+        var listContent by remember { mutableStateOf<String?>(null) }
 
         fun goBackToTop() {
             runCatching {
@@ -123,6 +145,13 @@ class UserListScreen(
                 .onEach { event ->
                     when (event) {
                         UserListMviModel.Effect.BackToTop -> goBackToTop()
+                        is UserListMviModel.Effect.SaveList -> {
+                            if (event.content.isEmpty()) {
+                                snackbarHostState.showSnackbar(errorMessage)
+                            } else {
+                                listContent = event.content
+                            }
+                        }
                     }
                 }.launchIn(this)
         }
@@ -204,7 +233,81 @@ class UserListScreen(
                             }
                         }
                     },
+                    actions = {
+                        val options =
+                            buildList {
+                                if (enableExport) {
+                                    this +=
+                                        CustomOptions.Export.toOption(
+                                            label = LocalStrings.current.actionExport,
+                                        )
+                                }
+                            }
+                        if (options.isNotEmpty()) {
+                            Box {
+                                var optionsOffset by remember { mutableStateOf(Offset.Zero) }
+                                var optionsMenuOpen by remember { mutableStateOf(false) }
+                                IconButton(
+                                    modifier =
+                                        Modifier.onGloballyPositioned {
+                                            optionsOffset = it.positionInParent()
+                                        },
+                                    onClick = {
+                                        optionsMenuOpen = true
+                                    },
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.MoreVert,
+                                        contentDescription = null,
+                                    )
+                                }
+
+                                CustomDropDown(
+                                    expanded = optionsMenuOpen,
+                                    onDismiss = {
+                                        optionsMenuOpen = false
+                                    },
+                                    offset =
+                                        with(LocalDensity.current) {
+                                            DpOffset(
+                                                x = optionsOffset.x.toDp(),
+                                                y = optionsOffset.y.toDp(),
+                                            )
+                                        },
+                                ) {
+                                    for (option in options) {
+                                        DropdownMenuItem(
+                                            text = {
+                                                Text(option.label)
+                                            },
+                                            onClick = {
+                                                optionsMenuOpen = false
+                                                when (option.id) {
+                                                    CustomOptions.Export -> {
+                                                        model.reduce(UserListMviModel.Intent.Export)
+                                                    }
+
+                                                    else -> Unit
+                                                }
+                                            },
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    },
                 )
+            },
+            snackbarHost = {
+                SnackbarHost(
+                    hostState = snackbarHostState,
+                ) { data ->
+                    Snackbar(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        snackbarData = data,
+                    )
+                }
             },
         ) { padding ->
             PullToRefreshBox(
@@ -292,6 +395,10 @@ class UserListScreen(
             }
         }
 
+        if (uiState.operationInProgress) {
+            ProgressHud()
+        }
+
         if (confirmUnfollowDialogUserId != null) {
             CustomConfirmDialog(
                 title = LocalStrings.current.actionUnfollow,
@@ -317,5 +424,31 @@ class UserListScreen(
                 },
             )
         }
+
+        listContent?.also { content ->
+            fileSystemManager.writeToFile(
+                mimeType = EXPORT_LIST_MIME_TYPE,
+                name = EXPORT_LIST_FILE_NAME,
+                data = content,
+            ) { success ->
+                scope.launch {
+                    snackbarHostState.showSnackbar(
+                        if (success) {
+                            successMessage
+                        } else {
+                            errorMessage
+                        },
+                    )
+                }
+                listContent = null
+            }
+        }
     }
 }
+
+private sealed interface CustomOptions : OptionId.Custom {
+    data object Export : CustomOptions
+}
+
+private const val EXPORT_LIST_MIME_TYPE = "text/plain"
+private const val EXPORT_LIST_FILE_NAME = "user_list.txt"
