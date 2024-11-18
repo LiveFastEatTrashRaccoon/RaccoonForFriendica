c7035533475ee83cbba68e6918baa7265c0bc53e
akesi seli <andongue.nena@gmail.com>
2024-11-18 11:22:24 +0100

    add send/schedule/save button to UtilsBar

--- feature/composer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/composer/components/UtilsBar.kt
+++ feature/composer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/composer/components/UtilsBar.kt
@@ -3,8 +3,11 @@ package com.livefast.eattrash.raccoonforfriendica.feature.composer.components
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ScheduleSend
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Code
 import androidx.compose.material.icons.filled.FormatBold
 import androidx.compose.material.icons.filled.FormatItalic
@@ -12,21 +15,22 @@ import androidx.compose.material.icons.filled.FormatStrikethrough
 import androidx.compose.material.icons.filled.FormatUnderlined
 import androidx.compose.material.icons.filled.Link
 import androidx.compose.material.icons.filled.PhotoCamera
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.FilledIconButton
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
+import com.livefast.eattrash.raccoonforfriendica.feature.composer.PublicationType
 
 @Composable
 internal fun UtilsBar(
     modifier: Modifier = Modifier,
     hasPoll: Boolean = false,
-    hasSpoiler: Boolean = false,
+    supportsRichEditing: Boolean = true,
+    publicationType: PublicationType,
     onLinkClicked: (() -> Unit)? = null,
     onAttachmentClicked: (() -> Unit)? = null,
     onBoldClicked: (() -> Unit)? = null,
@@ -34,119 +38,127 @@ internal fun UtilsBar(
     onUnderlineClicked: (() -> Unit)? = null,
     onStrikethroughClicked: (() -> Unit)? = null,
     onCodeClicked: (() -> Unit)? = null,
-    onSpoilerClicked: (() -> Unit)? = null,
+    onSubmitClicked: (() -> Unit)? = null,
 ) {
     Row(
-        modifier = modifier.horizontalScroll(rememberScrollState()),
+        modifier = modifier.padding(horizontal = Spacing.xxs),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
     ) {
-        IconButton(
-            enabled = !hasPoll,
-            onClick = {
-                onAttachmentClicked?.invoke()
-            },
+        Row(
+            modifier = Modifier.weight(1f).horizontalScroll(rememberScrollState()),
         ) {
-            Icon(
-                imageVector = Icons.Default.PhotoCamera,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onLinkClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.Link,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onBoldClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.FormatBold,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onItalicClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.FormatItalic,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onUnderlineClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.FormatUnderlined,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onStrikethroughClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.FormatStrikethrough,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        IconButton(
-            onClick = {
-                onCodeClicked?.invoke()
-            },
-        ) {
-            Icon(
-                imageVector = Icons.Default.Code,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        }
-        if (hasSpoiler) {
-            FilledIconButton(
-                colors =
-                    IconButtonDefaults.filledIconButtonColors(
-                        containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                    ),
-                onClick = {
-                    onSpoilerClicked?.invoke()
-                },
-            ) {
-                Icon(
-                    imageVector = Icons.Default.VisibilityOff,
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.surfaceVariant,
-                )
-            }
-        } else {
+            // add picture (from device gallery) button
             IconButton(
+                enabled = !hasPoll,
                 onClick = {
-                    onSpoilerClicked?.invoke()
+                    onAttachmentClicked?.invoke()
                 },
             ) {
                 Icon(
-                    imageVector = Icons.Default.VisibilityOff,
+                    imageVector = Icons.Default.PhotoCamera,
                     contentDescription = null,
                     tint = MaterialTheme.colorScheme.onSurfaceVariant,
                 )
             }
+
+            if (supportsRichEditing) {
+                // insert link button
+                IconButton(
+                    onClick = {
+                        onLinkClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Link,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+
+                // bold format button
+                IconButton(
+                    onClick = {
+                        onBoldClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FormatBold,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+
+                // italic format button
+                IconButton(
+                    onClick = {
+                        onItalicClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FormatItalic,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+
+                // underlined format button
+                IconButton(
+                    onClick = {
+                        onUnderlineClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FormatUnderlined,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+
+                // strikethrough format button
+                IconButton(
+                    onClick = {
+                        onStrikethroughClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.FormatStrikethrough,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+
+                // monospace format button
+                IconButton(
+                    onClick = {
+                        onCodeClicked?.invoke()
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Code,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+            }
+        }
+
+        // send/schedule/save button
+        IconButton(
+            onClick = {
+                onSubmitClicked?.invoke()
+            },
+        ) {
+            Icon(
+                imageVector =
+                    when (publicationType) {
+                        PublicationType.Draft -> Icons.Default.Save
+                        is PublicationType.Scheduled -> Icons.AutoMirrored.Default.ScheduleSend
+                        else -> Icons.AutoMirrored.Default.Send
+                    },
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+            )
         }
     }
 }
