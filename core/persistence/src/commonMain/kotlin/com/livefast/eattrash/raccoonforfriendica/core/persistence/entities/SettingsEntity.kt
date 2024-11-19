5791d46d11031cd879fbd9ff2c50db8adab8f3fe
akesi seli <andongue.nena@gmail.com>
2024-11-20 00:12:25 +0100

    migrate DB schema

--- core/persistence/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/persistence/entities/SettingsEntity.kt
+++ core/persistence/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/persistence/entities/SettingsEntity.kt
@@ -29,4 +29,5 @@ data class SettingsEntity(
     val pullNotificationCheckInterval: Long? = null,
     @ColumnInfo(defaultValue = "1") val autoloadImages: Int = 1,
     @ColumnInfo(defaultValue = "1") val hideNavigationBarWhileScrolling: Boolean = true,
+    @ColumnInfo(defaultValue = "0") val barTheme: Int = 0,
 )
