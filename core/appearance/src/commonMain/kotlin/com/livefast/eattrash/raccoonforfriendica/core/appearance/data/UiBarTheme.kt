5fe10cb90dd1045d38a92cabe1bc756309fc7805
akesi seli <andongue.nena@gmail.com>
2024-11-20 00:13:40 +0100

    update UiBarTheme values

--- core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/data/UiBarTheme.kt
+++ core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/data/UiBarTheme.kt
@@ -13,9 +13,16 @@ sealed interface UiBarTheme {
 
 fun UiBarTheme?.toInt(): Int =
     when (this) {
-        UiBarTheme.Transparent -> 2
-        UiBarTheme.Opaque -> 1
+        UiBarTheme.Solid -> 1
+        UiBarTheme.Opaque -> 2
         else -> 0
+    }
+
+fun Int.toUIBarTheme(): UiBarTheme =
+    when (this) {
+        1 -> UiBarTheme.Solid
+        2 -> UiBarTheme.Opaque
+        else -> UiBarTheme.Transparent
     }
 
 @Composable
@@ -23,5 +30,5 @@ fun UiBarTheme?.toReadableName(): String =
     when (this) {
         UiBarTheme.Transparent -> LocalStrings.current.barThemeTransparent
         UiBarTheme.Opaque -> LocalStrings.current.barThemeOpaque
-        else -> ""
+        else -> LocalStrings.current.barThemeSolid
     }
