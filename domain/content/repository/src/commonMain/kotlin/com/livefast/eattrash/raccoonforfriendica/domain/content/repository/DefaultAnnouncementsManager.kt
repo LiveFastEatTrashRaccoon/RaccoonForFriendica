9dbe702370609190149523d7ee3b6a400396b0c1
akesi seli <andongue.nena@gmail.com>
2024-11-20 22:00:03 +0100

    add announcements manager
    (api, impl, test)

---
+++ domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultAnnouncementsManager.kt
@@ -0,0 +1,28 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.content.repository
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.flow.update
+
+internal class DefaultAnnouncementsManager(
+    private val announcementRepository: AnnouncementRepository,
+) : AnnouncementsManager {
+    override val unreadCount = MutableStateFlow(0)
+
+    override suspend fun clearUnreadCount() {
+        unreadCount.update { 0 }
+    }
+
+    override suspend fun refreshUnreadCount() {
+        val announcements = announcementRepository.getAll(refresh = true)
+        unreadCount.update {
+            announcements?.count { !it.read } ?: 0
+        }
+    }
+
+    override suspend fun decrementUnreadCount() {
+        unreadCount.getAndUpdate {
+            (it - 1).coerceAtLeast(0)
+        }
+    }
+}
