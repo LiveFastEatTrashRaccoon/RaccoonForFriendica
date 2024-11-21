c130b6d71b752476ec33a1b574066fd2d291c2a3
akesi seli <andongue.nena@gmail.com>
2024-11-21 14:39:05 +0100

    add UI components for reactions

---
+++ feature/announcements/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/announcements/components/AnnouncementReactions.kt
@@ -0,0 +1,58 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.announcements.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.IconSize
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.ReactionModel
+
+@Composable
+internal fun AnnouncementReactions(
+    reactions: List<ReactionModel>,
+    modifier: Modifier = Modifier,
+    onAddNew: (() -> Unit)? = null,
+    onAdd: ((String) -> Unit)? = null,
+    onRemove: ((String) -> Unit)? = null,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Spacing.xs),
+    ) {
+        LazyRow(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.spacedBy(Spacing.xs),
+        ) {
+            items(reactions) { reaction ->
+                ReactionItem(
+                    reaction = reaction,
+                    onAdd = onAdd,
+                    onRemove = onRemove,
+                )
+            }
+        }
+        IconButton(
+            onClick = {
+                onAddNew?.invoke()
+            },
+        ) {
+            Icon(
+                modifier = Modifier.size(IconSize.l),
+                imageVector = Icons.Outlined.AddCircle,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
+    }
+}
