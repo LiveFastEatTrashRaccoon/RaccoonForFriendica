4613538351db4795b8aa2411e10e87dc913df6d5
akesi seli <andongue.nena@gmail.com>
2024-11-21 08:22:39 +0100

    update DI modules

---
+++ feature/announcements/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/announcements/di/AnnouncementsModule.kt
@@ -0,0 +1,18 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.announcements.di
+
+import com.livefast.eattrash.raccoonforfriendica.feature.announcements.AnnouncementsMviModel
+import com.livefast.eattrash.raccoonforfriendica.feature.announcements.AnnouncementsViewModel
+import org.koin.dsl.module
+
+val featureAnnouncementsModule =
+    module {
+        factory<AnnouncementsMviModel> {
+            AnnouncementsViewModel(
+                identityRepository = get(),
+                settingsRepository = get(),
+                announcementRepository = get(),
+                announcementsManager = get(),
+                imageAutoloadObserver = get(),
+            )
+        }
+    }
