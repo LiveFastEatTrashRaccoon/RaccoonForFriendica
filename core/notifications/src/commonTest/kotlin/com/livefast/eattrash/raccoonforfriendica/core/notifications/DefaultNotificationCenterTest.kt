0300a4b88e1e4f5f6c81b256a0b97e4a6015f059
akesi seli <andongue.nena@gmail.com>
2024-11-25 21:52:21 +0100

    improve DefaultNotificationCenterTest

--- core/notifications/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/core/notifications/DefaultNotificationCenterTest.kt
+++ core/notifications/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/core/notifications/DefaultNotificationCenterTest.kt
@@ -1,8 +1,16 @@
 package com.livefast.eattrash.raccoonforfriendica.core.notifications
 
 import app.cash.turbine.test
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.AlbumsUpdatedEvent
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.DraftDeletedEvent
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.TagUpdatedEvent
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.TimelineEntryCreatedEvent
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.TimelineEntryDeletedEvent
 import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.TimelineEntryUpdatedEvent
+import com.livefast.eattrash.raccoonforfriendica.core.notifications.events.UserUpdatedEvent
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TagModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
@@ -33,12 +41,48 @@ class DefaultNotificationCenterTest {
     }
 
     @Test
-    fun `given subscription when send event then event is received just once`() =
+    fun `given subscription when send UserUpdatedEvent then event is received just once`() =
         runTest {
             val expected =
-                TimelineEntryUpdatedEvent(
-                    entry = TimelineEntryModel(id = "0", content = ""),
-                )
+                UserUpdatedEvent(user = UserModel(id = "0"))
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(UserUpdatedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(UserUpdatedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send TimelineEntryCreatedEvent then event is received just once`() =
+        runTest {
+            val expected =
+                TimelineEntryCreatedEvent(entry = TimelineEntryModel(id = "0", content = ""))
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(TimelineEntryCreatedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(TimelineEntryCreatedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send TimelineEntryUpdatedEvent then event is received just once`() =
+        runTest {
+            val expected =
+                TimelineEntryUpdatedEvent(entry = TimelineEntryModel(id = "0", content = ""))
             launch {
                 sut.send(expected)
             }
@@ -49,6 +93,78 @@ class DefaultNotificationCenterTest {
             }
 
             sut.subscribe(TimelineEntryUpdatedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send TimelineEntryDeletedEvent then event is received just once`() =
+        runTest {
+            val expected = TimelineEntryDeletedEvent(id = "0")
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(TimelineEntryDeletedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(TimelineEntryDeletedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send TagUpdatedEvent then event is received just once`() =
+        runTest {
+            val expected = TagUpdatedEvent(tag = TagModel(name = "tag"))
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(TagUpdatedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(TagUpdatedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send AlbumsUpdatedEvent then event is received just once`() =
+        runTest {
+            val expected = AlbumsUpdatedEvent
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(AlbumsUpdatedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(AlbumsUpdatedEvent::class).test {
+                expectNoEvents()
+            }
+        }
+
+    @Test
+    fun `given subscription when send DraftDeletedEvent then event is received just once`() =
+        runTest {
+            val expected = DraftDeletedEvent(id = "0")
+            launch {
+                sut.send(expected)
+            }
+
+            sut.subscribe(DraftDeletedEvent::class).test {
+                val actual = awaitItem()
+                assertEquals(expected, actual)
+            }
+
+            sut.subscribe(DraftDeletedEvent::class).test {
                 expectNoEvents()
             }
         }
