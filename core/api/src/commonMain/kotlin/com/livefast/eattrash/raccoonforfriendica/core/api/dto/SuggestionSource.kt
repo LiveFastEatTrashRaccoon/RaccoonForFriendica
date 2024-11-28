8869e4bd1bd3ce394a9960355002ba8ea0f38d21
akesi seli <andongue.nena@gmail.com>
2024-11-28 08:26:21 +0100

    fix: update suggestion source values for Mastodon >= 4.3.0 (#589)

--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/dto/SuggestionSource.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/dto/SuggestionSource.kt
@@ -3,12 +3,30 @@ package com.livefast.eattrash.raccoonforfriendica.core.api.dto
 import kotlinx.serialization.SerialName
 
 enum class SuggestionSource {
+    @Deprecated("Replaced by FEATURED")
     @SerialName("staff")
     STAFF,
 
+    @Deprecated("Replaced by one of SIMILAR_TO_RECENTLY_FOLLOWED, FRIENDS_OF_FRIENDS")
     @SerialName("past_interactions")
     PAST_INTERACTIONS,
 
+    @Deprecated("Replaced by one of MOST_FOLLOWED, MOST_INTERACTIONS")
     @SerialName("global")
     GLOBAL,
+
+    @SerialName("similar_to_recently_followed")
+    SIMILAR_TO_RECENTLY_FOLLOWED,
+
+    @SerialName("friends_of_friends")
+    FRIENDS_OF_FRIENDS,
+
+    @SerialName("most_followed")
+    MOST_FOLLOWED,
+
+    @SerialName("most_interactions")
+    MOST_INTERACTIONS,
+
+    @SerialName("featured")
+    FEATURED,
 }
