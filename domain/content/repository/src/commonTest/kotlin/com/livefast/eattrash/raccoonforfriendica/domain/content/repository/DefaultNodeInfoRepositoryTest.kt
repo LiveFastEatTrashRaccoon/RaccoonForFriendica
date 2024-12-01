453729c5e9bb8ce365b4e47a22f6c1955649f2a2
akesi seli <andongue.nena@gmail.com>
2024-12-01 16:17:14 +0100

    chore: add unit tests for media, node info and notification repositories (#603)
    * add test for DefaultMediaRepository
    
    * add test for DefaultNodeInfoRepository
    
    * add test for DefaultNotificationRepository

---
+++ domain/content/repository/src/commonTest/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/DefaultNodeInfoRepositoryTest.kt
@@ -0,0 +1,47 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.content.repository
+
+import com.livefast.eattrash.raccoonforfriendica.core.api.dto.Instance
+import com.livefast.eattrash.raccoonforfriendica.core.api.dto.InstanceRule
+import com.livefast.eattrash.raccoonforfriendica.core.api.provider.ServiceProvider
+import com.livefast.eattrash.raccoonforfriendica.core.api.service.InstanceService
+import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.utils.toModel
+import dev.mokkery.answering.returns
+import dev.mokkery.every
+import dev.mokkery.everySuspend
+import dev.mokkery.mock
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DefaultNodeInfoRepositoryTest {
+    private val instanceService = mock<InstanceService>()
+    private val serviceProvider =
+        mock<ServiceProvider> { every { instance } returns instanceService }
+    private val sut = DefaultNodeInfoRepository(provider = serviceProvider)
+
+    @Test
+    fun `when getInfo then result is as expected`() =
+        runTest {
+            val instance = Instance(domain = "example.com")
+            everySuspend { instanceService.getInfo() } returns instance
+
+            val res = sut.getInfo()
+
+            assertEquals(instance.toModel(), res)
+        }
+
+    @Test
+    fun `when getRules then result is as expected`() =
+        runTest {
+            val list =
+                listOf(
+                    InstanceRule(id = "1", text = "rule 1"),
+                    InstanceRule(id = "2", text = "rule 2"),
+                )
+            everySuspend { instanceService.getRules() } returns list
+
+            val res = sut.getRules()
+
+            assertEquals(list.map { it.toModel() }, res)
+        }
+}
