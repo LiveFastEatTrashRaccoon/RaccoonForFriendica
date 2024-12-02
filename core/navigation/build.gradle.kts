5afd8624393a6f8844eceaa446c27f3418374c8f
akesi seli <andongue.nena@gmail.com>
2024-12-02 22:03:25 +0100

    migrate :core:navigation to koin annotations

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,16 +1,16 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.mokkery)
     alias(libs.plugins.kotlinx.kover)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.mokkery)
 }
 
-@OptIn(ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     applyDefaultHierarchyTemplate()
     androidTarget {
@@ -36,6 +36,7 @@ kotlin {
             implementation(compose.material3)
 
             implementation(libs.koin.core)
+            api(libs.koin.annotations)
             implementation(libs.stately.common)
             implementation(libs.voyager.navigator)
             implementation(libs.voyager.tab)
@@ -56,6 +57,14 @@ kotlin {
     }
 }
 
+dependencies {
+    add("kspCommonMainMetadata", libs.koin.ksp)
+    add("kspAndroid", libs.koin.ksp)
+    add("kspIosX64", libs.koin.ksp)
+    add("kspIosArm64", libs.koin.ksp)
+    add("kspIosSimulatorArm64", libs.koin.ksp)
+}
+
 android {
     namespace = "com.livefast.eattrash.raccoonforfriendica.core.navigation"
     compileSdk =
@@ -67,5 +76,15 @@ android {
             libs.versions.android.minSdk
                 .get()
                 .toInt()
+    }
+}
+
+kotlin.sourceSets.commonMain.configure {
+    kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+}
+
+tasks.withType(KotlinCompilationTask::class.java).configureEach {
+    if (name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
--- core/navigation/src/androidMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
+++ core/navigation/src/androidMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/di/Utils.kt
@@ -3,16 +3,15 @@ package com.livefast.eattrash.raccoonforfriendica.core.navigation.di
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DetailOpener
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.DrawerCoordinator
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.NavigationCoordinator
-import org.koin.java.KoinJavaComponent
 import org.koin.java.KoinJavaComponent.inject
 
 actual fun getNavigationCoordinator(): NavigationCoordinator {
-    val res by KoinJavaComponent.inject<NavigationCoordinator>(NavigationCoordinator::class.java)
+    val res by inject<NavigationCoordinator>(NavigationCoordinator::class.java)
     return res
 }
 
 actual fun getDetailOpener(): DetailOpener {
-    val res by KoinJavaComponent.inject<DetailOpener>(DetailOpener::class.java)
+    val res by inject<DetailOpener>(DetailOpener::class.java)
     return res
 }
 
