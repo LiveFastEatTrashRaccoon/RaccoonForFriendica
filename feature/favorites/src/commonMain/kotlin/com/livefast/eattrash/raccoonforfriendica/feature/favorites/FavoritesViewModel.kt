3ba214696d0b884aa9d219340749ad7a545f8804
akesi seli <andongue.nena@gmail.com>
2024-12-04 19:01:41 +0100

    migrate :feature:favorites to koin annotations

--- feature/favorites/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/favorites/FavoritesViewModel.kt
+++ feature/favorites/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/favorites/FavoritesViewModel.kt
@@ -23,10 +23,13 @@ import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.Sett
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import org.koin.core.annotation.Factory
+import org.koin.core.annotation.InjectedParam
 import kotlin.time.Duration
 
+@Factory(binds = [FavoritesMviModel::class])
 class FavoritesViewModel(
-    private val type: FavoritesType,
+    @InjectedParam private val type: FavoritesType,
     private val paginationManager: FavoritesPaginationManager,
     private val timelineEntryRepository: TimelineEntryRepository,
     private val settingsRepository: SettingsRepository,
