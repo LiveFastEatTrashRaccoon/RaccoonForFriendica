6c27969e7244357367d5a004bcc87715ec9bb8af
akesi seli <andongue.nena@gmail.com>
2024-12-09 08:56:32 +0100

    apply convention plugins to domain modules

--- domain/content/pagination/build.gradle.kts
+++ domain/content/pagination/build.gradle.kts
@@ -1,37 +1,13 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlinx.kover)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.mokkery)
+    id("com.livefast.eattrash.kotlinMultiplatform")
+    id("com.livefast.eattrash.koinWithKsp")
+    id("com.livefast.eattrash.test")
 }
 
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    applyDefaultHierarchyTemplate()
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "domain.content.pagination"
-            isStatic = true
-        }
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(libs.koin.core)
                 implementation(libs.kotlinx.coroutines)
 
                 implementation(projects.core.notifications)
@@ -42,49 +18,5 @@ kotlin {
                 implementation(projects.domain.content.repository)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                dependencies {
-                    implementation(kotlin("test"))
-                    implementation(libs.kotlinx.coroutines.test)
-                }
-            }
-        }
-    }
-}
-
-dependencies {
-    add("kspCommonMainMetadata", libs.koin.ksp)
-    add("kspAndroid", libs.koin.ksp)
-    add("kspIosX64", libs.koin.ksp)
-    add("kspIosArm64", libs.koin.ksp)
-    add("kspIosSimulatorArm64", libs.koin.ksp)
-}
-
-ksp {
-    arg("KOIN_DEFAULT_MODULE", "false")
-}
-
-android {
-    namespace = "com.livefast.eattrash.raccoonforfriendica.domain.content.pagination"
-    compileSdk =
-        libs.versions.android.compileSdk
-            .get()
-            .toInt()
-    defaultConfig {
-        minSdk =
-            libs.versions.android.minSdk
-                .get()
-                .toInt()
-    }
-}
-
-kotlin.sourceSets.commonMain.configure {
-    kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
-}
-
-tasks.withType(KotlinCompilationTask::class.java).configureEach {
-    if (name != "kspCommonMainKotlinMetadata") {
-        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
--- domain/pushnotifications/build.gradle.kts
+++ domain/pushnotifications/build.gradle.kts
@@ -1,38 +1,11 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlinx.kover)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.mokkery)
+    id("com.livefast.eattrash.kotlinMultiplatform")
+    id("com.livefast.eattrash.composeMultiplatform")
+    id("com.livefast.eattrash.koinWithKsp")
+    id("com.livefast.eattrash.test")
 }
 
 kotlin {
-    applyDefaultHierarchyTemplate()
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "domain.pushnotifications"
-            isStatic = true
-        }
-    }
-
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
-
     sourceSets {
         val androidMain by getting {
             dependencies {
@@ -42,10 +15,6 @@ kotlin {
         }
         val commonMain by getting {
             dependencies {
-                implementation(compose.runtime)
-
-                implementation(libs.koin.core)
-                api(libs.koin.annotations)
                 implementation(libs.ktor.client.core)
 
                 implementation(projects.core.api)
@@ -62,47 +31,5 @@ kotlin {
                 implementation(projects.domain.pullnotifications)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(libs.kotlinx.coroutines.test)
-            }
-        }
-    }
-}
-
-dependencies {
-    add("kspCommonMainMetadata", libs.koin.ksp)
-    add("kspAndroid", libs.koin.ksp)
-    add("kspIosX64", libs.koin.ksp)
-    add("kspIosArm64", libs.koin.ksp)
-    add("kspIosSimulatorArm64", libs.koin.ksp)
-}
-
-ksp {
-    arg("KOIN_DEFAULT_MODULE", "false")
-}
-
-android {
-    namespace = "com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications"
-    compileSdk =
-        libs.versions.android.compileSdk
-            .get()
-            .toInt()
-    defaultConfig {
-        minSdk =
-            libs.versions.android.minSdk
-                .get()
-                .toInt()
-    }
-}
-
-kotlin.sourceSets.commonMain.configure {
-    kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
-}
-
-tasks.withType(KotlinCompilationTask::class.java).configureEach {
-    if (name != "kspCommonMainKotlinMetadata") {
-        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
