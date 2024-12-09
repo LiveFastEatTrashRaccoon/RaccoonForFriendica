b3194bc8fcc96ed07e7559e4ce6ae8c022778d04
akesi seli <andongue.nena@gmail.com>
2024-12-09 11:01:49 +0100

    apply convention plugins to feature modules

--- feature/nodeinfo/build.gradle.kts
+++ feature/nodeinfo/build.gradle.kts
@@ -1,48 +1,15 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.kotlinx.kover)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.mokkery)
+    id("com.livefast.eattrash.kotlinMultiplatform")
+    id("com.livefast.eattrash.composeMultiplatform")
+    id("com.livefast.eattrash.koinWithKsp")
+    id("com.livefast.eattrash.test")
+    id("com.livefast.eattrash.uiTest")
 }
 
-@OptIn(ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    applyDefaultHierarchyTemplate()
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "feature.nodeinfo"
-            isStatic = true
-        }
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material)
-                implementation(compose.material3)
-                implementation(compose.materialIconsExtended)
-
-                implementation(libs.koin.core)
-                api(libs.koin.annotations)
                 implementation(libs.voyager.screenmodel)
                 implementation(libs.voyager.koin)
                 implementation(libs.lyricist)
@@ -61,69 +28,5 @@ kotlin {
                 implementation(projects.domain.identity.repository)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                dependencies {
-                    implementation(kotlin("test"))
-                    implementation(libs.kotlinx.coroutines.test)
-                    implementation(libs.turbine)
-                    implementation(projects.core.testutils)
-                }
-            }
-        }
-        val androidUnitTest by getting {
-            dependencies {
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-                implementation(compose.uiTest)
-                implementation(libs.compose.ui.test)
-                implementation(libs.robolectric)
-                implementation(projects.core.testutils)
-            }
-        }
-    }
-
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-    }
-}
-
-dependencies {
-    debugImplementation(libs.compose.ui.test.manifest)
-
-    add("kspCommonMainMetadata", libs.koin.ksp)
-    add("kspAndroid", libs.koin.ksp)
-    add("kspIosX64", libs.koin.ksp)
-    add("kspIosArm64", libs.koin.ksp)
-    add("kspIosSimulatorArm64", libs.koin.ksp)
-}
-
-ksp {
-    arg("KOIN_DEFAULT_MODULE", "false")
-}
-
-android {
-    namespace = "com.livefast.eattrash.raccoonforfriendica.feature.nodeinfo"
-    compileSdk =
-        libs.versions.android.compileSdk
-            .get()
-            .toInt()
-    defaultConfig {
-        minSdk =
-            libs.versions.android.minSdk
-                .get()
-                .toInt()
-        testOptions.unitTests.isIncludeAndroidResources = true
-        testInstrumentationRunner = "org.robolectric.RobolectricTestRunner"
-    }
-}
-
-kotlin.sourceSets.commonMain.configure {
-    kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
-}
-
-tasks.withType(KotlinCompilationTask::class.java).configureEach {
-    if (name != "kspCommonMainKotlinMetadata") {
-        dependsOn("kspCommonMainKotlinMetadata")
     }
 }
