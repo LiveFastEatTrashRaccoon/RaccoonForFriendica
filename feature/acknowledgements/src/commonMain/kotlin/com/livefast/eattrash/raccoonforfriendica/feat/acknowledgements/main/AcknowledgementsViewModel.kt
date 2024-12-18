bdb5a39b5968d8239aeb26dc3bed0377ab06ef94
akesi seli <andongue.nena@gmail.com>
2024-12-18 18:00:28 +0100

    add view model implementation

---
+++ feature/acknowledgements/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feat/acknowledgements/main/AcknowledgementsViewModel.kt
@@ -0,0 +1,47 @@
+package com.livefast.eattrash.raccoonforfriendica.feat.acknowledgements.main
+
+import cafe.adriel.voyager.core.model.screenModelScope
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.DefaultMviModel
+import com.livefast.eattrash.raccoonforfriendica.feat.acknowledgements.repository.AcknowledgementsRepository
+import kotlinx.coroutines.launch
+import org.koin.core.annotation.Factory
+
+@Factory(binds = [AcknowledgementsMviModel::class])
+internal class AcknowledgementsViewModel(
+    private val acknowledgementsRepository: AcknowledgementsRepository,
+) : DefaultMviModel<AcknowledgementsMviModel.Intent, AcknowledgementsMviModel.State, AcknowledgementsMviModel.Effect>(
+        initialState = AcknowledgementsMviModel.State(),
+    ),
+    AcknowledgementsMviModel {
+    init {
+        screenModelScope.launch {
+            refresh(initial = true)
+        }
+    }
+
+    override fun reduce(intent: AcknowledgementsMviModel.Intent) {
+        when (intent) {
+            AcknowledgementsMviModel.Intent.Refresh ->
+                screenModelScope.launch {
+                    refresh()
+                }
+        }
+    }
+
+    private suspend fun refresh(initial: Boolean = false) {
+        updateState {
+            it.copy(
+                initial = initial,
+                refreshing = !initial,
+            )
+        }
+        val items = acknowledgementsRepository.getAll()
+        updateState {
+            it.copy(
+                items = items.orEmpty(),
+                initial = false,
+                refreshing = false,
+            )
+        }
+    }
+}
