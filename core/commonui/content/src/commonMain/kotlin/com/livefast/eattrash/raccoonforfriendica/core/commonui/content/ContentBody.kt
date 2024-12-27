0bba406e7a46eca3c6c911d34c61b5a4b7dff4ae
Dieguitux <andongue.nena@gmail.com>
2024-12-27 23:16:04 +0100

    fix: process embedded images separately from text flow in post body (#692)
    * move common logic to separate file
    
    * remove embedded image processing from TextWithCustomEmojis composable
    
    * process text and images separately in post body

--- core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/ContentBody.kt
+++ core/commonui/content/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/content/ContentBody.kt
@@ -1,14 +1,23 @@
 package com.livefast.eattrash.raccoonforfriendica.core.commonui.content
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.CornerSize
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.ancillaryTextAlpha
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.CustomImage
 import com.livefast.eattrash.raccoonforfriendica.core.htmlparse.parseHtml
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.EmojiModel
+
+internal val IMAGE_REGEX = Regex("<img.*?/>")
 
 @Composable
 fun ContentBody(
@@ -19,33 +28,78 @@ fun ContentBody(
     autoloadImages: Boolean = true,
     emojis: List<EmojiModel> = emptyList(),
     onClick: (() -> Unit)? = null,
+    onOpenImage: ((String) -> Unit)? = null,
     onOpenUrl: ((String) -> Unit)? = null,
 ) {
     Box(modifier = modifier) {
-        val annotatedContent =
-            content.parseHtml(
-                linkColor = MaterialTheme.colorScheme.primary,
-                quoteColor = MaterialTheme.colorScheme.onBackground.copy(ancillaryTextAlpha),
-            )
-        TextWithCustomEmojis(
-            style = MaterialTheme.typography.bodyMedium.copy(color = color),
-            text = annotatedContent,
-            maxLines = maxLines,
-            overflow = TextOverflow.Ellipsis,
-            emojis = emojis,
-            autoloadImages = autoloadImages,
-            onClick = { offset ->
-                val url =
-                    annotatedContent
-                        .getStringAnnotations(start = offset, end = offset)
-                        .firstOrNull()
-                        ?.item
-                if (!url.isNullOrBlank()) {
-                    onOpenUrl?.invoke(url)
+        val chunks = content.splitTextAndImages()
+        Column {
+            for (chunk in chunks) {
+                if (IMAGE_REGEX.matches(chunk)) {
+                    extractImageData(chunk)?.also { data ->
+                        CustomImage(
+                            modifier =
+                                Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(CornerSize.xl))
+                                    .clickable {
+                                        onOpenImage?.invoke(data.url)
+                                    },
+                            url = data.url,
+                            contentDescription = data.description,
+                        )
+                    }
                 } else {
-                    onClick?.invoke()
+                    val annotatedContent =
+                        chunk.parseHtml(
+                            linkColor = MaterialTheme.colorScheme.primary,
+                            quoteColor =
+                                MaterialTheme.colorScheme.onBackground.copy(
+                                    ancillaryTextAlpha,
+                                ),
+                        )
+                    TextWithCustomEmojis(
+                        style = MaterialTheme.typography.bodyMedium.copy(color = color),
+                        text = annotatedContent,
+                        maxLines = maxLines,
+                        overflow = TextOverflow.Ellipsis,
+                        emojis = emojis,
+                        autoloadImages = autoloadImages,
+                        onClick = { offset ->
+                            val url =
+                                annotatedContent
+                                    .getStringAnnotations(start = offset, end = offset)
+                                    .firstOrNull()
+                                    ?.item
+                            if (!url.isNullOrBlank()) {
+                                onOpenUrl?.invoke(url)
+                            } else {
+                                onClick?.invoke()
+                            }
+                        },
+                    )
                 }
-            },
-        )
+            }
+        }
     }
 }
+
+private fun String.splitTextAndImages(): List<String> =
+    buildList {
+        val original = this@splitTextAndImages
+        val matches = IMAGE_REGEX.findAll(original).toList()
+        var index = 0
+        for (match in matches) {
+            val range = match.range
+            add(original.substring(index, range.first).trim())
+            val htmlImage = original.substring(range)
+            val data = extractImageData(htmlImage)
+            if (data != null && data.description?.looksLikeAnEmoji != true) {
+                add(htmlImage.trim())
+            }
+            index = range.last + 1
+        }
+        if (index < original.length) {
+            add(original.substring(index, original.length).trim())
+        }
+    }
