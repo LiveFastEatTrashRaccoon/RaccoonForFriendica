994a544e80a5fc092471e17415d4aa9598d7293f
akesi seli <andongue.nena@gmail.com>
2025-01-04 10:09:49 +0100

    update DI bindings

--- feature/settings/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/settings/di/SettingsModule.kt
+++ feature/settings/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/settings/di/SettingsModule.kt
@@ -4,16 +4,19 @@ import com.livefast.eattrash.raccoonforfriendica.feature.settings.SettingsMviMod
 import com.livefast.eattrash.raccoonforfriendica.feature.settings.SettingsViewModel
 import com.livefast.eattrash.raccoonforfriendica.feature.settings.feedback.UserFeedbackMviModel
 import com.livefast.eattrash.raccoonforfriendica.feature.settings.feedback.UserFeedbackViewModel
+import dev.icerock.moko.permissions.PermissionsController
 import org.kodein.di.DI
 import org.kodein.di.bind
+import org.kodein.di.factory
 import org.kodein.di.instance
 import org.kodein.di.provider
 
 val settingsModule =
     DI.Module("SettingsModule") {
         bind<SettingsMviModel> {
-            provider {
+            factory { controller: PermissionsController ->
                 SettingsViewModel(
+                    permissionController = controller,
                     settingsRepository = instance(),
                     l10nManager = instance(),
                     themeRepository = instance(),
