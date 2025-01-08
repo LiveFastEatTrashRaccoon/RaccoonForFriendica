93060877927afed79b6a2e4ef5131450bf20198f
akesi seli <andongue.nena@gmail.com>
2025-01-08 22:17:32 +0100

    update CustomColorPickerDialog

--- core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/components/CustomColorPickerDialog.kt
+++ core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/components/CustomColorPickerDialog.kt
@@ -11,9 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
@@ -40,6 +45,7 @@ import com.livefast.eattrash.raccoonforfriendica.core.l10n.LocalStrings
 @Composable
 fun CustomColorPickerDialog(
     initialValue: Color,
+    allowManualSelection: Boolean = false,
     onClose: ((Color?) -> Unit)? = null,
 ) {
     val controller =
@@ -47,6 +53,7 @@ fun CustomColorPickerDialog(
         }
     var selectedColor by remember { mutableStateOf(initialValue) }
     var selectedColorHex by remember { mutableStateOf("") }
+    var manualInputDialogOpen by remember { mutableStateOf(false) }
 
     BasicAlertDialog(
         modifier = Modifier.clip(RoundedCornerShape(CornerSize.xxl)),
@@ -84,24 +91,42 @@ fun CustomColorPickerDialog(
                 },
             )
 
-            Box(
-                modifier =
-                    Modifier
-                        .border(
-                            color = selectedColor,
-                            width = Dp.Hairline,
-                            shape = RoundedCornerShape(CornerSize.xxl),
-                        ).padding(
-                            horizontal = Spacing.m,
-                            vertical = Spacing.s,
-                        ),
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(
-                    text = "#$selectedColorHex",
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight.SemiBold,
-                    color = selectedColor,
-                )
+                Box(
+                    modifier =
+                        Modifier
+                            .border(
+                                color = selectedColor,
+                                width = Dp.Hairline,
+                                shape = RoundedCornerShape(CornerSize.xxl),
+                            ).padding(
+                                horizontal = Spacing.m,
+                                vertical = Spacing.s,
+                            ),
+                ) {
+                    SelectionContainer {
+                        Text(
+                            text = "#$selectedColorHex",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.SemiBold,
+                            color = selectedColor,
+                        )
+                    }
+                }
+                if (allowManualSelection) {
+                    FilledIconButton(
+                        onClick = {
+                            manualInputDialogOpen = true
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = LocalStrings.current.actionEdit,
+                        )
+                    }
+                }
             }
 
             Spacer(modifier = Modifier.height(Spacing.s))
@@ -126,5 +151,53 @@ fun CustomColorPickerDialog(
                 }
             }
         }
+    }
+
+    if (manualInputDialogOpen) {
+        var isError by remember { mutableStateOf(false) }
+        var label by remember { mutableStateOf("") }
+        val invalidFieldMessage = LocalStrings.current.messageInvalidField
+
+        EditTextualInfoDialog(
+            title = LocalStrings.current.colorPickerDialogInsertHex,
+            value = selectedColorHex,
+            label = label,
+            isError = isError,
+            singleLine = true,
+            onClose = { value ->
+                val newColor = value?.toColor()
+                when {
+                    value != null && newColor == null -> {
+                        isError = true
+                        label = invalidFieldMessage
+                    }
+
+                    newColor != null -> {
+                        isError = false
+                        label = ""
+                        selectedColorHex = value
+                        selectedColor = newColor
+                        manualInputDialogOpen = false
+                    }
+
+                    else -> manualInputDialogOpen = false
+                }
+            },
+        )
+    }
+}
+
+private fun String.toColor(): Color? {
+    val sanitized =
+        if (startsWith("#")) {
+            substring(1)
+        } else {
+            this
+        }
+    return when {
+        sanitized.length == 6 || sanitized.length == 8 ->
+            sanitized.toLongOrNull(16)?.let { Color(it) }
+
+        else -> null
     }
 }
