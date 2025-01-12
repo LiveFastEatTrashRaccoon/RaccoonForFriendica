bc451f11b1bdae33ebf1d97855520110948efe58
akesi seli <andongue.nena@gmail.com>
2025-01-12 09:18:10 +0100

    enhancement: avoid waiting for completion in content preload

--- domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/DefaultContentPreloadManager.kt
+++ domain/identity/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/usecase/DefaultContentPreloadManager.kt
@@ -3,17 +3,27 @@ package com.livefast.eattrash.raccoonforfriendica.domain.identity.usecase
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.NotificationRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.TimelineEntryRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.TrendingRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 internal class DefaultContentPreloadManager(
     private val timelineEntryRepository: TimelineEntryRepository,
     private val trendingRepository: TrendingRepository,
     private val notificationRepository: NotificationRepository,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ContentPreloadManager {
-    override suspend fun preload(userRemoteId: String?) =
-        coroutineScope {
+    private val scope = CoroutineScope(SupervisorJob() + dispatcher)
+
+    override suspend fun preload(userRemoteId: String?) {
+        // actually, it is not so important to wait for completion
+        // timelineEntryRepository.getByUser may take a long time
+        scope.launch {
             val tasks =
                 buildList {
                     this +=
@@ -44,4 +54,5 @@ internal class DefaultContentPreloadManager(
 
             tasks.awaitAll().let {}
         }
+    }
 }
