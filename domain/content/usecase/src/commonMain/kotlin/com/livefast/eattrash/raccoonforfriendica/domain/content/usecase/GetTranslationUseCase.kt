a4f8ca841b2e5ad0df5c85be9dedbe40de46801c
akesi seli <andongue.nena@gmail.com>
2025-01-19 14:48:23 +0100

    define use case to get translation

---
+++ domain/content/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/usecase/GetTranslationUseCase.kt
@@ -0,0 +1,11 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.content.usecase
+
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
+import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TranslatedTimelineEntryModel
+
+interface GetTranslationUseCase {
+    suspend operator fun invoke(
+        entry: TimelineEntryModel,
+        targetLang: String,
+    ): TranslatedTimelineEntryModel?
+}
