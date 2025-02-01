LiveFastEatTrashRaccoon/RaccoonForFriendica

commit de7184ee489eaea1824aefd747f85bc4bba221a9

fix: post detail cell spacing when opened from inbox, explore and other posts (#781)
* remove unused variable

* fix spacing between cells when entry detail is open without swipe navigation

* fix spacing between cells in thread screen too

feature/entrydetail/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/entrydetail/EntryDetailScreen.kt
@@ -282,7 +282,6 @@ class EntryDetailScreen(
                     },
                 ) {
                     LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
                         state = lazyListState,
                     ) {
                         itemsIndexed(
@@ -294,6 +293,7 @@ class EntryDetailScreen(
                                 modifier =
                                     Modifier.then(
                                         if (isMainEntry && uiState.layout == TimelineLayout.Card) {
+                                            // since the main entry is forced to "full", recreates card appearance
                                             Modifier
                                                 .padding(horizontal = Spacing.xs)
                                                 .shadow(
@@ -549,7 +549,7 @@ class EntryDetailScreen(
                                     }
                                 },
                             )
-                            if (idx < uiState.entries.lastIndex) {
+                            if (idx < currentEntryList.lastIndex) {
                                 TimelineDivider(layout = uiState.layout)
                             }
                         }

feature/inbox/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/inbox/InboxScreen.kt
@@ -196,7 +196,7 @@ class InboxScreen : Screen {
                 ) {
                     if (uiState.initial) {
                         val placeholderCount = 5
-                        items(placeholderCount) { idx ->
+                        items(placeholderCount) {
                             NotificationItemPlaceholder(modifier = Modifier.fillMaxWidth())
                         }
                     }

feature/thread/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/thread/ThreadScreen.kt
@@ -711,7 +711,7 @@ class ThreadScreen(
                                     }
                                 }
                             }
-                            if (idx < uiState.replies.lastIndex) {
+                            if (idx < replies.lastIndex) {
                                 TimelineDivider(layout = uiState.layout)
                             }
                         }

commit 06f9cbb2bebe40881cbcc494d3250feb19cdeb06

fix: change timeline type in homepage (#782)
* fix visual bug introduced by #781 in post detail

* fix: ignore loading state when changing timeline type

feature/entrydetail/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/entrydetail/EntryDetailScreen.kt
@@ -282,6 +282,7 @@ class EntryDetailScreen(
                     },
                 ) {
                     LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
                         state = lazyListState,
                     ) {
                         itemsIndexed(

feature/timeline/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/timeline/TimelineScreen.kt
@@ -564,10 +564,11 @@ class TimelineScreen : Screen {
         }
 
         if (timelineTypeSelectorOpen) {
+            val items = uiState.availableTimelineTypes
             CustomModalBottomSheet(
                 title = LocalStrings.current.feedTypeTitle,
                 items =
-                    uiState.availableTimelineTypes.map {
+                    items.map {
                         CustomModalBottomSheetItem(
                             label = it.toReadableName(),
                             trailingContent = {
@@ -583,7 +584,7 @@ class TimelineScreen : Screen {
                 onSelected = { index ->
                     timelineTypeSelectorOpen = false
                     if (index != null) {
-                        val type = uiState.availableTimelineTypes[index]
+                        val type = items[index]
                         model.reduce(TimelineMviModel.Intent.ChangeType(type))
                     }
                 },

feature/timeline/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/timeline/TimelineViewModel.kt
@@ -13,7 +13,6 @@ import com.livefast.eattrash.raccoonforfriendica.core.utils.vibrate.HapticFeedba
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineEntryModel
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.TimelineType
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.blurHashParamsForPreload
-import com.livefast.eattrash.raccoonforfriendica.domain.content.data.nodeName
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.original
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.toTimelineType
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.urlsForPreload
@@ -208,10 +207,6 @@ class TimelineViewModel(
     }
 
     private suspend fun changeTimelineType(type: TimelineType) {
-        if (uiState.value.loading) {
-            return
-        }
-
         updateState {
             it.copy(
                 initial = true,