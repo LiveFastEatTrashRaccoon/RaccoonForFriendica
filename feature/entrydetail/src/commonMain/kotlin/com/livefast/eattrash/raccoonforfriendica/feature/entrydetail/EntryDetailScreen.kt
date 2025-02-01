LiveFastEatTrashRaccoon/RaccoonForFriendica

commit de7184ee489eaea1824aefd747f85bc4bba221a9

fix: post detail cell spacing when opened from inbox, explore and other posts (#781)
* remove unused variable

* fix spacing between cells when entry detail is open without swipe navigation

* fix spacing between cells in thread screen too

feature/entrydetail/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/entrydetail/EntryDetailScreen.kt
@@ -282,7 +282,6 @@ class EntryDetailScreen(
                     },
                 ) {
                     LazyColumn(
-                        modifier = Modifier.fillMaxSize(),
                         state = lazyListState,
                     ) {
                         itemsIndexed(
@@ -294,6 +293,7 @@ class EntryDetailScreen(
                                 modifier =
                                     Modifier.then(
                                         if (isMainEntry && uiState.layout == TimelineLayout.Card) {
+                                            // since the main entry is forced to "full", recreates card appearance
                                             Modifier
                                                 .padding(horizontal = Spacing.xs)
                                                 .shadow(
@@ -549,7 +549,7 @@ class EntryDetailScreen(
                                     }
                                 },
                             )
-                            if (idx < uiState.entries.lastIndex) {
+                            if (idx < currentEntryList.lastIndex) {
                                 TimelineDivider(layout = uiState.layout)
                             }
                         }

feature/inbox/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/inbox/InboxScreen.kt
@@ -196,7 +196,7 @@ class InboxScreen : Screen {
                 ) {
                     if (uiState.initial) {
                         val placeholderCount = 5
-                        items(placeholderCount) { idx ->
+                        items(placeholderCount) {
                             NotificationItemPlaceholder(modifier = Modifier.fillMaxWidth())
                         }
                     }

feature/thread/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/thread/ThreadScreen.kt
@@ -711,7 +711,7 @@ class ThreadScreen(
                                     }
                                 }
                             }
-                            if (idx < uiState.replies.lastIndex) {
+                            if (idx < replies.lastIndex) {
                                 TimelineDivider(layout = uiState.layout)
                             }
                         }

commit c650c52be29e4731bcc0e79a245968a9884a83e3

fix visual bug introduced by #781 in post detail

feature/entrydetail/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/entrydetail/EntryDetailScreen.kt
@@ -282,6 +282,7 @@ class EntryDetailScreen(
                     },
                 ) {
                     LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
                         state = lazyListState,
                     ) {
                         itemsIndexed(