04ad1e8c37d740c85d120c2921b6fab3cd2844a7
akesi seli <andongue.nena@gmail.com>
2025-03-10 09:15:26 +0100

    remove unused function

--- domain/identity/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/repository/di/Utils.kt
+++ domain/identity/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/identity/repository/di/Utils.kt
@@ -1,15 +1,9 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.di
 
 import com.livefast.eattrash.raccoonforfriendica.core.di.RootDI
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.ApiConfigurationRepository
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AuthManager
 import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.SettingsRepository
 import org.kodein.di.instance
-
-fun getApiConfigurationRepository(): ApiConfigurationRepository {
-    val res by RootDI.di.instance<ApiConfigurationRepository>()
-    return res
-}
 
 fun getSettingsRepository(): SettingsRepository {
     val res by RootDI.di.instance<SettingsRepository>()
