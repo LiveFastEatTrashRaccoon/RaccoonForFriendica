3cfbe119977d90ad82112afd6de77a765f6bdee5
Dieguitux <andongue.nena@gmail.com>
2025-06-14 17:20:54 +0200

    update safeImePadding custom Modifier

--- core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/compose/SafeImePadding.kt
+++ core/utils/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/compose/SafeImePadding.kt
@@ -1,5 +1,7 @@
 package com.livefast.eattrash.raccoonforfriendica.core.utils.compose
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@Composable
 expect fun Modifier.safeImePadding(): Modifier
--- core/utils/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/compose/SafeImePadding.kt
+++ core/utils/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/utils/compose/SafeImePadding.kt
@@ -1,6 +1,8 @@
 package com.livefast.eattrash.raccoonforfriendica.core.utils.compose
 
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@Composable
 actual fun Modifier.safeImePadding(): Modifier = then(Modifier.imePadding())
