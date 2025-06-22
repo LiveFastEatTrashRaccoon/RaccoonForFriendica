efca0f2a3db0f737068defbf7676cbfbd63f6156
Dieguitux <andongue.nena@gmail.com>
2025-06-22 16:34:27 +0200

    update VMs in UnpublishedModule

--- feature/unpublished/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/unpublished/di/UnpublishedModule.kt
+++ feature/unpublished/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/unpublished/di/UnpublishedModule.kt
@@ -1,27 +1,23 @@
 package com.livefast.eattrash.raccoonforfriendica.feature.unpublished.di
 
-import com.livefast.eattrash.raccoonforfriendica.feature.unpublished.UnpublishedMviModel
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.di.bindViewModel
 import com.livefast.eattrash.raccoonforfriendica.feature.unpublished.UnpublishedViewModel
 import org.kodein.di.DI
-import org.kodein.di.bind
 import org.kodein.di.instance
-import org.kodein.di.provider
 
 val unpublishedModule =
     DI.Module("UnpublishedModule") {
-        bind<UnpublishedMviModel> {
-            provider {
-                UnpublishedViewModel(
-                    paginationManager = instance(),
-                    identityRepository = instance(),
-                    settingsRepository = instance(),
-                    scheduledEntryRepository = instance(),
-                    draftRepository = instance(),
-                    imagePreloadManager = instance(),
-                    blurHashRepository = instance(),
-                    imageAutoloadObserver = instance(),
-                    notificationCenter = instance(),
-                )
-            }
+        bindViewModel {
+            UnpublishedViewModel(
+                paginationManager = instance(),
+                identityRepository = instance(),
+                settingsRepository = instance(),
+                scheduledEntryRepository = instance(),
+                draftRepository = instance(),
+                imagePreloadManager = instance(),
+                blurHashRepository = instance(),
+                imageAutoloadObserver = instance(),
+                notificationCenter = instance(),
+            )
         }
     }
