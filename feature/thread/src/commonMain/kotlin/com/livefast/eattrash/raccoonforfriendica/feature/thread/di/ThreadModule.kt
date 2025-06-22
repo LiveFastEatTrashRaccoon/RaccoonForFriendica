5364dc49b15c0c15ee961f220b0ad38962026b1e
Dieguitux <andongue.nena@gmail.com>
2025-06-22 16:32:33 +0200

    update VMs in ThreadModule

--- feature/thread/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/thread/di/ThreadModule.kt
+++ feature/thread/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/thread/di/ThreadModule.kt
@@ -1,41 +1,38 @@
 package com.livefast.eattrash.raccoonforfriendica.feature.thread.di
 
-import com.livefast.eattrash.raccoonforfriendica.feature.thread.ThreadMviModel
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.di.ViewModelCreationArgs
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.di.bindViewModelWithArgs
 import com.livefast.eattrash.raccoonforfriendica.feature.thread.ThreadViewModel
 import org.kodein.di.DI
-import org.kodein.di.bind
-import org.kodein.di.factory
 import org.kodein.di.instance
 
-data class ThreadMviModelParams(val entryId: String, val swipeNavigationEnabled: Boolean)
+data class ThreadViewModelArgs(val entryId: String, val swipeNavigationEnabled: Boolean) : ViewModelCreationArgs
 
 val threadModule =
     DI.Module("ThreadModule") {
-        bind<ThreadMviModel> {
-            factory { params: ThreadMviModelParams ->
-                ThreadViewModel(
-                    entryId = params.entryId,
-                    swipeNavigationEnabled = params.swipeNavigationEnabled,
-                    timelineEntryRepository = instance(),
-                    identityRepository = instance(),
-                    settingsRepository = instance(),
-                    userRepository = instance(),
-                    apiConfigurationRepository = instance(),
-                    accountRepository = instance(),
-                    instanceShortcutRepository = instance(),
-                    hapticFeedback = instance(),
-                    entryCache = instance(),
-                    imagePreloadManager = instance(),
-                    blurHashRepository = instance(),
-                    imageAutoloadObserver = instance(),
-                    populateThread = instance(),
-                    toggleEntryFavorite = instance(),
-                    toggleEntryDislike = instance(),
-                    getTranslation = instance(),
-                    getInnerUrl = instance(),
-                    timelineNavigationManager = instance(),
-                    notificationCenter = instance(),
-                )
-            }
+        bindViewModelWithArgs { args: ThreadViewModelArgs ->
+            ThreadViewModel(
+                entryId = args.entryId,
+                swipeNavigationEnabled = args.swipeNavigationEnabled,
+                timelineEntryRepository = instance(),
+                identityRepository = instance(),
+                settingsRepository = instance(),
+                userRepository = instance(),
+                apiConfigurationRepository = instance(),
+                accountRepository = instance(),
+                instanceShortcutRepository = instance(),
+                hapticFeedback = instance(),
+                entryCache = instance(),
+                imagePreloadManager = instance(),
+                blurHashRepository = instance(),
+                imageAutoloadObserver = instance(),
+                populateThread = instance(),
+                toggleEntryFavorite = instance(),
+                toggleEntryDislike = instance(),
+                getTranslation = instance(),
+                getInnerUrl = instance(),
+                timelineNavigationManager = instance(),
+                notificationCenter = instance(),
+            )
         }
     }
