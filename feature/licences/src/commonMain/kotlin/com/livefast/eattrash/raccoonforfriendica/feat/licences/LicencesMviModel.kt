8f47e52e596ac7a20140c85847933c9f425874f9
Dieguitux <andongue.nena@gmail.com>
2025-06-22 15:53:25 +0200

    update VMs in LicenceModule

--- feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feat/licences/LicencesMviModel.kt
+++ feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feat/licences/LicencesMviModel.kt
@@ -1,12 +1,9 @@
 package com.livefast.eattrash.raccoonforfriendica.feat.licences
 
-import cafe.adriel.voyager.core.model.ScreenModel
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
 import com.livefast.eattrash.raccoonforfriendica.feat.licences.models.LicenceItem
 
-interface LicencesMviModel :
-    ScreenModel,
-    MviModel<LicencesMviModel.Intent, LicencesMviModel.State, LicencesMviModel.Effect> {
+interface LicencesMviModel : MviModel<LicencesMviModel.Intent, LicencesMviModel.State, LicencesMviModel.Effect> {
     sealed interface Intent
 
     data class State(val items: List<LicenceItem> = emptyList(), val hideNavigationBarWhileScrolling: Boolean = true)
--- feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feat/licences/di/LicenceModule.kt
+++ feature/licences/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feat/licences/di/LicenceModule.kt
@@ -1,19 +1,15 @@
 package com.livefast.eattrash.raccoonforfriendica.feat.licences.di
 
-import com.livefast.eattrash.raccoonforfriendica.feat.licences.LicencesMviModel
+import com.livefast.eattrash.raccoonforfriendica.core.architecture.di.bindViewModel
 import com.livefast.eattrash.raccoonforfriendica.feat.licences.LicencesViewModel
 import org.kodein.di.DI
-import org.kodein.di.bind
 import org.kodein.di.instance
-import org.kodein.di.provider
 
 val licenceModule =
     DI.Module("LicenceModule") {
-        bind<LicencesMviModel> {
-            provider {
-                LicencesViewModel(
-                    settingsRepository = instance(),
-                )
-            }
+        bindViewModel {
+            LicencesViewModel(
+                settingsRepository = instance(),
+            )
         }
     }
