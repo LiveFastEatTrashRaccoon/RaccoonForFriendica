5acb2d313426adea79dcd7e595108c448cdae25e
Dieguitux <andongue.nena@gmail.com>
2025-06-24 21:08:06 +0200

    update CircleMembersScreen

--- feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/editmembers/CircleMembersScreen.kt
+++ feature/circles/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/circles/editmembers/CircleMembersScreen.kt
@@ -42,8 +42,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
-import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.core.screen.ScreenKey
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.toWindowInsets
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.di.getViewModel
@@ -55,7 +53,7 @@ import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.UserItem
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.UserItemPlaceholder
 import com.livefast.eattrash.raccoonforfriendica.core.commonui.content.toOption
 import com.livefast.eattrash.raccoonforfriendica.core.l10n.LocalStrings
-import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getDetailOpener
+import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getMainRouter
 import com.livefast.eattrash.raccoonforfriendica.core.navigation.di.getNavigationCoordinator
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.components.CircleAddUserDialog
 import com.livefast.eattrash.raccoonforfriendica.feature.circles.di.CircleMembersViewModelArgs
@@ -63,228 +61,224 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class CircleMembersScreen(val id: String) : Screen {
-    override val key: ScreenKey
-        get() = super.key + id
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CircleMembersScreen(id: String, modifier: Modifier = Modifier) {
+    val model: CircleMembersMviModel =
+        getViewModel<CircleMembersViewModel>(arg = CircleMembersViewModelArgs(id))
+    val uiState by model.uiState.collectAsState()
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+    val navigationCoordinator = remember { getNavigationCoordinator() }
+    val lazyListState = rememberLazyListState()
+    val fabNestedScrollConnection = remember { getFabNestedScrollConnection() }
+    val isFabVisible by fabNestedScrollConnection.isFabVisible.collectAsState()
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val detailOpener = remember { getMainRouter() }
+    val genericError = LocalStrings.current.messageGenericError
+    var confirmRemoveUserId by remember { mutableStateOf<String?>(null) }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    override fun Content() {
-        val model: CircleMembersMviModel =
-            getViewModel<CircleMembersViewModel>(arg = CircleMembersViewModelArgs(id))
-        val uiState by model.uiState.collectAsState()
-        val topAppBarState = rememberTopAppBarState()
-        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
-        val navigationCoordinator = remember { getNavigationCoordinator() }
-        val lazyListState = rememberLazyListState()
-        val fabNestedScrollConnection = remember { getFabNestedScrollConnection() }
-        val isFabVisible by fabNestedScrollConnection.isFabVisible.collectAsState()
-        val scope = rememberCoroutineScope()
-        val snackbarHostState = remember { SnackbarHostState() }
-        val detailOpener = remember { getDetailOpener() }
-        val genericError = LocalStrings.current.messageGenericError
-        var confirmRemoveUserId by remember { mutableStateOf<String?>(null) }
-
-        fun goBackToTop() {
-            runCatching {
-                scope.launch {
-                    lazyListState.scrollToItem(0)
-                    topAppBarState.heightOffset = 0f
-                    topAppBarState.contentOffset = 0f
-                }
+    fun goBackToTop() {
+        runCatching {
+            scope.launch {
+                lazyListState.scrollToItem(0)
+                topAppBarState.heightOffset = 0f
+                topAppBarState.contentOffset = 0f
             }
         }
+    }
 
-        LaunchedEffect(model) {
-            model.effects
-                .onEach { event ->
-                    when (event) {
-                        CircleMembersMviModel.Effect.Failure ->
-                            snackbarHostState.showSnackbar(genericError)
-                    }
-                }.launchIn(this)
-        }
-
-        Scaffold(
-            contentWindowInsets = WindowInsets(0, 0, 0, 0),
-            topBar = {
-                TopAppBar(
-                    modifier = Modifier.clickable { scope.launch { goBackToTop() } },
-                    windowInsets = topAppBarState.toWindowInsets(),
-                    scrollBehavior = scrollBehavior,
-                    title = {
-                        Text(
-                            text = uiState.circle?.name.orEmpty(),
-                            style = MaterialTheme.typography.titleMedium,
-                        )
-                    },
-                    navigationIcon = {
-                        if (navigationCoordinator.canPop.value) {
-                            IconButton(
-                                onClick = {
-                                    navigationCoordinator.pop()
-                                },
-                            ) {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                                    contentDescription = LocalStrings.current.actionGoBack,
-                                )
-                            }
-                        }
-                    },
-                )
-            },
-            floatingActionButton = {
-                AnimatedVisibility(
-                    visible = isFabVisible,
-                    enter =
-                    slideInVertically(
-                        initialOffsetY = { it * 2 },
-                    ),
-                    exit =
-                    slideOutVertically(
-                        targetOffsetY = { it * 2 },
-                    ),
-                ) {
-                    FloatingActionButton(
-                        onClick = {
-                            model.reduce(CircleMembersMviModel.Intent.ToggleAddUsersDialog(true))
-                        },
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = LocalStrings.current.actionAddNew,
-                        )
-                    }
+    LaunchedEffect(model) {
+        model.effects
+            .onEach { event ->
+                when (event) {
+                    CircleMembersMviModel.Effect.Failure ->
+                        snackbarHostState.showSnackbar(genericError)
                 }
-            },
-            snackbarHost = {
-                SnackbarHost(
-                    hostState = snackbarHostState,
-                ) { data ->
-                    Snackbar(
-                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                        snackbarData = data,
+            }.launchIn(this)
+    }
+
+    Scaffold(
+        modifier = modifier,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.clickable { scope.launch { goBackToTop() } },
+                windowInsets = topAppBarState.toWindowInsets(),
+                scrollBehavior = scrollBehavior,
+                title = {
+                    Text(
+                        text = uiState.circle?.name.orEmpty(),
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                },
+                navigationIcon = {
+                    if (navigationCoordinator.canPop.value) {
+                        IconButton(
+                            onClick = {
+                                navigationCoordinator.pop()
+                            },
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = LocalStrings.current.actionGoBack,
+                            )
+                        }
+                    }
+                },
+            )
+        },
+        floatingActionButton = {
+            AnimatedVisibility(
+                visible = isFabVisible,
+                enter =
+                slideInVertically(
+                    initialOffsetY = { it * 2 },
+                ),
+                exit =
+                slideOutVertically(
+                    targetOffsetY = { it * 2 },
+                ),
+            ) {
+                FloatingActionButton(
+                    onClick = {
+                        model.reduce(CircleMembersMviModel.Intent.ToggleAddUsersDialog(true))
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = LocalStrings.current.actionAddNew,
                     )
                 }
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+            ) { data ->
+                Snackbar(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    snackbarData = data,
+                )
+            }
+        },
+    ) { padding ->
+        PullToRefreshBox(
+            modifier =
+            Modifier
+                .padding(padding)
+                .fillMaxWidth()
+                .then(
+                    if (uiState.hideNavigationBarWhileScrolling) {
+                        Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+                    } else {
+                        Modifier
+                    },
+                ).nestedScroll(fabNestedScrollConnection),
+            isRefreshing = uiState.refreshing,
+            onRefresh = {
+                model.reduce(CircleMembersMviModel.Intent.Refresh)
             },
-        ) { padding ->
-            PullToRefreshBox(
-                modifier =
-                Modifier
-                    .padding(padding)
-                    .fillMaxWidth()
-                    .then(
-                        if (uiState.hideNavigationBarWhileScrolling) {
-                            Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
-                        } else {
-                            Modifier
-                        },
-                    ).nestedScroll(fabNestedScrollConnection),
-                isRefreshing = uiState.refreshing,
-                onRefresh = {
-                    model.reduce(CircleMembersMviModel.Intent.Refresh)
-                },
+        ) {
+            LazyColumn(
+                state = lazyListState,
             ) {
-                LazyColumn(
-                    state = lazyListState,
-                ) {
-                    if (uiState.initial) {
-                        items(20) {
-                            UserItemPlaceholder(
-                                modifier = Modifier.fillMaxWidth(),
-                                withRelationshipButton = false,
-                            )
-                            Spacer(modifier = Modifier.height(Spacing.interItem))
-                        }
-                    }
-
-                    itemsIndexed(
-                        items = uiState.users,
-                        key = { _, e -> "circle-detail-${e.id}" },
-                    ) { _, user ->
-                        UserItem(
-                            user = user,
-                            autoloadImages = uiState.autoloadImages,
-                            onClick = {
-                                detailOpener.openUserDetail(user)
-                            },
-                            options =
-                            buildList {
-                                this += OptionId.Delete.toOption()
-                            },
-                            onSelectOption = { optionId ->
-                                when (optionId) {
-                                    OptionId.Delete -> confirmRemoveUserId = user.id
-                                    else -> Unit
-                                }
-                            },
+                if (uiState.initial) {
+                    items(20) {
+                        UserItemPlaceholder(
+                            modifier = Modifier.fillMaxWidth(),
+                            withRelationshipButton = false,
                         )
                         Spacer(modifier = Modifier.height(Spacing.interItem))
                     }
+                }
 
-                    item {
-                        if (uiState.loading && !uiState.refreshing) {
-                            Box(
-                                modifier = Modifier.fillMaxWidth(),
-                                contentAlignment = Alignment.Center,
-                            ) {
-                                ListLoadingIndicator()
+                itemsIndexed(
+                    items = uiState.users,
+                    key = { _, e -> "circle-detail-${e.id}" },
+                ) { _, user ->
+                    UserItem(
+                        user = user,
+                        autoloadImages = uiState.autoloadImages,
+                        onClick = {
+                            detailOpener.openUserDetail(user)
+                        },
+                        options =
+                        buildList {
+                            this += OptionId.Delete.toOption()
+                        },
+                        onSelectOption = { optionId ->
+                            when (optionId) {
+                                OptionId.Delete -> confirmRemoveUserId = user.id
+                                else -> Unit
                             }
-                        }
-                    }
-                    if (!uiState.initial && !uiState.refreshing && !uiState.loading && uiState.users.isEmpty()) {
-                        item {
-                            Text(
-                                modifier = Modifier.fillMaxWidth().padding(top = Spacing.m),
-                                text = LocalStrings.current.messageEmptyList,
-                                textAlign = TextAlign.Center,
-                                style = MaterialTheme.typography.bodyLarge,
-                            )
-                        }
-                    }
+                        },
+                    )
+                    Spacer(modifier = Modifier.height(Spacing.interItem))
+                }
 
-                    item {
-                        Spacer(modifier = Modifier.height(Spacing.xxxl))
+                item {
+                    if (uiState.loading && !uiState.refreshing) {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            ListLoadingIndicator()
+                        }
                     }
+                }
+                if (!uiState.initial && !uiState.refreshing && !uiState.loading && uiState.users.isEmpty()) {
+                    item {
+                        Text(
+                            modifier = Modifier.fillMaxWidth().padding(top = Spacing.m),
+                            text = LocalStrings.current.messageEmptyList,
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.bodyLarge,
+                        )
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.height(Spacing.xxxl))
                 }
             }
         }
+    }
 
-        if (confirmRemoveUserId != null) {
-            CustomConfirmDialog(
-                title = LocalStrings.current.actionDelete,
-                onClose = { confirm ->
-                    val userId = confirmRemoveUserId
-                    confirmRemoveUserId = null
-                    if (confirm && userId != null) {
-                        model.reduce(CircleMembersMviModel.Intent.Remove(userId))
-                    }
-                },
-            )
-        }
+    if (confirmRemoveUserId != null) {
+        CustomConfirmDialog(
+            title = LocalStrings.current.actionDelete,
+            onClose = { confirm ->
+                val userId = confirmRemoveUserId
+                confirmRemoveUserId = null
+                if (confirm && userId != null) {
+                    model.reduce(CircleMembersMviModel.Intent.Remove(userId))
+                }
+            },
+        )
+    }
 
-        if (uiState.addUsersDialogOpened) {
-            CircleAddUserDialog(
-                query = uiState.searchUsersQuery,
-                users = uiState.searchUsers,
-                autoloadImages = uiState.autoloadImages,
-                loading = uiState.userSearchLoading,
-                canFetchMore = uiState.userSearchCanFetchMore,
-                onLoadMoreUsers = {
-                    model.reduce(CircleMembersMviModel.Intent.UserSearchLoadNextPage)
-                },
-                onSearch = {
-                    model.reduce(CircleMembersMviModel.Intent.SetSearchUserQuery(it))
-                },
-                onClose = { values ->
-                    model.reduce(CircleMembersMviModel.Intent.ToggleAddUsersDialog(false))
-                    if (values != null) {
-                        model.reduce(CircleMembersMviModel.Intent.Add(values))
-                    }
-                },
-            )
-        }
+    if (uiState.addUsersDialogOpened) {
+        CircleAddUserDialog(
+            query = uiState.searchUsersQuery,
+            users = uiState.searchUsers,
+            autoloadImages = uiState.autoloadImages,
+            loading = uiState.userSearchLoading,
+            canFetchMore = uiState.userSearchCanFetchMore,
+            onLoadMoreUsers = {
+                model.reduce(CircleMembersMviModel.Intent.UserSearchLoadNextPage)
+            },
+            onSearch = {
+                model.reduce(CircleMembersMviModel.Intent.SetSearchUserQuery(it))
+            },
+            onClose = { values ->
+                model.reduce(CircleMembersMviModel.Intent.ToggleAddUsersDialog(false))
+                if (values != null) {
+                    model.reduce(CircleMembersMviModel.Intent.Add(values))
+                }
+            },
+        )
     }
 }
