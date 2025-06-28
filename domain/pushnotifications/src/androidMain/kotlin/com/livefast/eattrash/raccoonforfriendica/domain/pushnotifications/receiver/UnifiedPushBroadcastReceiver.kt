cc42582521755b6c66a7c80293d0ec48e42911ee
Dieguitux <andongue.nena@gmail.com>
2025-06-28 19:34:53 +0200

    simplify UnifiedPushBroadcastReceiver

--- domain/pushnotifications/src/androidMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/pushnotifications/receiver/UnifiedPushBroadcastReceiver.kt
+++ domain/pushnotifications/src/androidMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/pushnotifications/receiver/UnifiedPushBroadcastReceiver.kt
@@ -2,13 +2,7 @@ package com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications.recei
 
 import android.content.Context
 import com.livefast.eattrash.raccoonforfriendica.core.di.RootDI
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.repository.AccountRepository
-import com.livefast.eattrash.raccoonforfriendica.domain.pullnotifications.PullNotificationManager
-import com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications.manager.PushNotificationManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
+import com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications.common.UnifiedPushInteractor
 import org.kodein.di.instance
 import org.unifiedpush.android.connector.FailedReason
 import org.unifiedpush.android.connector.MessagingReceiver
@@ -16,42 +10,21 @@ import org.unifiedpush.android.connector.data.PushEndpoint
 import org.unifiedpush.android.connector.data.PushMessage
 
 class UnifiedPushBroadcastReceiver : MessagingReceiver() {
-    private val pullNotificationManager by RootDI.di.instance<PullNotificationManager>()
-    private val pushNotificationManager by RootDI.di.instance<PushNotificationManager>()
-    private val accountRepository by RootDI.di.instance<AccountRepository>()
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private val interactor by RootDI.di.instance<UnifiedPushInteractor>()
 
     override fun onMessage(context: Context, message: PushMessage, instance: String) {
-        pullNotificationManager.oneshotCheck()
+        interactor.onMessage(context = context, message = message, instance = instance)
     }
 
     override fun onNewEndpoint(context: Context, endpoint: PushEndpoint, instance: String) {
-        val accountId = instance.toLongOrNull() ?: return
-        scope.launch {
-            val account = accountRepository.getBy(accountId) ?: return@launch
-            endpoint.pubKeySet
-            pushNotificationManager.registerEndpoint(
-                account = account,
-                endpointUrl = endpoint.url,
-                pubKey = endpoint.pubKeySet?.pubKey.orEmpty(),
-                auth = endpoint.pubKeySet?.auth.orEmpty(),
-            )
-        }
+        interactor.onNewEndpoint(context = context, endpoint = endpoint, instance = instance)
     }
 
     override fun onRegistrationFailed(context: Context, reason: FailedReason, instance: String) {
-        val accountId = instance.toLongOrNull() ?: return
-        scope.launch {
-            val account = accountRepository.getBy(accountId) ?: return@launch
-            pushNotificationManager.unregisterEndpoint(account)
-        }
+        interactor.onRegistrationFailed(context = context, reason = reason, instance = instance)
     }
 
     override fun onUnregistered(context: Context, instance: String) {
-        val accountId = instance.toLongOrNull() ?: return
-        scope.launch {
-            val account = accountRepository.getBy(accountId) ?: return@launch
-            pushNotificationManager.unregisterEndpoint(account)
-        }
+        interactor.onUnregistered(context = context, instance = instance)
     }
 }
