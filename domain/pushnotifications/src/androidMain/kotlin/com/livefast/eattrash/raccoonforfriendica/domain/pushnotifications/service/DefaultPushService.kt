3bcb76118d341d9e719c8ac69f8869733b6e4589
Dieguitux <andongue.nena@gmail.com>
2025-06-28 19:35:11 +0200

    define PushService implementation

---
+++ domain/pushnotifications/src/androidMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/pushnotifications/service/DefaultPushService.kt
@@ -0,0 +1,29 @@
+package com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications.service
+
+import com.livefast.eattrash.raccoonforfriendica.core.di.RootDI
+import com.livefast.eattrash.raccoonforfriendica.domain.pushnotifications.common.UnifiedPushInteractor
+import org.kodein.di.instance
+import org.unifiedpush.android.connector.FailedReason
+import org.unifiedpush.android.connector.PushService
+import org.unifiedpush.android.connector.data.PushEndpoint
+import org.unifiedpush.android.connector.data.PushMessage
+
+class DefaultPushService : PushService() {
+    private val interactor by RootDI.di.instance<UnifiedPushInteractor>()
+
+    override fun onMessage(message: PushMessage, instance: String) {
+        interactor.onMessage(context = this, message = message, instance = instance)
+    }
+
+    override fun onNewEndpoint(endpoint: PushEndpoint, instance: String) {
+        interactor.onNewEndpoint(context = this, endpoint = endpoint, instance = instance)
+    }
+
+    override fun onRegistrationFailed(reason: FailedReason, instance: String) {
+        interactor.onRegistrationFailed(context = this, reason = reason, instance = instance)
+    }
+
+    override fun onUnregistered(instance: String) {
+        interactor.onUnregistered(context = this, instance = instance)
+    }
+}
