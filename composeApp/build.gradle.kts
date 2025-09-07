1145f5ab40e73b750a7280358b19a34dcd3f8fd0
Dieguitux <andongue.nena@gmail.com>
2025-09-07 15:15:27 +0200

    version 0.4.3-beta11

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -140,8 +140,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 100
-        versionName = "0.4.3-beta10"
+        versionCode = 101
+        versionName = "0.4.3-beta11"
     }
     base.archivesName = "RaccoonForFriendica"
     packaging {
