34177c3e65142b933943a1edd4d8244c26c3e998
Dieguitux <andongue.nena@gmail.com>
2026-03-29 12:00:40 +0200

    feat: expose API to read and set comment bar theme in ThemeRepository
    * add `CommentBarTheme` support to `ThemeRepository` and `DefaultThemeRepository`
    * implement color palettes for Green, Red, Blue, and Rainbow comment bar themes
    * add `changeCommentBarTheme` and `getCommentBarColors` methods to theme repositories
    * add unit test for comment bar theme selection

--- core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/repository/ThemeRepository.kt
+++ core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/repository/ThemeRepository.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.data.UiFontFamily
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.data.UiFontScale
 import com.livefast.eattrash.raccoonforfriendica.core.appearance.data.UiTheme
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.data.CommentBarTheme
 import kotlinx.coroutines.flow.StateFlow
 
 @Stable
@@ -14,6 +15,7 @@ interface ThemeRepository {
     val fontScale: StateFlow<UiFontScale>
     val dynamicColors: StateFlow<Boolean>
     val customSeedColor: StateFlow<Color?>
+    val commentBarTheme: StateFlow<CommentBarTheme>
 
     fun changeTheme(theme: UiTheme)
 
@@ -25,5 +27,9 @@ interface ThemeRepository {
 
     fun changeCustomSeedColor(color: Color?)
 
+    fun changeCommentBarTheme(theme: CommentBarTheme)
+
     fun getCommentBarColor(depth: Int): Color
+
+    fun getCommentBarColors(commentBarTheme: CommentBarTheme): List<Color>
 }
