05273d63957249cc093c4f73790f969b9063eaff
Dieguitux <andongue.nena@gmail.com>
2026-05-03 21:06:14 +0200

    fix: navigation destination state restoration (#1213)
    * feat(navigation): add Saver to BottomNavigationSection and Destination
    
    * fix: use Destination.Saver to persist selectedDestination

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/BottomNavigationSection.kt
+++ core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/BottomNavigationSection.kt
@@ -1,7 +1,10 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation
 
+import androidx.compose.runtime.saveable.Saver
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
+@Serializable
 sealed interface BottomNavigationSection {
     @Serializable
     data object Home : BottomNavigationSection
@@ -14,4 +17,11 @@ sealed interface BottomNavigationSection {
 
     @Serializable
     data object Profile : BottomNavigationSection
+
+    companion object {
+        val Saver = Saver<BottomNavigationSection, String>(
+            save = { Json.encodeToString(it) },
+            restore = { Json.decodeFromString<BottomNavigationSection>(it) },
+        )
+    }
 }
