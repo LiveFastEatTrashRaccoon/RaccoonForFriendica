cb79541cebdbebbbe8dae4e0b9dec3520b151efc
Dieguitux <andongue.nena@gmail.com>
2026-05-24 20:36:18 +0200

    refactor: update TopAppBar window inset calculation logic
    - Added `rememberMaxTopBarInset` to calculate insets based on status bar height.
    - Replaced `rememberCoroutineScope` with `LaunchedEffect` for collecting `collapsedFraction` changes.
    - Simplified DP/PX conversions in `toWindowInsets`.

--- core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/theme/TopAppBarWindowInsets.kt
+++ core/appearance/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/appearance/theme/TopAppBarWindowInsets.kt
@@ -1,30 +1,40 @@
 package com.livefast.eattrash.raccoonforfriendica.core.appearance.theme
 
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+
+@Composable
+fun rememberMaxTopBarInset(): Dp {
+    val density = LocalDensity.current
+    val statusBarHeight = WindowInsets.statusBars.getTop(density).let { with(density) { it.toDp() } }
+    return maxOf(statusBarHeight, Dimensions.maxTopBarInset)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBarState.toWindowInsets(): WindowInsets {
-    val scope = rememberCoroutineScope()
-    val maxTopInsetPx = with(LocalDensity.current) { Dimensions.maxTopBarInset.toPx() }
-    var topInsetPx by remember { mutableStateOf(maxTopInsetPx) }
-    val topInset = with(LocalDensity.current) { topInsetPx.toDp() }
-    snapshotFlow { collapsedFraction }
-        .onEach {
-            topInsetPx = maxTopInsetPx * (1 - it)
-        }.launchIn(scope)
+    val maxInset = rememberMaxTopBarInset()
+
+    var topInset by remember { mutableStateOf(maxInset) }
+
+    LaunchedEffect(maxInset, this) {
+        snapshotFlow {
+            maxInset * (1 - collapsedFraction)
+        }.collect {
+            topInset = it
+        }
+    }
     return WindowInsets(0.dp, topInset, 0.dp, 0.dp)
 }
