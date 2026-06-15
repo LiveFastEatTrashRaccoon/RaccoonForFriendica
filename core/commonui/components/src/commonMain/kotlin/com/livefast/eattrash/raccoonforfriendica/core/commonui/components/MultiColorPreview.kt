d13251c26fefb8423e7a304277fa201b7a9249aa
Dieguitux <andongue.nena@gmail.com>
2026-06-15 22:32:56 +0200

    add more previews to :core:commonui:components

--- core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/components/MultiColorPreview.kt
+++ core/commonui/components/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/commonui/components/MultiColorPreview.kt
@@ -1,9 +1,14 @@
 package com.livefast.eattrash.raccoonforfriendica.core.commonui.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.di.setupPreview
+import com.livefast.eattrash.raccoonforfriendica.core.di.RootDI
 
 @Composable
 fun MultiColorPreview(colors: List<Color>, modifier: Modifier = Modifier) {
@@ -22,4 +27,21 @@ fun MultiColorPreview(colors: List<Color>, modifier: Modifier = Modifier) {
             start += step
         }
     }
+}
+
+@Composable
+@Preview
+private fun MultiColorPreviewPreview() {
+    RootDI.setupPreview()
+    MultiColorPreview(
+        modifier = Modifier.size(24.dp),
+        colors = listOf(
+            Color(0xFF9400D3),
+            Color(0xFF0000FF),
+            Color(0xFF00FF00),
+            Color(0xFFFFFF00),
+            Color(0xFFFF7F00),
+            Color(0xFFFF0000),
+        ),
+    )
 }
