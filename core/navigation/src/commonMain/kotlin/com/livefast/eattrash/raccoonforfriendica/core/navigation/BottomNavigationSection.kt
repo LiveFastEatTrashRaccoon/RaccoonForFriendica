18601be1d259869b6fd6a00cf0956f9d3361a2c5
Dieguitux <andongue.nena@gmail.com>
2026-06-21 22:37:34 +0200

    update destinations

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/BottomNavigationSection.kt
+++ core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/BottomNavigationSection.kt
@@ -1,11 +1,15 @@
 package com.livefast.eattrash.raccoonforfriendica.core.navigation
 
 import androidx.compose.runtime.saveable.Saver
+import androidx.navigation3.runtime.NavKey
+import androidx.savedstate.serialization.SavedStateConfiguration
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
 
 @Serializable
-sealed interface BottomNavigationSection {
+sealed interface BottomNavigationSection : NavKey {
     @Serializable
     data object Home : BottomNavigationSection
 
@@ -19,6 +23,16 @@ sealed interface BottomNavigationSection {
     data object Profile : BottomNavigationSection
 
     companion object {
+        val SavedStateConfiguration = SavedStateConfiguration {
+            serializersModule = SerializersModule {
+                polymorphic(NavKey::class) {
+                    subclass(Home::class, Home.serializer())
+                    subclass(Explore::class, Explore.serializer())
+                    subclass(Inbox::class, Inbox.serializer())
+                    subclass(Profile::class, Profile.serializer())
+                }
+            }
+        }
         val Saver = Saver<BottomNavigationSection, String>(
             save = { Json.encodeToString(it) },
             restore = { Json.decodeFromString<BottomNavigationSection>(it) },
