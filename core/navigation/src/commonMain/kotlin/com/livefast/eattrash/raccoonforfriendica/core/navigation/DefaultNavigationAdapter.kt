6ca1a83c7d086f7b0548a44a0600e65b9e533bae
Dieguitux <andongue.nena@gmail.com>
2026-06-26 22:52:29 +0200

    refactor: remove throttling from navigation adapter

--- core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/DefaultNavigationAdapter.kt
+++ core/navigation/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/navigation/DefaultNavigationAdapter.kt
@@ -2,76 +2,39 @@ package com.livefast.eattrash.raccoonforfriendica.core.navigation
 
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 
-class DefaultNavigationAdapter(
-    private val backStack: NavBackStack<NavKey>,
-    dispatcher: CoroutineDispatcher = Dispatchers.Main,
-) : NavigationAdapter {
+class DefaultNavigationAdapter(private val backStack: NavBackStack<NavKey>) : NavigationAdapter {
 
     override val canPop = MutableStateFlow(false)
-    private val scope = CoroutineScope(SupervisorJob() + dispatcher)
-    private var job: Job? = null
 
     init {
         updateCanPop()
     }
 
     override fun navigate(destination: Destination, replaceTop: Boolean) {
-        if (job?.isActive == true) {
-            return
+        if (replaceTop) {
+            backStack[backStack.lastIndex] = destination
+        } else {
+            backStack.add(destination)
         }
-        perform {
-            if (replaceTop) {
-                backStack[backStack.lastIndex] = destination
-            } else {
-                backStack.add(destination)
-            }
-            updateCanPop()
-        }
+        updateCanPop()
     }
 
     override fun pop() {
-        if (job?.isActive == true) {
-            return
+        if (canPop.value) {
+            backStack.removeLast()
         }
-        perform {
-            if (canPop.value) {
-                backStack.removeLast()
-            }
-            updateCanPop()
-        }
+        updateCanPop()
     }
 
     override fun popUntilRoot() {
-        if (job?.isActive == true) {
-            return
-        }
-        perform {
-            backStack.retainAll { it == backStack.first() }
-            updateCanPop()
-        }
+        backStack.retainAll { it == backStack.first() }
+        updateCanPop()
     }
 
     private fun updateCanPop() {
         canPop.update { backStack.size > 1 }
-    }
-
-    private fun perform(interval: Duration = 250.milliseconds, action: () -> Unit) {
-        job = scope.launch {
-            action()
-            delay(interval)
-            job = null
-        }
     }
 }
