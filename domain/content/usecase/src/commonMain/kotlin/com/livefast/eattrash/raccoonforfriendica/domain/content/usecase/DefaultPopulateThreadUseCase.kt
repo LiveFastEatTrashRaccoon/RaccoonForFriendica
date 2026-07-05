3560a0a35e23e3424bf0cceed6314ffdad0d8e29
Dieguitux <andongue.nena@gmail.com>
2026-07-05 09:06:58 +0200

    refactor: improve DefaultPopulateThreadUseCase

--- domain/content/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/usecase/DefaultPopulateThreadUseCase.kt
+++ domain/content/usecase/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/usecase/DefaultPopulateThreadUseCase.kt
@@ -7,10 +7,7 @@ import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.Emoji
 import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.TimelineEntryRepository
 import kotlinx.coroutines.coroutineScope
 
-private data class ConversationNode(
-    val entry: TimelineEntryModel,
-    val children: MutableList<ConversationNode> = mutableListOf(),
-)
+private data class ConversationNode(val entry: TimelineEntryModel, var children: List<ConversationNode> = listOf())
 
 internal class DefaultPopulateThreadUseCase(
     private val timelineEntryRepository: TimelineEntryRepository,
@@ -21,7 +18,6 @@ internal class DefaultPopulateThreadUseCase(
         populateTree(
             node = root,
             maxDepth = maxDepth,
-            depthOffset = entry.depth,
             depth = 0,
         )
         val res =
@@ -33,55 +29,48 @@ internal class DefaultPopulateThreadUseCase(
         return res.toList()
     }
 
-    private suspend fun populateTree(node: ConversationNode, depth: Int, depthOffset: Int = 0, maxDepth: Int): Unit =
-        coroutineScope {
-            val entry = node.entry
-            if (entry.replyCount == 0) {
-                // base case 0: entry has no children
-                return@coroutineScope
-            }
-            if (depth >= maxDepth) {
-                // base case 1: max depth level reached
-                return@coroutineScope
-            }
-
-            // return all direct descendants
-            val descendants =
-                timelineEntryRepository
-                    .getContext(entry.id)
-                    ?.descendants
-                    .orEmpty()
-                    .map {
-                        with(emojiHelper) { it.withEmojisIfMissing() }
-                    }
-            val childNodes =
-                descendants
-                    .mapNotNull { child ->
-                        // needed because otherwise replies of different depths are included
-                        val referenceChild = child.original
-                        if (referenceChild.inReplyTo?.id == entry.id ||
-                            referenceChild.inReplyTo?.id == entry.reblog?.id
-                        ) {
-                            ConversationNode(
-                                entry =
-                                referenceChild.copy(
-                                    depth = depthOffset + depth + 1,
-                                ),
-                            )
-                        } else {
-                            null
-                        }
-                    }
-            for (child in childNodes) {
-                populateTree(
-                    node = child,
-                    depth = depth + 1,
-                    depthOffset = depthOffset,
-                    maxDepth = maxDepth,
-                )
-            }
-            node.children.addAll(childNodes)
+    private suspend fun populateTree(node: ConversationNode, depth: Int, maxDepth: Int): Unit = coroutineScope {
+        val entry = node.entry
+        if (entry.replyCount == 0) {
+            // base case 0: entry has no children
+            return@coroutineScope
         }
+        if (depth >= maxDepth) {
+            // base case 1: max depth level reached
+            return@coroutineScope
+        }
+
+        // return all direct descendants
+        val descendants =
+            timelineEntryRepository
+                .getContext(entry.id)
+                ?.descendants
+                .orEmpty()
+                .map {
+                    with(emojiHelper) { it.withEmojisIfMissing() }
+                }
+        val childNodes =
+            descendants
+                .mapNotNull { child ->
+                    // needed because otherwise replies of different depths are included
+                    val referenceChild = child.original
+                    if (referenceChild.inReplyTo?.id == entry.id ||
+                        referenceChild.inReplyTo?.id == entry.reblog?.id
+                    ) {
+                        ConversationNode(entry = referenceChild.copy(depth = node.entry.depth + 1))
+                    } else {
+                        null
+                    }
+                }
+        for (child in childNodes) {
+            populateTree(
+                node = child,
+                depth = depth + 1,
+                maxDepth = maxDepth,
+            )
+        }
+        node.children = childNodes
+    }
 
     private fun MutableList<TimelineEntryModel>.linearize(node: ConversationNode) {
         add(node.entry)
@@ -94,8 +83,7 @@ internal class DefaultPopulateThreadUseCase(
 
     private fun List<TimelineEntryModel>.populateLoadMore() = mapIndexed { idx, entry ->
         val hasMoreReplies = entry.replyCount > 0
-        val isNextCommentNotChild =
-            (idx < lastIndex && this[idx + 1].depth <= entry.depth) || idx == lastIndex
+        val isNextCommentNotChild = idx == lastIndex || this[idx + 1].depth <= entry.depth
         entry.copy(loadMoreButtonVisible = hasMoreReplies && isNextCommentNotChild)
     }
 }
