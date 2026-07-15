0b5e0f1301faabff7301bc6566f631a61d307704
Dieguitux <andongue.nena@gmail.com>
2026-07-15 21:04:35 +0200

    add QuotedInfo Composable

---
+++ feature/composer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/composer/components/QuotedInfo.kt
@@ -0,0 +1,64 @@
+package com.livefast.eattrash.raccoonforfriendica.feature.composer.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.IconSize
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.Spacing
+import com.livefast.eattrash.raccoonforfriendica.core.appearance.theme.ancillaryTextAlpha
+import com.livefast.eattrash.raccoonforfriendica.core.commonui.components.di.SetupPreview
+import com.livefast.eattrash.raccoonforfriendica.core.di.RootDI
+import com.livefast.eattrash.raccoonforfriendica.core.l10n.LocalStrings
+import com.livefast.eattrash.raccoonforfriendica.core.resources.LocalResources
+
+@Composable
+internal fun QuotedInfo(username: String, modifier: Modifier = Modifier) {
+    val fullColor = MaterialTheme.colorScheme.onBackground
+    val ancillaryColor = MaterialTheme.colorScheme.onBackground.copy(ancillaryTextAlpha)
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Spacing.s),
+    ) {
+        Icon(
+            modifier = Modifier.size(IconSize.s),
+            imageVector = LocalResources.current.formatQuoteFill,
+            contentDescription = null,
+            tint = ancillaryColor,
+        )
+        Text(
+            text =
+            buildAnnotatedString {
+                append(LocalStrings.current.actionQuote)
+                append(" ")
+                withStyle(SpanStyle(color = fullColor)) {
+                    append(username)
+                }
+            },
+            style = MaterialTheme.typography.bodyMedium,
+            color = ancillaryColor,
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun QuotedInfoPreview() {
+    RootDI.SetupPreview()
+    Surface {
+        QuotedInfo(
+            username = "User name",
+        )
+    }
+}
