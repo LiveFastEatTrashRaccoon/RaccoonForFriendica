21ddcef64ce76a9f1b8912d2c7939bac4ed4c25d
Dieguitux <andongue.nena@gmail.com>
2026-07-18 16:00:46 +0200

    update service definitions

--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/DefaultPhotoAlbumService.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/DefaultPhotoAlbumService.kt
@@ -1,12 +1,9 @@
 package com.livefast.eattrash.raccoonforfriendica.core.api.service
 
 import com.livefast.eattrash.raccoonforfriendica.core.api.dto.FriendicaApiResult
-import com.livefast.eattrash.raccoonforfriendica.core.api.dto.FriendicaPhoto
-import com.livefast.eattrash.raccoonforfriendica.core.api.dto.FriendicaPhotoAlbum
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.forms.FormDataContent
-import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
@@ -14,19 +11,6 @@ import io.ktor.http.contentType
 
 internal class DefaultPhotoAlbumService(private val baseUrl: String, private val client: HttpClient) :
     PhotoAlbumService {
-    override suspend fun getAll(): List<FriendicaPhotoAlbum> = client.post("$baseUrl/friendica/photoalbums").body()
-
-    override suspend fun getPhotos(
-        album: String,
-        maxId: String?,
-        latestFirst: Boolean,
-        limit: Int,
-    ): List<FriendicaPhoto> = client.post("$baseUrl/friendica/photoalbum") {
-        parameter("album", album)
-        parameter("maxId", maxId)
-        parameter("latest_first", latestFirst)
-        parameter("limit", limit)
-    }.body()
 
     override suspend fun update(data: FormDataContent): FriendicaApiResult =
         client.post("$baseUrl/friendica/photoalbum/update") {
--- core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/PhotoService.kt
+++ core/api/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/api/service/PhotoService.kt
@@ -5,6 +5,8 @@ import com.livefast.eattrash.raccoonforfriendica.core.api.dto.FriendicaPhoto
 import io.ktor.client.request.forms.MultiPartFormDataContent
 
 interface PhotoService {
+    suspend fun getAll(): List<FriendicaPhoto>
+
     suspend fun create(content: MultiPartFormDataContent): FriendicaPhoto
 
     suspend fun update(content: MultiPartFormDataContent): FriendicaApiResult
