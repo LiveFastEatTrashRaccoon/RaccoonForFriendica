6c79844db75b133d0198c18b5018c748cb7de110
Dieguitux <andongue.nena@gmail.com>
2026-07-18 14:59:14 +0200

    refactor: move QuotePolicy#toReadableName() to domain

--- domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/QuotePolicy.kt
+++ domain/content/data/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/data/QuotePolicy.kt
@@ -1,7 +1,17 @@
 package com.livefast.eattrash.raccoonforfriendica.domain.content.data
 
+import androidx.compose.runtime.Composable
+import com.livefast.eattrash.raccoonforfriendica.core.l10n.LocalStrings
+
 sealed interface QuotePolicy {
     data object Public : QuotePolicy
     data object Followers : QuotePolicy
     data object Nobody : QuotePolicy
+}
+
+@Composable
+fun QuotePolicy.toReadableName(): String = when (this) {
+    QuotePolicy.Followers -> LocalStrings.current.quotePolicyFollowers
+    QuotePolicy.Nobody -> LocalStrings.current.quotePolicyNobody
+    QuotePolicy.Public -> LocalStrings.current.quotePolicyPublic
 }
