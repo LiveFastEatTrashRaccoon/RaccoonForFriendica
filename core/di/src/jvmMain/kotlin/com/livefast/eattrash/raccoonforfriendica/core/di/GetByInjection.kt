dad736ceb71a4af9b4b94e3a3fea74f801aee3f0
Dieguitux <andongue.nena@gmail.com>
2026-07-26 22:23:05 +0200

    fix DI for iOS interop

--- core/di/src/jvmMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/di/GetByInjection.kt
+++ core/di/src/jvmMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/di/GetByInjection.kt
@@ -2,11 +2,9 @@ package com.livefast.eattrash.raccoonforfriendica.core.di
 
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
-import org.koin.java.KoinJavaComponent.inject
+import org.koin.java.KoinJavaComponent.get
 import kotlin.reflect.KClass
 
 @DelicateDiApi
-actual fun <T : Any> getByInjection(clazz: KClass<T>, qualifier: Qualifier?, parameters: ParametersDefinition?): T {
-    val res: T by inject(clazz = clazz.java, qualifier = qualifier, parameters = parameters)
-    return res
-}
+actual fun <T : Any> getByInjection(clazz: KClass<T>, qualifier: Qualifier?, parameters: ParametersDefinition?): T =
+    get(clazz = clazz.java, qualifier = qualifier, parameters = parameters)
