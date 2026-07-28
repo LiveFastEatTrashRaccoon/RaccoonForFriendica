467ebd4cb8d5a9cf0a40792f4fb035cab6e64ca4
Dieguitux <andongue.nena@gmail.com>
2026-07-28 22:42:15 +0200

    remove unneeded DelicateDiApi usage from LocalStrings CompositionLocal

--- core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/l10n/ProvideStrings.kt
+++ core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/l10n/ProvideStrings.kt
@@ -5,19 +5,18 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalLayoutDirection
-import com.livefast.eattrash.raccoonforfriendica.core.di.DelicateDiApi
-import com.livefast.eattrash.raccoonforfriendica.core.l10n.di.getStrings
+import com.livefast.eattrash.raccoonforfriendica.core.l10n.di.rememberStrings
 
 val LocalStrings: ProvidableCompositionLocal<Strings> =
     staticCompositionLocalOf {
         error("CompositionLocal Strings not found")
     }
 
-@OptIn(DelicateDiApi::class)
 @Composable
 fun ProvideStrings(lang: String, content: @Composable () -> Unit) {
+    val strings = rememberStrings(lang)
     CompositionLocalProvider(
-        LocalStrings provides getStrings(lang),
+        LocalStrings provides strings,
         LocalLayoutDirection provides lang.toLanguageDirection(),
         content = content,
     )
--- core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/l10n/di/Utils.kt
+++ core/l10n/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/core/l10n/di/Utils.kt
@@ -12,5 +12,8 @@ import org.koin.core.parameter.parametersOf
 @Composable
 fun rememberL10nManager() = remember { getByInjection(L10nManager::class) }
 
-@DelicateDiApi
-internal fun getStrings(lang: String): Strings = getByInjection(clazz = Strings::class, parameters = { parametersOf(lang) })
+@OptIn(DelicateDiApi::class)
+@Composable
+internal fun rememberStrings(lang: String): Strings = remember {
+    getByInjection(clazz = Strings::class, parameters = { parametersOf(lang) })
+}
