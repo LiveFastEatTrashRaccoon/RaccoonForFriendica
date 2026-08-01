29b9387ca63fb53f84b50e33257322c892c991af
Dieguitux <andongue.nena@gmail.com>
2026-08-01 08:20:02 +0200

    remove all remember* accessors definitions

--- domain/content/repository/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/domain/content/repository/di/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.livefast.eattrash.raccoonforfriendica.domain.content.repository.di
-
-import com.livefast.eattrash.raccoonforfriendica.core.di.DelicateDiApi
-import com.livefast.eattrash.raccoonforfriendica.core.di.getByInjection
-import com.livefast.eattrash.raccoonforfriendica.domain.content.repository.AttachmentCache
-
-@DelicateDiApi
-fun getAttachmentCache(): AttachmentCache = getByInjection(AttachmentCache::class)
