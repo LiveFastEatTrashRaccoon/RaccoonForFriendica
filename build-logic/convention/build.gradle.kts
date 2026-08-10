38238bab103ef698c5b1b242c5587613c255f564
Dieguitux <andongue.nena@gmail.com>
2026-08-10 17:47:42 +0200

    update DiPlugin to use Koin Compiler Plugin and annotations
    with optional compileSafety

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
     compileOnly(libs.spotless.gradlePlugin)
+    compileOnly(libs.koin.gradlePlugin)
     implementation(libs.kotlinpoet)
 }
 
