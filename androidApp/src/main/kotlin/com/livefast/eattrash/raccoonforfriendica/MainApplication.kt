d97ec339c4ae5918556a308ea139de93f8e7de8a
Dieguitux <andongue.nena@gmail.com>
2026-08-12 19:06:59 +0200

    call workManagerFactory in Android entry point

--- androidApp/src/main/kotlin/com/livefast/eattrash/raccoonforfriendica/MainApplication.kt
+++ androidApp/src/main/kotlin/com/livefast/eattrash/raccoonforfriendica/MainApplication.kt
@@ -3,6 +3,7 @@ package com.livefast.eattrash.raccoonforfriendica
 import android.app.Application
 import com.livefast.eattrash.raccoonforfriendica.di.setupDi
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.workmanager.koin.workManagerFactory
 
 class MainApplication : Application() {
 
@@ -10,6 +11,7 @@ class MainApplication : Application() {
         super.onCreate()
         setupDi {
             androidContext(applicationContext)
+            workManagerFactory()
         }
     }
 }
