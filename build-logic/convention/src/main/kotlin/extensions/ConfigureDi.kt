893d93e87206cc890ec75618bb3d1061c0dc30c1
Dieguitux <andongue.nena@gmail.com>
2026-09-06 13:01:43 +0200

    define new convention plugin for DI

--- build-logic/convention/src/main/kotlin/extensions/ConfigureDi.kt
+++ build-logic/convention/src/main/kotlin/extensions/ConfigureDi.kt
@@ -3,34 +3,20 @@ package extensions
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.koin.compiler.plugin.KoinGradleExtension
 import utils.dependency
 import utils.libs
-import utils.pluginId
 
 interface CustomDiExtension {
     fun useCompose(withViewModels: Boolean = false)
-    fun useAnnotations()
-    fun useCompilerPlugin(enableValidation: Boolean = true)
 }
 
-internal fun Project.configureDi(extension: KotlinMultiplatformExtension) {
+internal fun Project.configureDi() {
     extensions.create(
         CustomDiExtension::class.java,
         "customDiExtension",
         CustomDiExtensionImpl::class.java,
         this,
     )
-    extension.apply {
-        sourceSets.apply {
-            commonMain {
-                dependencies {
-                    implementation(project.dependencies.platform(libs.findLibrary("koin-bom").dependency))
-                    implementation(libs.findLibrary("koin-core").dependency)
-                }
-            }
-        }
-    }
 }
 
 open class CustomDiExtensionImpl(private val target: Project) : CustomDiExtension {
@@ -38,31 +24,9 @@ open class CustomDiExtensionImpl(private val target: Project) : CustomDiExtensio
         with(target) {
             extensions.configure<KotlinMultiplatformExtension> {
                 sourceSets.getByName("commonMain").dependencies {
-                    implementation(libs.findLibrary("koin-compose").dependency)
                     if (withViewModels) {
-                        implementation(libs.findLibrary("koin-compose-viewmodel").dependency)
+                        implementation(libs.findLibrary("metrox-viewmodel-compose").dependency)
                     }
-                }
-            }
-        }
-    }
-
-    override fun useAnnotations() {
-        with(target) {
-            extensions.configure<KotlinMultiplatformExtension> {
-                sourceSets.getByName("commonMain").dependencies {
-                    implementation(libs.findLibrary("koin-annotations").dependency)
-                }
-            }
-        }
-    }
-
-    override fun useCompilerPlugin(enableValidation: Boolean) {
-        with(target) {
-            pluginManager.apply(libs.findPlugin("koin-compiler").pluginId)
-            if (!enableValidation) {
-                extensions.configure(KoinGradleExtension::class.java) {
-                    compileSafety.set(false)
                 }
             }
         }
--- build-logic/convention/src/main/kotlin/plugins/DiPlugin.kt
+++ build-logic/convention/src/main/kotlin/plugins/DiPlugin.kt
@@ -3,14 +3,16 @@ package plugins
 import extensions.configureDi
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import utils.libs
+import utils.pluginId
 
 class DiPlugin : Plugin<Project>  {
     override fun apply(target: Project) {
         with(target) {
-            extensions.configure(KotlinMultiplatformExtension::class.java) {
-               configureDi(this)
+            with(pluginManager) {
+                apply(libs.findPlugin("metro").pluginId)
             }
+            configureDi()
         }
     }
 }
