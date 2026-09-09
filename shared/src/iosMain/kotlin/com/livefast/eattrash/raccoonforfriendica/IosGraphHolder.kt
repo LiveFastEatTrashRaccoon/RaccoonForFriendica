1e401bf362b5efa16c0c21688acd39dcb55f2bc4
Dieguitux <andongue.nena@gmail.com>
2026-09-09 06:55:06 +0200

    chore: remove unused iOS helper

--- shared/src/iosMain/kotlin/com/livefast/eattrash/raccoonforfriendica/IosGraphHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.livefast.eattrash.raccoonforfriendica
-
-import dev.zacsweers.metro.createGraph
-
-object IosGraphHolder {
-    val rootGraph: IosRootGraph by lazy { createGraph<IosRootGraph>() }
-}
