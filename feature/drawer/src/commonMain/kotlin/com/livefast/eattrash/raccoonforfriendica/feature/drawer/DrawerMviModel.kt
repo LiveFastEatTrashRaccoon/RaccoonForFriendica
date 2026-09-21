1330f149225e33ac7cbfd05bf8878d0ab21d04e5
Dieguitux <andongue.nena@gmail.com>
2026-09-21 23:24:45 +0200

    cleanup DrawerViewModel

--- feature/drawer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/drawer/DrawerMviModel.kt
+++ feature/drawer/src/commonMain/kotlin/com/livefast/eattrash/raccoonforfriendica/feature/drawer/DrawerMviModel.kt
@@ -2,19 +2,11 @@ package com.livefast.eattrash.raccoonforfriendica.feature.drawer
 
 import androidx.compose.runtime.Stable
 import com.livefast.eattrash.raccoonforfriendica.core.architecture.MviModel
-import com.livefast.eattrash.raccoonforfriendica.core.utils.validation.ValidationError
 import com.livefast.eattrash.raccoonforfriendica.domain.content.data.UserModel
-import com.livefast.eattrash.raccoonforfriendica.domain.identity.data.AccountModel
 
 @Stable
 interface DrawerMviModel : MviModel<DrawerMviModel.Intent, DrawerMviModel.State, DrawerMviModel.Effect> {
-    sealed interface Intent {
-        data class SetAnonymousChangeNode(val nodeName: String) : Intent
-
-        data object SubmitAnonymousChangeNode : Intent
-
-        data class SwitchAccount(val account: AccountModel) : Intent
-    }
+    sealed interface Intent
 
     data class State(
         val user: UserModel? = null,
@@ -23,16 +15,9 @@ interface DrawerMviModel : MviModel<DrawerMviModel.Intent, DrawerMviModel.State,
         val hasGallery: Boolean = false,
         val hasCalendar: Boolean = false,
         val hasAnnouncements: Boolean = false,
-        val anonymousChangeNodeName: String = "",
-        val anonymousChangeNodeValidationInProgress: Boolean = false,
-        val anonymousChangeNodeNameError: ValidationError? = null,
-        val availableAccounts: List<AccountModel> = emptyList(),
+        val canSwitchAccount: Boolean = false,
         val autoloadImages: Boolean = true,
     )
 
-    sealed interface Effect {
-        data object AnonymousChangeNodeSuccess : Effect
-
-        data object AccountChangeSuccess : Effect
-    }
+    sealed interface Effect
 }
